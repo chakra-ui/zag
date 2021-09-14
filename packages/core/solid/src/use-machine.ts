acb3770ea532e0578a5bc5c653b5b36d1c251c3e
Segun Adebayo <joseshegs@gmail.com>
2021-09-14 21:51:36 +0400

    feat: add solidjs hook

---
+++ packages/core/solid/src/use-machine.ts
@@ -0,0 +1,40 @@
+import { MachineSrc, StateMachine as S } from "@ui-machines/core"
+import { createSignal, onCleanup, onMount } from "solid-js"
+
+export function useMachine<
+  TContext extends Record<string, any>,
+  TState extends S.StateSchema,
+  TEvent extends S.EventObject = S.AnyEventObject,
+>(
+  machine: MachineSrc<TContext, TState, TEvent>,
+  options?: S.MachineOptions<TContext, TState, TEvent> & {
+    state?: S.StateInit<TContext, TState>
+  },
+) {
+  const { actions, state: hydratedState } = options ?? {}
+
+  const service = (() => {
+    const resolvedMachine = typeof machine === "function" ? machine() : machine.clone()
+    return options ? resolvedMachine.withOptions(options) : resolvedMachine
+  })()
+
+  const [state, setState] = createSignal({} as S.State<TContext, TState>)
+
+  let unsubscribe: () => void
+
+  onMount(() => {
+    service.start(hydratedState)
+    unsubscribe = service.subscribe(setState)
+  })
+
+  onCleanup(() => {
+    service.stop()
+    unsubscribe()
+  })
+
+  onMount(() => {
+    service.updateActions(actions)
+  })
+
+  return [state, service.send, service] as const
+}
