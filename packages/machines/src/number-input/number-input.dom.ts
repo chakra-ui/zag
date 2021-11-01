e649febb7e2b99f830638c39a5f1f04291d7eb63
Segun Adebayo <joseshegs@gmail.com>
2021-11-01 18:59:59 +0400

    refactor: number input machine

--- packages/machines/src/number-input/number-input.dom.ts
+++ packages/machines/src/number-input/number-input.dom.ts
@@ -5,12 +5,15 @@ type ButtonEl = HTMLButtonElement | null
 
 export const dom = {
   getDoc: (ctx: Ctx) => ctx.doc ?? document,
+  getWin: (ctx: Ctx) => dom.getDoc(ctx).defaultView ?? window,
 
   getInputId: (ctx: Ctx) => `input-${ctx.uid}`,
   getIncButtonId: (ctx: Ctx) => `inc-btn-${ctx.uid}`,
   getDecButtonId: (ctx: Ctx) => `dec-btn-${ctx.uid}`,
+  getScrubberId: (ctx: Ctx) => `scrubber-${ctx.uid}`,
 
   getInputEl: (ctx: Ctx) => dom.getDoc(ctx).getElementById(dom.getInputId(ctx)) as InputEl,
   getIncButtonEl: (ctx: Ctx) => dom.getDoc(ctx).getElementById(dom.getIncButtonId(ctx)) as ButtonEl,
   getDecButtonEl: (ctx: Ctx) => dom.getDoc(ctx).getElementById(dom.getDecButtonId(ctx)) as ButtonEl,
+  getScrubberEl: (ctx: Ctx) => dom.getDoc(ctx).getElementById(dom.getScrubberId(ctx)),
 }
