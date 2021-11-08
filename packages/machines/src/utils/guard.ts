3943bdf7fe8221afbb7fae861d04ab4bb287238b
Segun Adebayo <joseshegs@gmail.com>
2021-11-09 01:14:13 +0400

    chore: update is-safari check

--- packages/machines/src/utils/guard.ts
+++ packages/machines/src/utils/guard.ts
@@ -1,6 +1,5 @@
 const platform = (v: RegExp) => isDom() && v.test(navigator.platform)
 const ua = (v: RegExp) => isDom() && v.test(navigator.userAgent)
-const vendor = (v: RegExp) => isDom() && v.test(navigator.vendor)
 
 export const isDev = () => process.env.NODE_ENV !== "production"
 export const isDom = () => !!(typeof window !== "undefined")
@@ -8,7 +7,7 @@ export const isMac = () => platform(/^Mac/)
 export const isIPhone = () => platform(/^iPhone/)
 export const isIPad = () => platform(/^iPad/) || (isMac() && navigator.maxTouchPoints > 1)
 export const isIos = () => isIPhone() || isIPad()
-export const isSafari = () => ua(/^Safari/) && vendor(/Apple Computer/)
+export const isSafari = () => ua(/^((?!chrome|android).)*safari/i)
 export const isFirefox = () => ua(/^Firefox/)
 export const isWebkit = () => ua(/^WebKit/) && !ua(/Chrome/)
 export const isApple = () => isMac() || isIos()
