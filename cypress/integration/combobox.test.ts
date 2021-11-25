0121e38f6108ab69fc9fcd6bf5bdd20227d7c1be
Segun Adebayo <joseshegs@gmail.com>
2021-11-25 22:17:09 +0400

    test: scaffold combobox test

---
+++ cypress/integration/combobox.test.ts
@@ -0,0 +1,10 @@
+/* eslint-disable jest/expect-expect */
+describe("accordion", () => {
+  beforeEach(() => {
+    cy.visit("/combobox")
+    cy.injectAxe()
+  })
+
+  describe("basic combobox", () => {})
+  describe("autocomplete combobox", () => {})
+})
