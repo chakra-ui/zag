a5555289a310be9434de3d12ff89532151bfa943
Segun Adebayo <joseshegs@gmail.com>
2021-11-29 22:21:14 +0400

    chore: update vue use-controls

--- examples/vue-ts/src/hooks/use-controls.tsx
+++ examples/vue-ts/src/hooks/use-controls.tsx
@@ -1,30 +1,7 @@
-///<reference path="../shims-vue.d.ts"/>
 import { computed, h, Fragment, Ref, ref, inject, provide } from "vue"
+import { ControlRecord, ControlValue } from "@ui-machines/types"
 
-type Prop =
-  | { type: "boolean"; label: string; defaultValue: boolean }
-  | { type: "string"; label: string; defaultValue: string; placeholder?: string }
-  | { type: "select"; options: readonly string[]; defaultValue: string; label: string }
-  | { type: "multiselect"; options: readonly string[]; defaultValue: string[]; label: string }
-  | { type: "number"; label: string; defaultValue: number; min?: number; max?: number }
-
-type ControlRecord = Record<string, Prop>
-
-type Value<T extends ControlRecord> = {
-  [K in keyof T]: T[K] extends { type: "boolean" }
-    ? boolean
-    : T[K] extends { type: "string" }
-    ? string
-    : T[K] extends { type: "select" }
-    ? T[K]["options"][number]
-    : T[K] extends { type: "multiselect" }
-    ? T[K]["options"][number][]
-    : T[K] extends { type: "number" }
-    ? number
-    : never
-}
-
-export const PropertyControlsInjectionSymbol = Symbol("ChakraPropertyControlsState")
+const ControlsSymbol = Symbol("use-controls")
 
 function getDefaultValues<T extends ControlRecord>(obj: T) {
   return Object.keys(obj).reduce(
@@ -32,7 +9,7 @@ function getDefaultValues<T extends ControlRecord>(obj: T) {
       ...acc,
       [key]: obj[key].defaultValue,
     }),
-    {} as Value<T>,
+    {} as ControlValue<T>,
   )
 }
 
@@ -40,10 +17,10 @@ export function useControls<T extends ControlRecord>(config: T) {
   const defaults = getDefaultValues(config)
   const state = ref(defaults)
 
-  provide(PropertyControlsInjectionSymbol, state)
+  provide(ControlsSymbol, state)
 
   function useControlsState() {
-    return inject<Ref<Value<T>>>(PropertyControlsInjectionSymbol)!
+    return inject<Ref<ControlValue<T>>>(ControlsSymbol)!
   }
 
   return {
@@ -63,7 +40,7 @@ export function useControls<T extends ControlRecord>(config: T) {
             margin: "24px",
           }}
         >
-          {Object.keys(config).map((key: keyof Value<T>) => {
+          {Object.keys(config).map((key: keyof ControlValue<T>) => {
             const { type, label, options, placeholder, min, max } = (config[key] ?? {}) as any
             switch (type) {
               case "boolean":
