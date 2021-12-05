881534ef0bc8296596b7a27cfd01cc2d2841aa56
Segun Adebayo <joseshegs@gmail.com>
2021-12-06 01:53:10 +0400

    chore: update range utils

--- packages/utilities/number/src/rangy.ts
+++ packages/utilities/number/src/rangy.ts
@@ -23,31 +23,16 @@ export type RangeOptions<T = string | number> = Num<"min" | "max"> & {
 export function rangy(o: RangeOptions) {
   const wrap = (v: string | number) => rangy({ ...o, value: v })
   return {
-    get isInRange() {
-      return isWithinRange(o.value, o)
-    },
-    get isAtMax() {
-      return isAtMax(o.value, o)
-    },
-    get isAtMin() {
-      return isAtMin(o.value, o)
-    },
-    get percent() {
-      return valueToPercent(o.value, o)
-    },
-    get valueAsNumber() {
-      return valueOf(o.value)
-    },
-    get value(): string {
-      return this.toPrecision().value
-    },
-    get precision() {
-      return getMaxPrecision(o)
-    },
+    isInRange: isWithinRange(o.value, o),
+    isAtMax: isAtMax(o.value, o),
+    isAtMin: isAtMin(o.value, o),
+    percent: valueToPercent(o.value, o),
+    valueAsNumber: valueOf(o.value),
+    value: round(o.value, getMaxPrecision(o)),
+    precision: getMaxPrecision(o),
     snapToStep: () => wrap(snapToStep(o.value, o.step)),
     increment: (s?: number) => wrap(increment(o.value, s || o.step)),
     decrement: (s?: number) => wrap(decrement(o.value, s || o.step)),
-    toPrecision: () => wrap(round(o.value, getMaxPrecision(o))),
     toMax: () => wrap(o.max),
     toMin: () => wrap(o.min),
     fromPercent: (p: number) => wrap(percentToValue(p, o)),
