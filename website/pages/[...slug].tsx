1a073d27eecd996b544e245f3311f9919a659cd0
Segun Adebayo <joseshegs@gmail.com>
2021-12-07 09:15:06 +0400

    docs: support other slugs

--- website/pages/[...slug].tsx
+++ website/pages/[...slug].tsx
@@ -1,4 +1,4 @@
-import { allComponents } from ".contentlayer/data"
+import { allComponents, allGuides, allOverviews } from ".contentlayer/data"
 import type { Component } from ".contentlayer/types"
 import { useMDX } from "components/mdx-components"
 import { Framework, FrameworkContext, FRAMEWORKS } from "lib/framework"
@@ -14,11 +14,14 @@ export default function Page({ page, framework }: { page: Component; framework: 
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const basePaths = allOverviews.concat(allGuides as any).map((p) => ({ params: { slug: [p.slug] } }))
+
   let paths = allComponents
     .map((p) => FRAMEWORKS.map((s) => [s, ...p.params]))
     .flat()
     .map((slug) => ({ params: { slug } }))
-  return { paths, fallback: false }
+
+  return { paths: paths.concat(basePaths), fallback: false }
 }
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
@@ -29,6 +32,6 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
     framework = slug.shift()
   }
   const _slug = slug.join("/")
-  const page = allComponents.find((post) => post.frontmatter.slug.endsWith(_slug))
+  const page = [...allComponents, ...allGuides, ...allOverviews].find((post) => post.frontmatter.slug.endsWith(_slug))
   return { props: { page, framework } }
 }
