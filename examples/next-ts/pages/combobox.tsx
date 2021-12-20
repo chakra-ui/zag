32f59c370087f040febdda0fb692e8aaee7721a2
Segun Adebayo <joseshegs@gmail.com>
2021-12-20 14:03:22 +0100

    chore: update examples

--- examples/next-ts/pages/combobox.tsx
+++ examples/next-ts/pages/combobox.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import styled from "@emotion/styled"
 import * as Combobox from "@ui-machines/combobox"
 import { useMachine } from "@ui-machines/react"
--- examples/next-ts/pages/number-input.tsx
+++ examples/next-ts/pages/number-input.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import { mergeProps } from "@ui-machines/core"
 import * as NumberInput from "@ui-machines/number-input"
 import { useMachine } from "@ui-machines/react"
