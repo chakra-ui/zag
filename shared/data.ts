f1a03daebb38adb3b275302cdf1b0887c1b70205
Segun Adebayo <joseshegs@gmail.com>
2022-01-16 19:16:03 +0100

    refactor: shared tabs data

--- shared/data.ts
+++ shared/data.ts
@@ -30,3 +30,31 @@ export const comboboxData = [
   { label: "Trinidad and Tobago", code: "TT" },
   { label: "Tunisia", code: "TN" },
 ]
+
+export const tabsData = [
+  {
+    id: "nils",
+    label: "Nils Frahm",
+    content: `
+    Nils Frahm is a German musician, composer and record producer based in Berlin. He is known for combining
+            classical and electronic music and for an unconventional approach to the piano in which he mixes a grand
+            piano, upright piano, Roland Juno-60, Rhodes piano, drum machine, and Moog Taurus.
+    `,
+  },
+  {
+    id: "agnes",
+    label: "Agnes Obel",
+    content: `
+    Agnes Caroline Thaarup Obel is a Danish singer/songwriter. Her first album, Philharmonics, was released by
+            PIAS Recordings on 4 October 2010 in Europe. Philharmonics was certified gold in June 2011 by the Belgian
+            Entertainment Association (BEA) for sales of 10,000 Copies.
+    `,
+  },
+  {
+    id: "joke",
+    label: "Joke",
+    content: `
+    Fear of complicated buildings: A complex complex complex.
+    `,
+  },
+]
