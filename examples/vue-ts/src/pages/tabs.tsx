c69b4926ad0454346568f75a759969bf2476b9ca
Segun Adebayo <joseshegs@gmail.com>
2022-01-16 19:15:51 +0100

    refactor: shared tabs data

--- examples/vue-ts/src/pages/tabs.tsx
+++ examples/vue-ts/src/pages/tabs.tsx
@@ -2,37 +2,10 @@ import * as Tabs from "@ui-machines/tabs"
 import { normalizeProps, useMachine, VuePropTypes } from "@ui-machines/vue"
 import { defineComponent } from "@vue/runtime-core"
 import { computed, h, Fragment } from "vue"
+import { tabsData } from "../../../../shared/data"
 import { StateVisualizer } from "../components/state-visualizer"
 import { useControls } from "../hooks/use-controls"
 import { useMount } from "../hooks/use-mount"
-
-const tabsData = [
-  {
-    id: "nils",
-    label: "Nils Frahm",
-    content: `
-    Nils Frahm is a German musician, composer and record producer based in Berlin. He is known for combining
-            classical and electronic music and for an unconventional approach to the piano in which he mixes a grand
-            piano, upright piano, Roland Juno-60, Rhodes piano, drum machine, and Moog Taurus.
-    `,
-  },
-  {
-    id: "agnes",
-    label: "Agnes Obel",
-    content: `
-    Agnes Caroline Thaarup Obel is a Danish singer/songwriter. Her first album, Philharmonics, was released by
-            PIAS Recordings on 4 October 2010 in Europe. Philharmonics was certified gold in June 2011 by the Belgian
-            Entertainment Association (BEA) for sales of 10,000 Copies.
-    `,
-  },
-  {
-    id: "joke",
-    label: "Joke",
-    content: `
-    Fear of complicated buildings: A complex complex complex.
-    `,
-  },
-]
 
 export default defineComponent({
   name: "Tabs",
