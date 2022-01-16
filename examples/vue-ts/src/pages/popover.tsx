4b1bdf3e6bb52f0d743f4b73cf71ae0d7f76e6c8
Segun Adebayo <joseshegs@gmail.com>
2022-01-16 23:32:03 +0100

    chore: update vue popover example

--- examples/vue-ts/src/pages/popover.tsx
+++ examples/vue-ts/src/pages/popover.tsx
@@ -1,43 +1,77 @@
-import { css } from "@emotion/css"
+import { injectGlobal } from "@emotion/css"
 import * as Popover from "@ui-machines/popover"
 import { normalizeProps, useMachine, VuePropTypes } from "@ui-machines/vue"
 import { defineComponent } from "@vue/runtime-core"
-import { computed, h, Fragment } from "vue"
+import { useControls } from "../hooks/use-controls"
+import { computed, h, Fragment, Teleport } from "vue"
 import { popoverStyle } from "../../../../shared/style"
 import { StateVisualizer } from "../components/state-visualizer"
 import { useMount } from "../hooks/use-mount"
 
-const styles = css(popoverStyle)
+injectGlobal(popoverStyle)
 
 export default defineComponent({
   name: "Popover",
   setup() {
-    const [state, send] = useMachine(
-      Popover.machine.withContext({
-        autoFocus: true,
-      }),
-    )
+    const controls = useControls({
+      modal: { type: "boolean", defaultValue: false },
+      portalled: { type: "boolean", defaultValue: true },
+      autoFocus: { type: "boolean", defaultValue: true },
+      closeOnEsc: { type: "boolean", defaultValue: true },
+    })
+
+    const [state, send] = useMachine(Popover.machine, {
+      context: controls.context,
+    })
 
     const ref = useMount(send)
 
     const popoverRef = computed(() => Popover.connect<VuePropTypes>(state.value, send, normalizeProps))
 
     return () => {
-      const { triggerProps, contentProps } = popoverRef.value
+      const { triggerProps, headerProps, closeButtonProps, contentProps, arrowProps, innerArrowProps, portalled } =
+        popoverRef.value
+
+      const Wrapper = portalled ? Teleport : Fragment
+
       return (
-        <div class={styles}>
-          <div style={{ width: "300px" }} ref={ref}>
-            <button {...triggerProps}>Click me</button>
-            <div {...contentProps}>
-              <div>Popover content</div>
-              <div>
-                <input placeholder="hello" />
+        <>
+          <div class="popover" ref={ref}>
+            <button data-testid="button-before">Button :before</button>
+
+            <button class="popover__trigger" data-testid="popover-trigger" {...triggerProps}>
+              Click me
+            </button>
+
+            <Wrapper to="body">
+              <div class="popover__content" data-testid="popover-content" {...contentProps}>
+                <div class="popover__arrow" {...arrowProps}>
+                  <div {...innerArrowProps} />
+                </div>
+                <div class="popover__title" data-testid="popover-title" {...headerProps}>
+                  Popover Title
+                </div>
+                <div class="popover__body" data-testid="popover-body">
+                  <a>Non-focusable Link</a>
+                  <a href="#" data-testid="focusable-link">
+                    Focusable Link
+                  </a>
+                  <input data-testid="input" placeholder="input" />
+                  <button class="popover__close-button" data-testid="popover-close-button" {...closeButtonProps}>
+                    X
+                  </button>
+                </div>
               </div>
-            </div>
+            </Wrapper>
+
+            <span data-testid="plain-text">I am just text</span>
+            <button data-testid="button-after">Button :after</button>
           </div>
 
+          <controls.ui />
+
           <StateVisualizer state={state.value} />
-        </div>
+        </>
       )
     }
   },
