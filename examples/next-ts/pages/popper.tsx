3128f0bee026147f5b4f2e3fb30ac5bca15dc94b
Segun Adebayo <joseshegs@gmail.com>
2022-01-25 13:12:41 +0100

    chore: fix popper page

--- examples/next-ts/pages/popper.tsx
+++ examples/next-ts/pages/popper.tsx
@@ -1,4 +1,4 @@
-import { DEFAULT_FLOATING_STYLE, getPlacement } from "@ui-machines/dom-utils"
+import { PLACEMENT_STYLE, getPlacement } from "@ui-machines/popper"
 import React, { useEffect, useLayoutEffect } from "react"
 
 const useSafeEffect = typeof window !== "undefined" ? useLayoutEffect : useEffect
@@ -19,7 +19,7 @@ export default function App() {
   return (
     <div>
       <button ref={referenceRef}>Hello StackBlitz!</button>
-      <div style={DEFAULT_FLOATING_STYLE} ref={floatingRef}>
+      <div style={PLACEMENT_STYLE.floating()} ref={floatingRef}>
         Start editing to see some magic happen :)
       </div>
     </div>
