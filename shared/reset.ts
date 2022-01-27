b07c04677769fc2b052a11e66331dcf304c936ab
Segun Adebayo <joseshegs@gmail.com>
2022-01-27 03:48:23 +0100

    chore: update reset styles

--- shared/reset.ts
+++ shared/reset.ts
@@ -1,29 +1,23 @@
 import { injectGlobal } from "@emotion/css"
 
-injectGlobal`
-:root {
-  --ring-color: blue;
-}
-
-* {
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans",
-    "Helvetica Neue", sans-serif;
-}
-
-*:focus {
-    outline: 2px dashed var(--ring-color);
-    outline-offset: 2px;
-}
-
-.root {
-  margin: 24px;
-}
-
-.pre {
-  background: rgba(0, 0, 0, 0.05);
-  padding: 20px;
-  margin-bottom: 40px;
-  border-radius: 10px;
-  font-family: monospace;
-}
-`
+injectGlobal({
+  ":root": {
+    "--ring-color": "blue",
+  },
+  "*": {
+    fontFamily:
+      '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans",\n    "Helvetica Neue", sans-serif',
+  },
+  "*:focus": {
+    outline: "2px dashed var(--ring-color)",
+    outlineOffset: "2px",
+  },
+  ".root": { margin: "24px" },
+  ".pre": {
+    background: "rgba(0, 0, 0, 0.05)",
+    padding: "20px",
+    marginBottom: "40px",
+    borderRadius: "10px",
+    fontFamily: "monospace",
+  },
+})
