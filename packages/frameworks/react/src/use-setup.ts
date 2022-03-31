22abf502fc0ddd1a15796624f591b34b6c2f47c9
Segun Adebayo <joseshegs@gmail.com>
2022-03-31 13:38:30 +0400

    revert: use-setup

--- packages/frameworks/react/src/use-setup.ts
+++ packages/frameworks/react/src/use-setup.ts
@@ -1,18 +1,16 @@
 import type { StateMachine as S } from "@ui-machines/core"
-import { useEffect, useLayoutEffect, useRef } from "react"
+import { useEffect, useRef } from "react"
 
 export type UseSetupProps = {
   send: (evt: S.Event<S.AnyEventObject>) => void
   id: string
 }
 
-const useSafeLayoutEffect = typeof document !== "undefined" ? useLayoutEffect : useEffect
-
 export function useSetup<T extends HTMLElement = HTMLDivElement>(props: UseSetupProps) {
   const { send, id } = props
   const ref = useRef<T>(null)
 
-  useSafeLayoutEffect(() => {
+  useEffect(() => {
     Promise.resolve().then(() => {
       const doc = ref.current?.ownerDocument
       send({ type: "SETUP", doc, id })
