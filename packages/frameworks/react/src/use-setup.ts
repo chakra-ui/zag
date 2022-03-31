1253d6494421a1ee9f19535d672693a487591e6a
Segun Adebayo <joseshegs@gmail.com>
2022-03-31 13:25:29 +0400

    chore: move setup to layout effect time

--- packages/frameworks/react/src/use-setup.ts
+++ packages/frameworks/react/src/use-setup.ts
@@ -1,16 +1,18 @@
 import type { StateMachine as S } from "@ui-machines/core"
-import { useEffect, useRef } from "react"
+import { useEffect, useLayoutEffect, useRef } from "react"
 
 export type UseSetupProps = {
   send: (evt: S.Event<S.AnyEventObject>) => void
   id: string
 }
 
+const useSafeLayoutEffect = typeof document !== "undefined" ? useLayoutEffect : useEffect
+
 export function useSetup<T extends HTMLElement = HTMLDivElement>(props: UseSetupProps) {
   const { send, id } = props
   const ref = useRef<T>(null)
 
-  useEffect(() => {
+  useSafeLayoutEffect(() => {
     Promise.resolve().then(() => {
       const doc = ref.current?.ownerDocument
       send({ type: "SETUP", doc, id })
