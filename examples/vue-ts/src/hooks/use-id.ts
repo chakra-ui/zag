3400bff4f8a020851c800451f29846a0a14d1df9
Segun Adebayo <joseshegs@gmail.com>
2022-04-16 18:48:00 +0400

    refactor: vue useid hook

--- examples/vue-ts/src/hooks/use-id.ts
+++ examples/vue-ts/src/hooks/use-id.ts
@@ -6,12 +6,9 @@ const genId = () => ++_id
 
 /**
  * Generates a unique id
- *
- * @param id external ID provided by consumer/user.
- * @param prefix prefix to append before the id
  */
-export const useId = (id?: string, prefix?: string) => {
-  const initialId = id || (serverHandoffComplete ? genId() : null)
+export const useId = () => {
+  const initialId = serverHandoffComplete ? genId() : null
   const uid = ref(initialId)
 
   onBeforeMount(() => {
@@ -28,17 +25,6 @@ export const useId = (id?: string, prefix?: string) => {
 
   return computed(() => {
     const __id__ = uid.value !== null ? uid.value.toString() : undefined
-    return (prefix ? `${prefix}-${__id__}` : __id__) as string
+    return `v:${__id__}`
   })
-}
-
-/**
- * Hook to generate ids for use in compound components
- *
- * @param id the external id passed from the user
- * @param prefixes array of prefixes to use
- */
-export function useIds(id?: string, ...prefixes: string[]) {
-  const __id__ = useId(id)
-  return prefixes.map((prefix) => computed(() => `${prefix}-${__id__.value}`))
 }
