83972339360b6440c150c4868a898141cf26cf6f
Segun Adebayo <joseshegs@gmail.com>
2022-04-17 17:21:48 +0400

    chore: rm unused machine api

--- packages/core/src/machine.ts
+++ packages/core/src/machine.ts
@@ -324,18 +324,8 @@ export class Machine<
     return new Machine({ ...this.config, context: newContext }, this.options)
   }
 
-  withOptions = (options: Partial<S.MachineOptions<TContext, TState, TEvent>>) => {
-    this.detachComputed()
-    return new Machine(this.config, { ...this.options, ...options })
-  }
-
   setActions = (actions: Partial<S.MachineOptions<TContext, TState, TEvent>>["actions"]) => {
     this.actionMap = { ...this.actionMap, ...actions }
-  }
-
-  clone = () => {
-    this.detachComputed()
-    return new Machine(this.config, this.options)
   }
 
   private getStateNode = (state: TState["value"] | null) => {
