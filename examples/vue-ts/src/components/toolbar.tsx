a45bbe3394551b3f7e5d9a1a53c70e100dab5289
anubra266 <anubra266@gmail.com>
2022-04-30 14:27:39 +0100

    chore: add toolbar

---
+++ examples/vue-ts/src/components/toolbar.tsx
@@ -0,0 +1,55 @@
+import { dataAttr } from "@zag-js/dom-utils"
+import { defineComponent, h, ref, defineProps } from "vue"
+import { getToolbarStyles } from "../../../../shared/style"
+
+type ToolbarProps = {
+  controls: null | (() => JSX.Element)
+  visualizer: null | JSX.Element
+  count?: number
+}
+
+export const Toolbar = defineComponent({
+  props: ["count", "controls", "visualizer"],
+  setup(props) {
+    const activeState = ref(props.controls === null ? 1 : 0)
+
+    const style = getToolbarStyles(props.count ?? 1)
+
+    return () => {
+      return (
+        <div class={style}>
+          <nav>
+            {props.controls !== null && (
+              <button
+                data-active={dataAttr(activeState.value === 0)}
+                onClick={() => {
+                  activeState.value = 0
+                }}
+              >
+                Controls
+              </button>
+            )}
+            <button
+              data-active={dataAttr(activeState.value === 1)}
+              onClick={() => {
+                activeState.value = 1
+              }}
+            >
+              Visualizer
+            </button>
+          </nav>
+          <div>
+            {props.controls !== null && (
+              <div data-content data-active={dataAttr(activeState.value === 0)}>
+                <props.controls />
+              </div>
+            )}
+            <div data-content data-active={dataAttr(activeState.value === 1)}>
+              {props.visualizer}
+            </div>
+          </div>
+        </div>
+      )
+    }
+  },
+})
