d6f132bd4f8f56187c24ff9236f1043eaf411b81
anubra266 <anubra266@gmail.com>
2022-05-05 21:54:34 +0100

    feat: remove imports

---
+++ scripts/src/commands/visualize.ts
@@ -0,0 +1,46 @@
+import fs from "fs"
+import generate from "@babel/generator"
+import traverse from "@babel/traverse"
+import * as t from "@babel/types"
+
+import { createLogger } from "../utilities/log"
+import { getMachinePackages } from "../utilities/packages"
+
+const parser = require("@babel/parser")
+
+const logger = createLogger("visualize")
+
+const DISALLOWED_PROPERTIES = ["context", "computed", "watch"]
+
+type VisualizeOpts = {
+  outFile?: string
+}
+
+export default async function visualize(component: string, opts: VisualizeOpts) {
+  const { outFile } = opts
+  const modules = await getMachinePackages()
+  const componentModule = modules.find((module) => module.dir.endsWith(component))
+  const machineFile = `${componentModule?.dir}/src/${component}.machine.ts`
+  //   const code = fs.readFileSync(machineFile, "utf8")
+  const code = fs.readFileSync("./test.ts", "utf8")
+
+  const ast = parser.parse(code, {
+    sourceType: "module",
+    plugins: ["typescript"],
+  })
+
+  //store machine config so we can ignore all other keys
+  let machineObj = null
+
+  traverse(ast, {
+    ImportDeclaration: function (path) {
+      path.remove()
+    },
+  })
+
+  if (machineObj && outFile) {
+    const machineObjOutput = generate(machineObj, {}, code)
+    fs.writeFileSync(outFile, machineObjOutput.code)
+    logger.success(`${component} machine visualization complete. 😎`)
+  }
+}
