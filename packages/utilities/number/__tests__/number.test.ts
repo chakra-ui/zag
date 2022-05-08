524905d29f19f0724a8d57f09dcc8f3eb7085074
Segun Adebayo <segun.adebayo@vercel.com>
2022-05-08 22:37:37 +0400

    test: update tests

--- packages/utilities/number/__tests__/number.test.ts
+++ packages/utilities/number/__tests__/number.test.ts
@@ -1,12 +1,24 @@
 import { countDecimals, roundToPrecision } from ".."
 
-test("should round to precision", () => {
-  expect(
-    roundToPrecision(12.345678, {
-      precision: 2,
-      step: 1,
-    }),
-  ).toBe("12.35")
+describe("roundToPrecision", () => {
+  test("with unit step", () => {
+    expect(
+      roundToPrecision(12.345678, {
+        precision: 2,
+        step: 1,
+      }),
+    ).toBe("12.35")
+  })
+
+  test("with fractional step", () => {
+    expect(
+      // will use the maximum of step decimals and precision
+      roundToPrecision(12.345678, {
+        precision: 2,
+        step: 0.001,
+      }),
+    ).toBe("12.346")
+  })
 })
 
 test("should count decimals", () => {
