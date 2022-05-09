996e9e7999288bcaa1ab4b53a27f6cbee62b2fed
Segun Adebayo <segun.adebayo@vercel.com>
2022-05-09 10:55:49 +0400

    chore: update number format

--- packages/utilities/number/src/number-format.ts
+++ packages/utilities/number/src/number-format.ts
@@ -1,4 +1,4 @@
-export const nf = new Intl.NumberFormat("en-US", { style: "decimal" })
+export const nf = new Intl.NumberFormat("en-US", { style: "decimal", maximumFractionDigits: 20 })
 
 export function formatter(n: number) {
   return parseFloat(nf.format(n))
