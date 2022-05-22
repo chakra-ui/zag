eafec246b5dfb0c9f4cc421974a8bfa651fe81f0
Segun Adebayo <joseshegs@gmail.com>
2022-05-22 21:03:45 +0400

    feat: add fieldset monitoring utils

--- packages/utilities/dom/src/form.ts
+++ packages/utilities/dom/src/form.ts
@@ -1,17 +1,30 @@
-export function getClosestFormElement(el: HTMLElement) {
+import { observeAttributes } from "./mutation-observer"
+
+export function getClosestForm(el: HTMLElement) {
   if (isFormElement(el)) return el.form
   else return el.closest("form")
 }
 
 function isFormElement(el: HTMLElement): el is HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement {
-  return ["textarea", "input", "select", "button"].includes(el.localName)
+  return el.matches("textarea, input, select, button")
 }
 
 export function trackFormReset(el: HTMLElement | null | undefined, callback: () => void) {
   if (!el) return
-  const form = getClosestFormElement(el)
+  const form = getClosestForm(el)
   form?.addEventListener("reset", callback, { passive: true })
   return () => {
     form?.removeEventListener("reset", callback)
   }
+}
+
+export function trackFieldsetDisabled(el: HTMLElement | null | undefined, callback: (disabled: boolean) => void) {
+  const fieldset = el?.closest("fieldset")
+  if (!fieldset) return
+  callback(fieldset.disabled)
+  return observeAttributes(fieldset, ["disabled"], () => callback(fieldset.disabled))
+}
+
+export function isNativeDisabled(el: HTMLElement) {
+  return el.matches(":disabled")
 }
