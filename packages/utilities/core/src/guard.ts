a9e8b34dcfb2c273577c004c12458f9b07bdfa2b
Segun Adebayo <joseshegs@gmail.com>
2022-05-22 10:02:23 +0400

    refactor: guard pointer assertions

--- packages/utilities/core/src/guard.ts
+++ packages/utilities/core/src/guard.ts
@@ -25,8 +25,9 @@ export const supportsMouseEvent = () => isDom() && window.onmousedown === null
 
 export const isMouseEvent = (v: any): v is MouseEvent => isObject(v) && "button" in v
 export const isTouchEvent = (v: any): v is TouchEvent => isObject(v) && "touches" in v
-export const isLeftClick = (v: MouseEvent | PointerEvent) => v.button === 0
-export const isRightClick = (v: MouseEvent | PointerEvent) => v.button === 2
+export const isLeftClick = (v: { button: number }) => v.button === 0
+export const isRightClick = (v: { button: number }) => v.button === 2
 export const isModifiedEvent = (v: Pick<KeyboardEvent, "ctrlKey" | "metaKey" | "altKey">) =>
   v.ctrlKey || v.altKey || v.metaKey
-export const isCtrlKey = (v: KeyboardEvent) => (isMac() ? v.metaKey && !v.ctrlKey : v.ctrlKey && !v.metaKey)
+export const isCtrlKey = (v: Pick<KeyboardEvent, "ctrlKey" | "metaKey">) =>
+  isMac() ? v.metaKey && !v.ctrlKey : v.ctrlKey && !v.metaKey
