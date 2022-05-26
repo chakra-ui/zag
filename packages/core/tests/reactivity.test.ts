c236c71aa37fb35e04b68c84d1e74856ab4bf587
Segun Adebayo <segun.adebayo@vercel.com>
2022-05-26 16:11:04 +0400

    test: add tests for computed and watch

---
+++ packages/core/tests/reactivity.test.ts
@@ -0,0 +1,56 @@
+import { createMachine } from ".."
+
+type Context = {
+  values: string[]
+  readonly length: number
+}
+
+type State = {
+  value: "idle"
+}
+
+const watchers = {
+  length: jest.fn(),
+  values: jest.fn(),
+}
+
+const service = createMachine<Context, State>({
+  context: {
+    values: [],
+  },
+  computed: {
+    length: (ctx) => ctx.values.length,
+  },
+  watch: {
+    values: watchers.values,
+    length: watchers.length,
+  },
+  on: {
+    ADD: {
+      actions: (ctx) => ctx.values.push("foo"),
+    },
+  },
+})
+
+beforeEach(() => {
+  service.start()
+  service.send("ADD")
+})
+
+afterEach(() => {
+  service.stop()
+  watchers.length.mockReset()
+  watchers.values.mockReset()
+})
+
+test("[computed] should compute context values", () => {
+  expect(service.state.context.length).toBe(1)
+})
+
+test("[watch] should watch mutable context value", () => {
+  expect(watchers.length).toHaveBeenCalledTimes(1)
+})
+
+test("[watch] should watch mutable context value", () => {
+  expect(watchers.values).toHaveBeenCalledTimes(1)
+})
