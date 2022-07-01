fd1710a0472201b58cd3167a7a6320f94d15a7d3
Segun Adebayo <joseshegs@gmail.com>
2022-07-01 19:18:38 +0400

    chore: update

--- packages/machines/popover/src/popover.connect.ts
+++ packages/machines/popover/src/popover.connect.ts
@@ -1,7 +1,6 @@
-import { dataAttr, isPortalEvent } from "@zag-js/dom-utils"
+import { dataAttr } from "@zag-js/dom-utils"
 import { getPlacementStyles } from "@zag-js/popper"
 import type { NormalizeProps, PropTypes } from "@zag-js/types"
-import { isSafari } from "@zag-js/utils"
 import { dom } from "./popover.dom"
 import type { Send, State } from "./popover.types"
 
