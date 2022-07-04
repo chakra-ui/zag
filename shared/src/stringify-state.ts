d393a2d9813ff99aab59a2babf1acd665a599235
Segun Adebayo <joseshegs@gmail.com>
2022-07-04 14:59:53 +0400

    fix: vue example

--- shared/src/stringify-state.ts
+++ shared/src/stringify-state.ts
@@ -4,16 +4,20 @@ export function stringifyState(state: Record<string, any>) {
   const code = JSON.stringify(
     state,
     (_k, v) => {
-      switch (v?.toString()) {
-        case "[object Machine]":
-          const id = v.state.context.uid ?? v.id
-          return `Machine: ${id}`
-        case "[object ShadowRoot]":
-          return "#shadow-root"
-        case "[object HTMLDocument]":
-          return "#document"
-        default:
-          return v !== null && typeof v === "object" && v.nodeType === 1 ? v.tagName : v
+      try {
+        switch (v?.toString()) {
+          case "[object Machine]":
+            const id = v.state.context.uid ?? v.id
+            return `Machine: ${id}`
+          case "[object ShadowRoot]":
+            return "#shadow-root"
+          case "[object HTMLDocument]":
+            return "#document"
+          default:
+            return v !== null && typeof v === "object" && v.nodeType === 1 ? v.tagName : v
+        }
+      } catch {
+        return v
       }
     },
     4,
