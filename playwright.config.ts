f61c3d35528cf736ff748e5caea49ced2c70a33d
anubra266 <anubra266@gmail.com>
2022-07-14 13:29:52 +0100

    chore: fix test results paths

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { PlaywrightTestConfig } from "@playwright/test"
+import { PlaywrightTestConfig, ReporterDescription } from "@playwright/test"
 
 const config: PlaywrightTestConfig = {
   testDir: "./e2e",
@@ -9,12 +9,19 @@ const config: PlaywrightTestConfig = {
     timeout: 5_000,
   },
   forbidOnly: !!process.env.CI,
+  reporter: [
+    process.env.CI ? ["list"] : ["line"],
+    process.env.CI ? ["junit", { outputFile: "e2e/junit.xml" }] : null,
+    ["html", { outputFolder: "e2e/report", open: "never" }],
+  ].filter(Boolean) as ReporterDescription[],
   retries: process.env.CI ? 2 : 0,
   use: {
-    trace: "on-first-retry",
+    trace: "retain-on-failure",
     baseURL: process.env.PLAYWRIGHT_BASE_URL || "http://localhost:3000",
+    screenshot: "only-on-failure",
     video: "retain-on-failure",
     locale: "en-US",
+    timezoneId: "GMT",
   },
 }
 export default config
