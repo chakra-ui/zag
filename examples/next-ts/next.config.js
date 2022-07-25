e908f17bd18dd8c7f5a8c68896526109e1f71b44
Segun Adebayo <joseshegs@gmail.com>
2022-07-25 09:55:46 +0400

    chore: update example config

--- examples/next-ts/next.config.js
+++ examples/next-ts/next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
   experimental: {
     externalDir: true,
   },
