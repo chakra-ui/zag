5e981508bbd262cbbfd8289900ec4e999c3d480e
Abraham <anubra266@gmail.com>
2022-09-13 06:25:54 +0100

    fix anatomy ids (#315)

---
+++ .changeset/early-news-explode.md
@@ -0,0 +1,5 @@
+---
+"@zag-js/hover-card": patch
+---
+
+Fix anatomy ids
--- packages/machines/hover-card/src/hover-card.dom.ts
+++ packages/machines/hover-card/src/hover-card.dom.ts
@@ -2,10 +2,10 @@ import { defineDomHelpers } from "@zag-js/dom-utils"
 import type { MachineContext as Ctx } from "./hover-card.types"
 
 export const dom = defineDomHelpers({
-  getTriggerId: (ctx: Ctx) => ctx.ids?.trigger ?? `popover:${ctx.id}:trigger`,
-  getContentId: (ctx: Ctx) => ctx.ids?.content ?? `popover:${ctx.id}:content`,
-  getPositionerId: (ctx: Ctx) => `popover:${ctx.id}:popper`,
-  getArrowId: (ctx: Ctx) => `popover:${ctx.id}:arrow`,
+  getTriggerId: (ctx: Ctx) => ctx.ids?.trigger ?? `hover-card:${ctx.id}:trigger`,
+  getContentId: (ctx: Ctx) => ctx.ids?.content ?? `hover-card:${ctx.id}:content`,
+  getPositionerId: (ctx: Ctx) => `hover-card:${ctx.id}:popper`,
+  getArrowId: (ctx: Ctx) => `hover-card:${ctx.id}:arrow`,
 
   getTriggerEl: (ctx: Ctx) => dom.getById(ctx, dom.getTriggerId(ctx)),
   getContentEl: (ctx: Ctx) => dom.getById(ctx, dom.getContentId(ctx)),
