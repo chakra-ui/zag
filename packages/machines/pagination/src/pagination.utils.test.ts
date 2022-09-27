d27232b6888c9a8216693864296cb388f3b1f50f
anubra266 <anubra266@gmail.com>
2022-09-27 17:13:03 +0100

    chore: use snapshots for tests

--- packages/machines/pagination/src/pagination.utils.test.ts
+++ packages/machines/pagination/src/pagination.utils.test.ts
@@ -2,16 +2,42 @@ import { utils } from "./pagination.utils"
 
 describe("@zag-js/pagination utils", () => {
   test("range method", () => {
-    const range = utils.range(1, 5)
-    expect(range[0]).toEqual(1)
-    expect(range[4]).toEqual(5)
-    expect(range.length).toEqual(5)
+    expect(utils.range(1, 5)).toMatchInlineSnapshot(`
+    [
+      1,
+      2,
+      3,
+      4,
+      5,
+    ]
+  `)
   })
 
   test("transform method", () => {
     const items = [1, 2, "ellipsis", 3, 4]
     const transformed = utils.transform(items)
-    expect(transformed[0].type).toEqual("page")
-    expect(transformed[2].type).toEqual("ellipsis")
+    expect(transformed).toMatchInlineSnapshot(`
+    [
+      {
+        "type": "page",
+        "value": 1,
+      },
+      {
+        "type": "page",
+        "value": 2,
+      },
+      {
+        "type": "ellipsis",
+      },
+      {
+        "type": "page",
+        "value": 3,
+      },
+      {
+        "type": "page",
+        "value": 4,
+      },
+    ]
+  `)
   })
 })
