c56a73dc0011f194940793082ece9383fdd1f437
anubra266 <anubra266@gmail.com>
2022-10-06 13:59:47 +0100

    fix(editable): make component uncontrolled

--- .changeset/nine-cups-whisper.md
+++
@@ -1,5 +0,0 @@
----
-"@zag-js/form-utils": patch
----
-
-Add `setInputValue` method and refactor `dispatchInputValueEvent` method
--- packages/utilities/form-utils/src/input-event.ts
+++ packages/utilities/form-utils/src/input-event.ts
@@ -11,21 +11,15 @@ function getDescriptor(el: HTMLElement, options: DescriptorOptions) {
   return Object.getOwnPropertyDescriptor(proto, property) ?? {}
 }
 
-export function setInputValue(el: HTMLInputElement | null, value: string | number) {
-  if (!el) return
-
-  // set property value
-  const desc = getDescriptor(el, { type: "HTMLInputElement", property: "value" })
-  desc.set?.call(el, value)
-}
-
 export function dispatchInputValueEvent(el: HTMLElement | null, value: string | number) {
   if (!el) return
 
   const win = getWindow(el)
   if (!(el instanceof win.HTMLInputElement)) return
 
-  setInputValue(el, value)
+  // set property value
+  const desc = getDescriptor(el, { type: "HTMLInputElement", property: "value" })
+  desc.set?.call(el, value)
 
   // dispatch input event
   const event = new win.Event("input", { bubbles: true })
