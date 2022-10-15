9b2333ae8745a91b7664fb08a0939abe7e3298a0
Segun Adebayo <joseshegs@gmail.com>
2022-10-15 11:07:34 +0400

    refactor: event helper

--- packages/utilities/dom/src/query.ts
+++ packages/utilities/dom/src/query.ts
@@ -94,11 +94,14 @@ export function defineDomHelpers<T>(helpers: T) {
         throw new Error("[dom/create-emit] Expected an element but got undefined")
       }
 
-      return (evt: string, detail: Record<string, any>, options?: Omit<CustomEventInit<EventInit>, "detail">) => {
-        const event = new win.CustomEvent(`zag:${ns}:${evt}`, {
+      return (evt: string, detail: Record<string, any>, options?: EventInit) => {
+        const { bubbles = true, cancelable } = options ?? {}
+        const eventName = `zag:${ns}:${evt}`
+        const event = new win.CustomEvent(eventName, {
           detail,
-          bubbles: options?.bubbles || true,
-          cancelable: options?.cancelable,
+          bubbles,
+          cancelable,
+          composed: true,
         })
         target.dispatchEvent(event)
       }
