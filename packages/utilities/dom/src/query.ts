731ea128964af197fbc70be30c2e8dba9acd58f3
anubra266 <anubra266@gmail.com>
2022-10-18 14:07:44 +0100

    fix(dom-utils): remove redundant dom helpers

--- packages/utilities/dom/src/query.ts
+++ packages/utilities/dom/src/query.ts
@@ -76,43 +76,6 @@ export function getParent(el: HTMLElement): HTMLElement {
   return el.assignedSlot || el.parentElement || doc.documentElement
 }
 
-type Ctx = {
-  getRootNode?: () => Document | ShadowRoot | Node
-}
-
-export function defineDomHelpers<T>(helpers: T) {
-  const dom = {
-    getRootNode: (ctx: Ctx) => (ctx.getRootNode?.() ?? document) as Document | ShadowRoot,
-    getDoc: (ctx: Ctx) => getDocument(dom.getRootNode(ctx)),
-    getWin: (ctx: Ctx) => dom.getDoc(ctx).defaultView ?? window,
-    getActiveElement: (ctx: Ctx) => dom.getDoc(ctx).activeElement as HTMLElement | null,
-    getById: <T = HTMLElement>(ctx: Ctx, id: string) => dom.getRootNode(ctx).getElementById(id) as T | null,
-    createEmitter: (ctx: Ctx, target: HTMLElement) => {
-      const win = dom.getWin(ctx)
-      return function emit(evt: string, detail: Record<string, any>, options?: EventInit) {
-        const { bubbles = true, cancelable, composed = true } = options ?? {}
-        const eventName = `zag:${evt}`
-        const init: CustomEventInit = { bubbles, cancelable, composed, detail }
-        const event = new win.CustomEvent(eventName, init)
-        target.dispatchEvent(event)
-      }
-    },
-    createListener: (target: HTMLElement) => {
-      return function listen<T = any>(evt: string, handler: (e: CustomEvent<T>) => void) {
-        const eventName = `zag:${evt}`
-        const listener: any = (e: CustomEvent) => handler(e)
-        target.addEventListener(eventName, listener)
-        return () => target.removeEventListener(eventName, listener)
-      }
-    },
-  }
-
-  return {
-    ...dom,
-    ...helpers,
-  }
-}
-
 export function contains(
   parent: HTMLElement | EventTarget | null | undefined,
   child: HTMLElement | EventTarget | null,
