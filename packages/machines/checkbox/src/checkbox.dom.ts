e5f25ce503905073c8d009079bca7544fc74ce1c
anubra266 <anubra266@gmail.com>
2022-10-18 14:26:01 +0100

    fix(checkbox): update events api

--- packages/machines/checkbox/src/checkbox.dom.ts
+++ packages/machines/checkbox/src/checkbox.dom.ts
@@ -1,7 +1,7 @@
-import { defineDomHelpers } from "@zag-js/dom-utils"
+import { defineHelpers } from "@zag-js/dom-query"
 import type { EventMap, MachineContext as Ctx } from "./checkbox.types"
 
-export const dom = defineDomHelpers({
+export const dom = defineHelpers({
   getRootId: (ctx: Ctx) => ctx.ids?.root ?? `checkbox:${ctx.id}`,
   getLabelId: (ctx: Ctx) => ctx.ids?.label ?? `checkbox:${ctx.id}:label`,
   getControlId: (ctx: Ctx) => ctx.ids?.control ?? `checkbox:${ctx.id}:control`,
@@ -14,6 +14,6 @@ export const dom = defineDomHelpers({
   },
   getInputEl: (ctx: Ctx) => dom.getById<HTMLInputElement>(ctx, dom.getInputId(ctx)),
 
-  emitter: (ctx: Ctx) => dom.createEmitter(ctx, dom.getRootEl(ctx)),
-  listener: (ctx: Ctx) => dom.createListener(dom.getRootEl(ctx)),
+  emitter: (ctx: Ctx) => dom.createEmitter<EventMap>(ctx, dom.getRootEl(ctx)),
+  listener: (ctx: Ctx) => dom.createListener<EventMap>(dom.getRootEl(ctx)),
 })
