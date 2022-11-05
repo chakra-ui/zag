2f122739601961ef69303c4c7ad62f735b69de9f
Segun Adebayo <joseshegs@gmail.com>
2022-11-05 10:11:23 +0400

    feat: add split utils

--- packages/utilities/core/src/object.ts
+++ packages/utilities/core/src/object.ts
@@ -10,3 +10,16 @@ export function compact<T>(value: T) {
   }
   return value
 }
+
+export function split<T, K extends keyof T>(value: T, keys: K[]): [Pick<T, K>, Omit<T, K>] {
+  const left = {} as any
+  const right = {} as any
+  for (const key in value) {
+    if (keys.includes(key as any)) {
+      left[key] = value[key]
+    } else {
+      right[key] = value[key]
+    }
+  }
+  return [left, right]
+}
