2a816a3a324efcc6ed53e803f9edea517e15b2db
Segun Adebayo <joseshegs@gmail.com>
2022-11-18 15:56:21 +0400

    refactor: select utils

--- packages/machines/select/src/select.utils.ts
+++ packages/machines/select/src/select.utils.ts
@@ -1,21 +1,22 @@
-export const a11yMessages = {
-  onOpen(options: { count: number; prevCount: number }): string {
-    const { count, prevCount } = options
+import { isObject } from "@zag-js/utils"
+import { Option } from "./select.types"
 
-    if (!count) {
-      return "No results are available."
-    }
+export function isOptionData(v: any): v is Option {
+  return isObject(v) && "value" in v && "label" in v
+}
 
-    if (count !== prevCount) {
-      return `${count} result${
-        count === 1 ? " is" : "s are"
-      } available, use up and down arrow keys to navigate. Press Enter or Space Bar keys to select.`
-    }
+export function validateOptionData(v: any): asserts v is Option {
+  if (!isOptionData(v)) {
+    throw new TypeError("Expected an option data object")
+  }
+}
 
-    return ""
-  },
-  onHighlight(options: { label: number }): string {
-    const { label } = options
-    return `${label} selected`
-  },
+export function highlightMessage(label: string): string {
+  return `${label} selected`
+}
+
+export function openMessage(count: number): string {
+  return `${count} result${
+    count === 1 ? " is" : "s are"
+  } available, use up and down arrow keys to navigate. Press Enter or Space Bar keys to select.`
 }
