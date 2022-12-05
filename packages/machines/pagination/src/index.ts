b487700dc923e0bb8d6d356d5e75b7c7c5b07644
Christian Schröter <dev@grizzly.codes>
2022-12-05 14:15:34 +0100

    chore: pagination

--- packages/machines/pagination/src/index.ts
+++ packages/machines/pagination/src/index.ts
@@ -1,3 +1,4 @@
+export { anatomy } from "./pagination.anatomy"
 export { connect } from "./pagination.connect"
 export { machine } from "./pagination.machine"
 export type { UserDefinedContext as Context } from "./pagination.types"
--- packages/machines/pagination/src/pagination.anatomy.ts
+++ packages/machines/pagination/src/pagination.anatomy.ts
@@ -1,3 +1,4 @@
 import { createAnatomy } from "@zag-js/anatomy"
 
 export const anatomy = createAnatomy("pagination").parts("root", "item", "ellipsis", "prevItem", "nextItem")
+export const parts = anatomy.build()
