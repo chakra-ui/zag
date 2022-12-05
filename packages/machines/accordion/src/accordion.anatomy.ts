3bed29ee906793aae74e61c0a3abbd5c5b04845b
Christian Schröter <dev@grizzly.codes>
2022-12-05 14:00:53 +0100

    chore: accordioN

--- packages/machines/accordion/src/accordion.anatomy.ts
+++ packages/machines/accordion/src/accordion.anatomy.ts
@@ -1,3 +1,4 @@
 import { createAnatomy } from "@zag-js/anatomy"
 
 export const anatomy = createAnatomy("accordion").parts("root", "item", "trigger", "content")
+export const parts = anatomy.build()
