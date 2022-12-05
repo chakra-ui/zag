5b19fdbca276eaa0c6bdbfb1437e32041656eac2
Christian Schröter <dev@grizzly.codes>
2022-12-05 14:38:28 +0100

    chore: tabs

--- packages/machines/tabs/src/index.ts
+++ packages/machines/tabs/src/index.ts
@@ -1,3 +1,4 @@
+export { anatomy } from "./tabs.anatomy"
 export { connect } from "./tabs.connect"
 export { machine } from "./tabs.machine"
 export type { UserDefinedContext as Context } from "./tabs.types"
