09cc3e77cfdfe577d5b53c2a06308acd316ef004
Christian Schröter <dev@grizzly.codes>
2022-12-05 14:20:04 +0100

    chore: popover

--- packages/machines/popover/src/index.ts
+++ packages/machines/popover/src/index.ts
@@ -1,3 +1,4 @@
+export { anatomy } from "./popover.anatomy"
 export { connect } from "./popover.connect"
 export { machine } from "./popover.machine"
 export type { UserDefinedContext as Context } from "./popover.types"
--- packages/machines/popover/src/popover.anatomy.ts
+++ packages/machines/popover/src/popover.anatomy.ts
@@ -1,12 +1,14 @@
 import { createAnatomy } from "@zag-js/anatomy"
 
 export const anatomy = createAnatomy("popover").parts(
+  "arrow",
+  "arrowInner",
+  "anchor",
   "trigger",
   "positioner",
   "content",
   "title",
   "description",
   "closeButton",
-  "arrow",
-  "anchor",
 )
+export const parts = anatomy.build()
