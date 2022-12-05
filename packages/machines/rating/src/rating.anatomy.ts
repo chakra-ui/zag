7e9845370b339869b262f991da316130873a4666
Christian Schröter <dev@grizzly.codes>
2022-12-05 14:26:30 +0100

    chore: rating

--- packages/machines/rating/src/rating.anatomy.ts
+++ packages/machines/rating/src/rating.anatomy.ts
@@ -1,3 +1,4 @@
 import { createAnatomy } from "@zag-js/anatomy"
 
-export const anatomy = createAnatomy("rating").parts("root", "label", "item", "itemGroup", "input")
+export const anatomy = createAnatomy("rating").parts("root", "input", "label", "item", "itemGroup")
+export const parts = anatomy.build()
