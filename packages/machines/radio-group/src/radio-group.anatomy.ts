07d84b73ea194dff396dc48df4285ec282ee0213
anubra266 <anubra266@gmail.com>
2022-12-08 15:57:44 +0100

    feat: rework `item` to `radio`

--- packages/machines/radio-group/src/radio-group.anatomy.ts
+++ packages/machines/radio-group/src/radio-group.anatomy.ts
@@ -3,7 +3,7 @@ import { createAnatomy } from "@zag-js/anatomy"
 export const anatomy = createAnatomy("radio-group").parts(
   "root",
   "label",
-  "item", // TODO rename to radio
+  "radio",
   "itemLabel", // TODO rename to radioLabel
   "itemControl", // TODO rename to radioControl
   "itemInput", // TODO rename to radioInput
