eff42501dd2ec7b243b2d496bca6111a6d78d610
Segun Adebayo <joseshegs@gmail.com>
2022-12-21 21:31:50 +0400

    chore: add explicit type for range-slider

--- packages/anatomy/src/index.ts
+++ packages/anatomy/src/index.ts
@@ -1,2 +1,2 @@
 export { createAnatomy } from "./create-anatomy"
-export type { Anatomy, AnatomyPart } from "./create-anatomy"
+export type { Anatomy, AnatomyPart, AnatomyInstance, AnatomyPartName } from "./create-anatomy"
