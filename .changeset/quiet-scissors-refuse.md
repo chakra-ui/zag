063bbc40a2674049a343226a42af2ed1d2b7e884
Segun Adebayo <joseshegs@gmail.com>
2022-12-28 20:58:13 +0400

    feat(menu): navigate options with tab

---
+++ .changeset/quiet-scissors-refuse.md
@@ -0,0 +1,5 @@
+---
+"@zag-js/menu": patch
+---
+
+Support the use of `Tab` key press to navigate through the menu items.
--- e2e/menu.e2e.ts
+++ e2e/menu.e2e.ts
@@ -1,19 +1,33 @@
-import { expect, test } from "@playwright/test"
+import { expect, Page, test } from "@playwright/test"
 import { controls, testid } from "./__utils"
 
 const trigger = testid("trigger")
 const menu = testid("menu")
+
+const expectToBeFocused = async (page: Page, id: string) => {
+  return expect(page.locator(`[id=${id}]`).first()).toHaveAttribute("data-focus", "")
+}
 
 test.describe("menu", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("/menu")
   })
 
-  test("should note close menu when `closeOnSelect` is false [keyboard]", async ({ page }) => {
+  test("should stay open when `closeOnSelect` is false", async ({ page }) => {
     await controls(page).bool("closeOnSelect", false)
     await page.click(trigger)
     await page.keyboard.press("ArrowDown")
     await page.keyboard.press("Enter", { delay: 10 })
     await expect(page.locator(menu)).toBeVisible()
+  })
+
+  test("should navigate menu items with tab", async ({ page }) => {
+    await page.click(trigger)
+    await page.keyboard.press("Tab")
+    await page.keyboard.press("Tab")
+    await expectToBeFocused(page, "duplicate")
+    await page.keyboard.press("Tab")
+    await page.keyboard.press("Shift+Tab")
+    await expectToBeFocused(page, "duplicate")
   })
 })
