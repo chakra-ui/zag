dd0968c3f6172b2a4f1de87fd20f0dc289d6625c
Segun Adebayo <joseshegs@gmail.com>
2023-02-01 20:49:57 +0400

    chore: add controls for transition

--- examples/solid-ts/src/pages/transition.tsx
+++ examples/solid-ts/src/pages/transition.tsx
@@ -43,7 +43,7 @@ export default function Page() {
           </div>
         </div>
       </main>
-      <Toolbar>
+      <Toolbar controls={controls.ui}>
         <StateVisualizer state={state} />
       </Toolbar>
     </>
