c2a9756e3aced465317a292743fc60a093fa09d2
Jörg Bayreuther <jb@visualjerk.de>
2023-02-17 22:50:46 +0100

    chore: add tags input click outside test

---
+++ packages/machines/tags-input/src/tags-input.machine.test.ts
@@ -0,0 +1,49 @@
+import { UserDefinedContext } from "./tags-input.types"
+import { expect, describe, test, afterEach } from "vitest"
+import { machine } from "./tags-input.machine"
+import userEvent from "@testing-library/user-event"
+
+function clickOutside() {
+  return userEvent.click(document.body)
+}
+
+function createElement(tag: keyof HTMLElementTagNameMap, id: string) {
+  const el = document.createElement(tag)
+  document.body.append(el)
+  el.id = id
+  return el
+}
+
+function setupTest(userContext: Partial<UserDefinedContext> = {}) {
+  const input = createElement("input", "foo")
+  const { state, send } = machine({
+    id: "bar",
+    ids: {
+      input: input.id,
+    },
+    ...userContext,
+  })
+
+  function expectStateToBe(value: (typeof state)["value"]) {
+    expect(state.value).toBe(value)
+  }
+
+  return {
+    send,
+    expectStateToBe,
+  }
+}
+
+afterEach(() => {
+  document.body.innerHTML = ""
+})
+
+describe("@zag-js/tags-input machine", () => {
+  test("has idle state after click outside", async () => {
+    const { send, expectStateToBe } = setupTest()
+    send("FOCUS")
+    expectStateToBe("focused:input")
+    await clickOutside()
+    expectStateToBe("idle")
+  })
+})
