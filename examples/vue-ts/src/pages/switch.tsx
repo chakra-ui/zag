66f7ed9d88500d955e198376f84cb3f8cd501ce7
anubra266 <anubra266@gmail.com>
2023-03-04 14:13:55 +0100

    feat: add switch machine

---
+++ examples/vue-ts/src/pages/switch.tsx
@@ -0,0 +1,42 @@
+import * as zagSwitch from "@zag-js/switch"
+import { normalizeProps, useMachine } from "@zag-js/vue"
+import { computed, defineComponent, h } from "vue"
+import { switchControls } from "@zag-js/shared"
+import { StateVisualizer } from "../components/state-visualizer"
+import { Toolbar } from "../components/toolbar"
+import { useControls } from "../hooks/use-controls"
+
+export default defineComponent({
+  name: "switch",
+  setup() {
+    const controls = useControls(switchControls)
+
+    const [state, send] = useMachine(zagSwitch.machine({ id: "1" }), {
+      context: controls.context,
+    })
+
+    const apiRef = computed(() => zagSwitch.connect(state.value, send, normalizeProps))
+
+    return () => {
+      const api = apiRef.value
+
+      return (
+        <>
+          <main class="switch">
+            <label {...api.rootProps}>
+              <input type="checkbox" {...api.inputProps} />
+              <span {...api.controlProps}>
+                <span {...api.thumbProps} />
+              </span>
+              <span {...api.labelProps}>Feature is {api.isChecked ? "enabled" : "disabled"}</span>
+            </label>
+          </main>
+
+          <Toolbar controls={controls.ui}>
+            <StateVisualizer state={state} />
+          </Toolbar>
+        </>
+      )
+    }
+  },
+})
