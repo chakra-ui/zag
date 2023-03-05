4741eea34594cb790fae0b9b4752adfb50c97a17
Segun Adebayo <joseshegs@gmail.com>
2023-03-05 11:54:22 +0400

    chore: export anatomy

--- packages/machines/date-picker/src/index.ts
+++ packages/machines/date-picker/src/index.ts
@@ -1,3 +1,4 @@
+export { anatomy } from "./date-picker.anatomy"
 export { connect } from "./date-picker.connect"
 export { machine } from "./date-picker.machine"
 export type { UserDefinedContext as Context } from "./date-picker.types"
