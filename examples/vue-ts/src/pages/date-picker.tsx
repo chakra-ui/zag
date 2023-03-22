5fe03833b4966f42a2f7d1748aac46e74cd06455
Segun Adebayo <joseshegs@gmail.com>
2023-03-22 22:54:33 +0000

    chore: add vue datepicker

--- examples/vue-ts/src/pages/date-picker.tsx
+++ examples/vue-ts/src/pages/date-picker.tsx
@@ -1,3 +1,4 @@
+import { CalendarDate } from "@internationalized/date"
 import * as datePicker from "@zag-js/date-picker"
 import { normalizeProps, useMachine, mergeProps } from "@zag-js/vue"
 import { computed, defineComponent, h, Fragment } from "vue"
@@ -5,15 +6,25 @@ import { datePickerControls } from "@zag-js/shared"
 import { StateVisualizer } from "../components/state-visualizer"
 import { Toolbar } from "../components/toolbar"
 import { useControls } from "../hooks/use-controls"
+import { getYearsRange } from "@zag-js/date-utils"
 
 export default defineComponent({
   name: "datePicker",
   setup() {
     const controls = useControls(datePickerControls)
 
-    const [state, send] = useMachine(datePicker.machine({ id: "1" }), {
-      context: controls.context,
-    })
+    const [state, send] = useMachine(
+      datePicker.machine({
+        id: "1",
+        locale: "en",
+        selectionMode: "single",
+        max: new CalendarDate(2023, 4, 20),
+        min: new CalendarDate(2010, 1, 20),
+      }),
+      {
+        context: controls.context,
+      },
+    )
 
     const apiRef = computed(() => datePicker.connect(state.value, send, normalizeProps))
 
@@ -23,11 +34,153 @@ export default defineComponent({
       return (
         <>
           <main class="date-picker">
-            <div {...api.rootProps}></div>
+            <div>
+              <button>Outside Element</button>
+            </div>
+            <p>{`Visible range: ${api.visibleRangeText.formatted}`}</p>
+
+            <output class="date-output">
+              <div>Selected: {api.valueAsString ?? "-"}</div>
+              <div>Focused: {api.focusedValueAsString}</div>
+            </output>
+
+            <div {...api.controlProps}>
+              <input {...api.inputProps} />
+              <button {...api.clearTriggerProps}>❌</button>
+              <button {...api.triggerProps}>🗓</button>
+            </div>
+
+            <div {...api.contentProps}>
+              <div style={{ marginBlock: "20px" }}>
+                <select {...api.monthSelectProps}>
+                  {api.getMonths().map((month, i) => (
+                    <option key={i} value={i + 1} selected={api.focusedValue.month === i + 1}>
+                      {month}
+                    </option>
+                  ))}
+                </select>
+
+                <select {...api.yearSelectProps}>
+                  {getYearsRange({ from: 1_000, to: 4_000 }).map((year, i) => (
+                    <option key={i} value={year} selected={api.focusedValue.year === year}>
+                      {year}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <div hidden={api.view !== "day"} style={{ maxWidth: "230px" }}>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginBlock: "10px",
+                  }}
+                >
+                  <button {...api.getPrevTriggerProps()}>Prev</button>
+                  <button {...api.viewTriggerProps} style={{ border: "0", padding: "4px 20px", borderRadius: "4px" }}>
+                    {api.visibleRangeText.start}
+                  </button>
+                  <button {...api.getNextTriggerProps()}>Next</button>
+                </div>
+
+                <table {...api.getGridProps()}>
+                  <thead {...api.getHeaderProps()}>
+                    <tr>
+                      {api.weekDays.map((day, i) => (
+                        <th scope="col" key={i} aria-label={day.long}>
+                          {day.narrow}
+                        </th>
+                      ))}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {api.weeks.map((week, i) => (
+                      <tr key={i}>
+                        {week.map((value, i) => {
+                          if (value === null) return <td key={i} />
+                          return (
+                            <td key={i} {...api.getDayCellProps({ value })}>
+                              <div {...api.getDayCellTriggerProps({ value })}>{value.day}</div>
+                            </td>
+                          )
+                        })}
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+
+              <div style={{ display: "flex", gap: "40px", marginTop: "24px" }}>
+                <div hidden={api.view !== "month"}>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      marginBlock: "10px",
+                    }}
+                  >
+                    <button {...api.getPrevTriggerProps({ view: "month" })}>Prev</button>
+                    <span {...api.viewTriggerProps}>{api.visibleRange.start.year}</span>
+                    <button {...api.getNextTriggerProps({ view: "month" })}>Next</button>
+                  </div>
+
+                  <table {...api.getGridProps({ view: "month", columns: 4 })}>
+                    <tbody>
+                      {api.getMonths({ columns: 4, format: "short" }).map((months, row) => (
+                        <tr key={row}>
+                          {months.map((month, index) => {
+                            const value = row * 4 + index + 1
+                            return (
+                              <td key={index} {...api.getMonthCellProps({ value })}>
+                                <div {...api.getMonthCellTriggerProps({ value })}>{month}</div>
+                              </td>
+                            )
+                          })}
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+
+                <div hidden={api.view !== "year"}>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      marginBlock: "10px",
+                    }}
+                  >
+                    <button {...api.getPrevTriggerProps({ view: "year" })}>Prev</button>
+                    <span>
+                      {api.getDecade().start} - {api.getDecade().end}
+                    </span>
+                    <button {...api.getNextTriggerProps({ view: "year" })}>Next</button>
+                  </div>
+
+                  <table {...api.getGridProps({ view: "year", columns: 4 })}>
+                    <tbody>
+                      {api.getYears({ columns: 4 }).map((years, row) => (
+                        <tr key={row}>
+                          {years.map((year, index) => (
+                            <td colspan={4} key={index} {...api.getYearCellProps({ value: year })}>
+                              {year}
+                            </td>
+                          ))}
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
           </main>
 
-          <Toolbar controls={controls.ui}>
-            <StateVisualizer state={state} />
+          <Toolbar viz controls={controls.ui}>
+            <StateVisualizer state={state} omit={["weeks"]} />
           </Toolbar>
         </>
       )
