d420ae144ff7288c2d3ba3647716d87bd05ab983
Segun Adebayo <joseshegs@gmail.com>
2023-04-13 15:42:10 +0100

    chore: add vue and solid examples

--- examples/solid-ts/src/pages/color-picker.tsx
+++ examples/solid-ts/src/pages/color-picker.tsx
@@ -1,29 +1,64 @@
-import * as color-picker from "@zag-js/color-picker"
-import { normalizeProps, useMachine, mergeProps } from "@zag-js/solid"
+import * as colorPicker from "@zag-js/color-picker"
+import { normalizeProps, useMachine } from "@zag-js/solid"
+import { colorPickerControls } from "@zag-js/shared"
 import { createMemo, createUniqueId } from "solid-js"
-import { color-pickerControls, color-pickerData } from "@zag-js/shared"
 import { StateVisualizer } from "../components/state-visualizer"
 import { Toolbar } from "../components/toolbar"
 import { useControls } from "../hooks/use-controls"
 
 export default function Page() {
-  const controls = useControls(color-pickerControls)
+  const controls = useControls(colorPickerControls)
 
-  const [state, send] = useMachine(color-picker.machine({ id: createUniqueId() }), {
-    context: controls.context,
-  })
+  const [state, send] = useMachine(
+    colorPicker.machine({
+      id: createUniqueId(),
+      value: "hsl(0, 100%, 50%)",
+    }),
+    {
+      context: controls.context,
+    },
+  )
 
-  const api = createMemo(() => color-picker.connect(state, send, normalizeProps))
+  const api = createMemo(() => colorPicker.connect(state, send, normalizeProps))
+  const [hue, saturation, lightness] = api().channels
 
   return (
     <>
-      <main class="color-picker"> 
-        <div {...api().rootProps}> 
-            
+      <main class="color-picker">
+        <div {...api().contentProps}>
+          <div {...api().getAreaProps({ xChannel: saturation, yChannel: lightness })}>
+            <div {...api().getAreaGradientProps({ xChannel: saturation, yChannel: lightness })} />
+            <div {...api().getAreaThumbProps({ xChannel: saturation, yChannel: lightness })} />
+          </div>
+
+          <div {...api().getChannelSliderTrackProps({ channel: hue })}>
+            <div {...api().getChannelSliderThumbProps({ channel: hue })} />
+          </div>
+
+          <div {...api().getChannelSliderTrackProps({ channel: "alpha" })}>
+            <div {...api().getChannelSliderBackgroundProps({ channel: "alpha" })} />
+            <div {...api().getChannelSliderThumbProps({ channel: "alpha" })} />
+          </div>
+
+          <div style={{ display: "flex", width: "100%" }}>
+            <input {...api().getChannelInputProps({ channel: hue })} />
+            <input {...api().getChannelInputProps({ channel: saturation })} />
+            <input {...api().getChannelInputProps({ channel: lightness })} />
+            <input {...api().getChannelInputProps({ channel: "alpha" })} />
+          </div>
+
+          <div style={{ display: "flex", gap: "10px", "align-items": "center" }}>
+            <div {...api().getSwatchProps({ value: api().valueAsColor })}>
+              <div {...api().getSwatchBackgroundProps({ value: api().valueAsColor })} />
+            </div>
+            <p>{api().value}</p>
+          </div>
+
+          <button {...api().eyeDropperTriggerProps}>Eye Dropper</button>
         </div>
       </main>
 
-      <Toolbar controls={controls.ui}>
+      <Toolbar viz controls={controls.ui}>
         <StateVisualizer state={state} />
       </Toolbar>
     </>
--- examples/vue-ts/src/pages/color-picker.tsx
+++ examples/vue-ts/src/pages/color-picker.tsx
@@ -1,34 +1,63 @@
-import * as color-picker from "@zag-js/color-picker"
-import { normalizeProps, useMachine, mergeProps } from "@zag-js/vue"
-import { computed, defineComponent, h, Fragment } from "vue"
-import { color-pickerControls, color-pickerData } from "@zag-js/shared"
+import * as colorPicker from "@zag-js/color-picker"
+import { colorPickerControls } from "@zag-js/shared"
+import { normalizeProps, useMachine } from "@zag-js/vue"
+import { computed, defineComponent } from "vue"
 import { StateVisualizer } from "../components/state-visualizer"
 import { Toolbar } from "../components/toolbar"
 import { useControls } from "../hooks/use-controls"
 
 export default defineComponent({
-  name: "color-picker",
+  name: "ColorPicker",
   setup() {
-    const controls = useControls(color-pickerControls)
+    const controls = useControls(colorPickerControls)
 
-    const [state, send] = useMachine(color-picker.machine({ id: "1" }), {
+    const [state, send] = useMachine(colorPicker.machine({ id: "1", value: "hsl(0, 100%, 50%)" }), {
       context: controls.context,
     })
 
-    const apiRef = computed(() => color-picker.connect(state.value, send, normalizeProps))
+    const apiRef = computed(() => colorPicker.connect(state.value, send, normalizeProps))
 
     return () => {
       const api = apiRef.value
+      const [hue, saturation, lightness] = api.channels
 
       return (
         <>
           <main class="color-picker">
-            <div {...api.rootProps}>
-            
+            <div {...api.contentProps}>
+              <div {...api.getAreaProps({ xChannel: saturation, yChannel: lightness })}>
+                <div {...api.getAreaGradientProps({ xChannel: saturation, yChannel: lightness })} />
+                <div {...api.getAreaThumbProps({ xChannel: saturation, yChannel: lightness })} />
+              </div>
+
+              <div {...api.getChannelSliderTrackProps({ channel: hue })}>
+                <div {...api.getChannelSliderThumbProps({ channel: hue })} />
+              </div>
+
+              <div {...api.getChannelSliderTrackProps({ channel: "alpha" })}>
+                <div {...api.getChannelSliderBackgroundProps({ channel: "alpha" })} />
+                <div {...api.getChannelSliderThumbProps({ channel: "alpha" })} />
+              </div>
+
+              <div style={{ display: "flex", width: "100%" }}>
+                <input {...api.getChannelInputProps({ channel: hue })} />
+                <input {...api.getChannelInputProps({ channel: saturation })} />
+                <input {...api.getChannelInputProps({ channel: lightness })} />
+                <input {...api.getChannelInputProps({ channel: "alpha" })} />
+              </div>
+
+              <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
+                <div {...api.getSwatchProps({ value: api.valueAsColor })}>
+                  <div {...api.getSwatchBackgroundProps({ value: api.valueAsColor })} />
+                </div>
+                <p>{api.value}</p>
+              </div>
+
+              <button {...api.eyeDropperTriggerProps}>Eye Dropper</button>
             </div>
           </main>
 
-          <Toolbar controls={controls.ui}>
+          <Toolbar viz controls={controls.ui}>
             <StateVisualizer state={state} />
           </Toolbar>
         </>
