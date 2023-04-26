0a3b173f07941b4f07b78f298caa62395caa7282
Segun Adebayo <joseshegs@gmail.com>
2023-04-26 19:38:45 +0100

    fix: export types for splitter

---
+++ .changeset/nervous-keys-trade.md
@@ -0,0 +1,5 @@
+---
+"@zag-js/splitter": patch
+---
+
+Export types for panel and resize trigger props
--- packages/machines/splitter/src/index.ts
+++ packages/machines/splitter/src/index.ts
@@ -1,4 +1,4 @@
 export { anatomy } from "./splitter.anatomy"
 export { connect } from "./splitter.connect"
 export { machine } from "./splitter.machine"
-export type { MachineState, UserDefinedContext as Context } from "./splitter.types"
+export type { MachineState, UserDefinedContext as Context, PanelProps, ResizeTriggerProps } from "./splitter.types"
