fb4fb42d8aacc5844945dd7b1bd27b94c978ca4e
Segun Adebayo <joseshegs@gmail.com>
2023-05-04 12:13:35 +0100

    refactor: add support defer in most activities

--- packages/utilities/aria-hidden/src/index.ts
+++ packages/utilities/aria-hidden/src/index.ts
@@ -1,12 +1,21 @@
 // Credits: https://github.com/adobe/react-spectrum/blob/main/packages/@react-aria/overlays/src/ariaHideOutside.ts
+import { raf } from "@zag-js/dom-query"
 
-const elementCountMap = new WeakMap<Element, number>()
+const refCountMap = new WeakMap<Element, number>()
+const observerStack: any[] = []
 
-function isLiveRegion(node: Node, win: typeof window): node is HTMLElement {
-  return node instanceof win.HTMLElement && node.dataset.liveAnnouncer === "true"
+export type AriaHiddenOptions = {
+  rootEl?: HTMLElement
+  defer?: boolean
 }
 
-export function ariaHidden(targets: Array<HTMLElement | null>, rootEl?: HTMLElement) {
+type MaybeElement = HTMLElement | null
+type Targets = Array<MaybeElement>
+type TargetsOrFn = Targets | (() => Targets)
+
+function ariaHiddenImpl(targets: Targets, options: AriaHiddenOptions = {}) {
+  const { rootEl } = options
+
   const exclude = targets.filter(Boolean) as HTMLElement[]
   if (exclude.length === 0) return
 
@@ -18,36 +27,53 @@ export function ariaHidden(targets: Array<HTMLElement | null>, rootEl?: HTMLElem
 
   const root = rootEl ?? doc.body
 
-  const walker = doc.createTreeWalker(root, NodeFilter.SHOW_ELEMENT, {
-    acceptNode(node) {
-      // If this node is a live announcer, add it to the set of nodes to keep visible.
-      if (isLiveRegion(node, win)) {
-        visibleNodes.add(node)
-      }
+  let walk = (root: Element) => {
+    // Keep live announcer and top layer elements (e.g. toasts) visible.
+    for (let element of root.querySelectorAll("[data-live-announcer], [data-zag-top-layer]")) {
+      visibleNodes.add(element)
+    }
 
+    let acceptNode = (node: Element) => {
       // Skip this node and its children if it is one of the target nodes, or a live announcer.
-      // Also skip children of already hidden nodes, as aria-hidden is recursive.
-      if (visibleNodes.has(node as Element) || hiddenNodes.has(node.parentElement!)) {
+      // Also skip children of already hidden nodes, as aria-hidden is recursive. An exception is
+      // made for elements with role="row" since VoiceOver on iOS has issues hiding elements with role="row".
+      // For that case we want to hide the cells inside as well (https://bugs.webkit.org/show_bug.cgi?id=222623).
+      if (
+        visibleNodes.has(node) ||
+        (hiddenNodes.has(node.parentElement!) && node.parentElement!.getAttribute("role") !== "row")
+      ) {
         return NodeFilter.FILTER_REJECT
       }
 
-      // VoiceOver on iOS has issues hiding elements with role="row". Hide the cells inside instead.
-      // https://bugs.webkit.org/show_bug.cgi?id=222623
-      if (node instanceof win.HTMLElement && node.getAttribute("role") === "row") {
-        return NodeFilter.FILTER_SKIP
-      }
-
       // Skip this node but continue to children if one of the targets is inside the node.
-      if (exclude.some((target) => node.contains(target))) {
-        return NodeFilter.FILTER_SKIP
+      for (let target of visibleNodes) {
+        if (node.contains(target)) {
+          return NodeFilter.FILTER_SKIP
+        }
       }
 
       return NodeFilter.FILTER_ACCEPT
-    },
-  })
+    }
 
-  const hide = (node: Element) => {
-    let refCount = elementCountMap.get(node) ?? 0
+    let walker = doc.createTreeWalker(root, NodeFilter.SHOW_ELEMENT, { acceptNode })
+
+    // TreeWalker does not include the root.
+    let acceptRoot = acceptNode(root)
+    if (acceptRoot === NodeFilter.FILTER_ACCEPT) {
+      hide(root)
+    }
+
+    if (acceptRoot !== NodeFilter.FILTER_REJECT) {
+      let node = walker.nextNode() as Element
+      while (node != null) {
+        hide(node)
+        node = walker.nextNode() as Element
+      }
+    }
+  }
+
+  let hide = (node: Element) => {
+    let refCount = refCountMap.get(node) ?? 0
 
     // If already aria-hidden, and the ref count is zero, then this element
     // was already hidden and there's nothing for us to do.
@@ -60,28 +86,39 @@ export function ariaHidden(targets: Array<HTMLElement | null>, rootEl?: HTMLElem
     }
 
     hiddenNodes.add(node)
-    elementCountMap.set(node, refCount + 1)
+    refCountMap.set(node, refCount + 1)
   }
 
-  let node = walker.nextNode() as Element
-
-  while (node != null) {
-    hide(node)
-    node = walker.nextNode() as Element
+  if (observerStack.length) {
+    observerStack[observerStack.length - 1].disconnect()
   }
+
+  walk(root)
 
   const observer = new win.MutationObserver((changes) => {
     for (let change of changes) {
-      if (change.type !== "childList" || change.addedNodes.length === 0) continue
+      if (change.type !== "childList" || change.addedNodes.length === 0) {
+        continue
+      }
 
       // If the parent element of the added nodes is not within one of the targets,
       // and not already inside a hidden node, hide all of the new children.
       if (![...visibleNodes, ...hiddenNodes].some((node) => node.contains(change.target))) {
-        for (const node of change.addedNodes) {
-          if (isLiveRegion(node, win)) {
+        for (let node of change.removedNodes) {
+          if (node instanceof win.Element) {
+            visibleNodes.delete(node)
+            hiddenNodes.delete(node)
+          }
+        }
+
+        for (let node of change.addedNodes) {
+          if (
+            (node instanceof win.HTMLElement || node instanceof win.SVGElement) &&
+            (node.dataset.liveAnnouncer === "true" || node.dataset.zagTopLayer === "true")
+          ) {
             visibleNodes.add(node)
           } else if (node instanceof win.Element) {
-            hide(node)
+            walk(node)
           }
         }
       }
@@ -90,21 +127,53 @@ export function ariaHidden(targets: Array<HTMLElement | null>, rootEl?: HTMLElem
 
   observer.observe(root, { childList: true, subtree: true })
 
+  let observerWrapper = {
+    observe() {
+      observer.observe(root, { childList: true, subtree: true })
+    },
+    disconnect() {
+      observer.disconnect()
+    },
+  }
+
+  observerStack.push(observerWrapper)
+
   return () => {
     observer.disconnect()
 
     for (let node of hiddenNodes) {
-      let count = elementCountMap.get(node)
-
+      let count = refCountMap.get(node)
       if (count === 1) {
         node.removeAttribute("aria-hidden")
-        elementCountMap.delete(node)
-        continue
-      }
-
-      if (count !== undefined) {
-        elementCountMap.set(node, count - 1)
+        refCountMap.delete(node)
+      } else {
+        refCountMap.set(node, count! - 1)
       }
     }
+
+    // Remove this observer from the stack, and start the previous one.
+    if (observerWrapper === observerStack[observerStack.length - 1]) {
+      observerStack.pop()
+      if (observerStack.length) {
+        observerStack[observerStack.length - 1].observe()
+      }
+    } else {
+      observerStack.splice(observerStack.indexOf(observerWrapper), 1)
+    }
+  }
+}
+
+export function ariaHidden(targetsOrFn: TargetsOrFn, options: AriaHiddenOptions = {}) {
+  const { defer } = options
+  const func = defer ? raf : (v: any) => v()
+  const cleanups: (VoidFunction | undefined)[] = []
+  cleanups.push(
+    func(() => {
+      const targets = typeof targetsOrFn === "function" ? targetsOrFn() : targetsOrFn
+      cleanups.push(ariaHiddenImpl(targets, options))
+    }),
+  )
+  return () => {
+    cleanups.forEach((fn) => fn?.())
   }
 }
