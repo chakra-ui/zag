cc54b83f1890a99c606be10e7bb126a4ea103080
Segun Adebayo <joseshegs@gmail.com>
2023-05-04 16:23:48 +0100

    fix: type issues in popper

--- packages/utilities/popper/src/auto-update.ts
+++ packages/utilities/popper/src/auto-update.ts
@@ -51,9 +51,9 @@ export function autoUpdate(
   }
 
   function addResizeListeners() {
-    let cleanups: VoidFunction[] = [trackElementRect(floating, update, { scope: "size" })]
+    let cleanups: VoidFunction[] = [trackElementRect(floating, { scope: "size", onChange: update })]
     if (referenceResize && isHTMLElement(reference)) {
-      cleanups.push(trackElementRect(reference, update))
+      cleanups.push(trackElementRect(reference, { onChange: update }))
     }
     cleanups.push(callAll(...ancestors.map((el: any) => addDomEvent(el, "resize", update))))
     return () => cleanups.forEach((fn) => fn())
