b365cecd6f5862f0e9c14a0f507a6864789f042d
junghyeonsu <jung660317@naver.com>
2023-05-09 13:15:28 +0900

    fix: switch machine and transition

--- .xstate/switch.js
+++ .xstate/switch.js
@@ -1,42 +1,45 @@
-"use strict"
+"use strict";
 
-var _xstate = require("xstate")
-const { actions, createMachine, assign } = _xstate
-const { choose } = actions
-const fetchMachine = createMachine(
-  {
-    id: "switch",
-    initial: "ready",
-    context: {},
-    activities: ["trackFormControlState"],
-    on: {
-      "CHECKED.TOGGLE": {
-        actions: ["toggleChecked"],
-      },
-      "CHECKED.SET": {
-        actions: ["dispatchChangeEvent"],
-      },
-      "CONTEXT.SET": {
-        actions: ["setContext"],
-      },
+var _xstate = require("xstate");
+const {
+  actions,
+  createMachine,
+  assign
+} = _xstate;
+const {
+  choose
+} = actions;
+const fetchMachine = createMachine({
+  id: "switch",
+  initial: "ready",
+  context: {},
+  activities: ["trackFormControlState"],
+  on: {
+    "CHECKED.TOGGLE": {
+      actions: ["toggleChecked"]
     },
-    on: {
-      UPDATE_CONTEXT: {
-        actions: "updateContext",
-      },
+    "CHECKED.SET": {
+      actions: ["dispatchChangeEvent"]
     },
-    states: {
-      ready: {},
-    },
+    "CONTEXT.SET": {
+      actions: ["setContext"]
+    }
   },
-  {
-    actions: {
-      updateContext: assign((context, event) => {
-        return {
-          [event.contextKey]: true,
-        }
-      }),
-    },
-    guards: {},
+  on: {
+    UPDATE_CONTEXT: {
+      actions: "updateContext"
+    }
   },
-)
+  states: {
+    ready: {}
+  }
+}, {
+  actions: {
+    updateContext: assign((context, event) => {
+      return {
+        [event.contextKey]: true
+      };
+    })
+  },
+  guards: {}
+});
--- packages/machines/switch/src/index.ts
+++ packages/machines/switch/src/index.ts
@@ -1,3 +1,4 @@
+export { anatomy } from "./switch.anatomy"
 export { connect } from "./switch.connect"
 export { machine } from "./switch.machine"
 export type { UserDefinedContext as Context } from "./switch.types"
