424a147cba5ed354835753ccecd5fd296808bff5
anubra266 <anubra266@gmail.com>
2023-05-13 14:59:47 +0100

    feat: add checkbox

---
+++ examples/svelte-kit/src/routes/checkbox/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import * as checkbox from "@zag-js/checkbox"
+  import { events, normalizeProps, useMachine } from "@zag-js/svelte"
+  import { checkboxControls } from "@zag-js/shared"
+  import serialize from "form-serialize"
+  import StateVisualizer from "../../components/state-visualizer.svelte"
+  import Toolbar from "../../components/toolbar.svelte"
+  import { ControlsUI, useControls } from "../../stores/controls"
+
+  const [context, defaultValues] = useControls(checkboxControls)
+  $: $context = defaultValues
+
+  const [state, send] = useMachine(checkbox.machine({ id: "checkbox", name: "checkbox" }), { context })
+
+  $: api = checkbox.connect($state, send, normalizeProps)
+</script>
+
+<main class="checkbox">
+  <form
+    on:change={(e) => {
+      const result = serialize(e.currentTarget, { hash: true })
+      console.log(result)
+    }}
+  >
+    <fieldset>
+      <label {...api.rootProps.attrs} use:events={api.rootProps.handlers}>
+        <div {...api.controlProps.attrs} use:events={api.controlProps.handlers} />
+        <span {...api.labelProps.attrs} use:events={api.labelProps.handlers}
+          >Input {api.isChecked ? "Checked" : "Unchecked"}</span
+        >
+        <input {...api.inputProps.attrs} use:events={api.inputProps.handlers} />
+      </label>
+
+      <button type="button" disabled={api.isChecked} on:click={() => api.setChecked(true)}> Check </button>
+      <button type="button" disabled={!api.isChecked} on:click={() => api.setChecked(false)}> Uncheck </button>
+      <button type="reset">Reset Form</button>
+    </fieldset>
+  </form>
+</main>
+
+<Toolbar>
+  <ControlsUI slot="controls" {context} controls={checkboxControls} />
+  <StateVisualizer state={$state} />
+</Toolbar>
