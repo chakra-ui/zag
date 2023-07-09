4f3718742dc88a2cd8726bdd889c9bbde94f5bce
Segun Adebayo <joseshegs@gmail.com>
2023-07-09 14:11:03 +0100

    build: use tsup for build and emit sourcemaps

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "tsup"
 
 export default defineConfig({
   clean: true,
+  dts: true,
+  entry: ["src/index.ts"],
   target: "es2020",
+  sourcemap: true,
   format: ["esm", "cjs"],
 })
