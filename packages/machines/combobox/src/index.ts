6e05db6778d5e2cdabeb934b0128a47c5775e5d6
Christian Schröter <dev@grizzly.codes>
2023-07-12 17:47:49 +0200

    chore: rm exports for combobox

--- packages/machines/combobox/src/index.ts
+++ packages/machines/combobox/src/index.ts
@@ -1,12 +1,4 @@
 export { anatomy } from "./combobox.anatomy"
 export { connect } from "./combobox.connect"
 export { machine } from "./combobox.machine"
-export type {
-  UserDefinedContext as Context,
-  InteractOutsideEvent,
-  OptionData,
-  OptionGroupProps,
-  OptionProps,
-  Placement,
-  PositioningOptions,
-} from "./combobox.types"
+export type { UserDefinedContext as Context, OptionData, OptionGroupProps, OptionProps } from "./combobox.types"
