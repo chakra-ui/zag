f749cb91aeb5b5edfe69f0414870bb978d7501e3
Christian Schröter <dev@grizzly.codes>
2023-07-13 13:29:34 +0200

    feat(accordion): export public api

--- packages/machines/accordion/src/index.ts
+++ packages/machines/accordion/src/index.ts
@@ -1,4 +1,4 @@
 export { anatomy } from "./accordion.anatomy"
 export { connect } from "./accordion.connect"
 export { machine } from "./accordion.machine"
-export type { UserDefinedContext as Context, ItemProps, ItemState } from "./accordion.types"
+export type { UserDefinedContext as Context, ItemProps, ItemState, PublicApi } from "./accordion.types"
