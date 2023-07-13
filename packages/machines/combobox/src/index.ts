d24e87b376bac3030937efb65815b831e490ef63
Christian Schröter <dev@grizzly.codes>
2023-07-13 15:14:25 +0200

    fix(combobox): export all external types

--- packages/machines/combobox/src/index.ts
+++ packages/machines/combobox/src/index.ts
@@ -3,8 +3,11 @@ export { connect } from "./combobox.connect"
 export { machine } from "./combobox.machine"
 export type {
   UserDefinedContext as Context,
+  InteractOutsideEvent,
   OptionData,
   OptionGroupProps,
   OptionProps,
+  Placement,
+  PositioningOptions,
   PublicApi,
 } from "./combobox.types"
