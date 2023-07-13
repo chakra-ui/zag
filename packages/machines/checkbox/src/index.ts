4e697b346e9c67f610bca335f372273a2dbe1966
Christian Schröter <dev@grizzly.codes>
2023-07-13 12:51:13 +0200

    fix(checkbox): export CheckedState

--- packages/machines/checkbox/src/index.ts
+++ packages/machines/checkbox/src/index.ts
@@ -1,4 +1,4 @@
 export { anatomy } from "./checkbox.anatomy"
 export { connect } from "./checkbox.connect"
 export { machine } from "./checkbox.machine"
-export type { UserDefinedContext as Context } from "./checkbox.types"
+export type { CheckedState, UserDefinedContext as Context } from "./checkbox.types"
