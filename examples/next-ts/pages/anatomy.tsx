37fb88a2f6cf080aa0e5793e8788bc0add57889d
anubra266 <anubra266@gmail.com>
2023-07-18 23:04:45 +0100

    chore: cleanup

--- examples/next-ts/pages/anatomy.tsx
+++ examples/next-ts/pages/anatomy.tsx
@@ -1,7 +1,5 @@
 import * as Anatomies from "@zag-js/anatomy-icons"
 
-// const Anatomy = Anatomies.TooltipAnatomy
-
 const ACCENTS = {
   "#2CFF80": "linear-gradient(112deg, #41B883 0%, #299464 100%)",
   red: "linear-gradient(112deg, #BB4141 0%, #942929 100%)",
@@ -10,13 +8,10 @@ const ACCENTS = {
 
 export default function Page() {
   return (
-    <div
-      className="anatomy"
-      style={{ position: "fixed", top: 0, bottom: 0, left: 0, overflow: "auto", maxHeight: "100%" }}
-    >
+    <div className="anatomy">
       {Object.entries(Anatomies).map(([name, Anatomy], i) => (
-        <div style={{ display: "flex", overflow: "auto", width: "100%", position: "relative" }} key={i}>
-          <span style={{ position: "absolute", top: 2, left: 2, fontWeight: "500" }}>{name}</span>
+        <div className="anatomy__item" key={i}>
+          <span>{name}</span>
           {Object.entries(ACCENTS).map(([accent, background], i) => (
             <div
               key={i}
