9cbfe0c64461b8c8ac5a464f1522832aedf50f8b
anubra266 <anubra266@gmail.com>
2023-07-22 13:39:59 +0100

    chore: little refactor

--- examples/next-ts/pages/anatomy.tsx
+++ examples/next-ts/pages/anatomy.tsx
@@ -1,5 +1,5 @@
 import * as Anatomies from "@zag-js/anatomy-icons"
-import { adjustHue, darken, desaturate } from "color2k"
+import { adjustHue, darken, desaturate, toHex } from "color2k"
 
 const ACCENTS = ["red", "#2CFF80", "blue"]
 
@@ -9,19 +9,21 @@ export default function Page() {
       {Object.entries(Anatomies).map(([name, Anatomy], i) => (
         <div className="anatomy__item" key={i}>
           <span>{name.replace("Anatomy", "")}</span>
-          {ACCENTS.map((accent, i) => (
-            <div
-              key={i}
-              style={{
-                background: `linear-gradient(112deg, ${darken(
-                  desaturate(adjustHue(accent, 9), 0.52),
-                  0.1,
-                )} 0%, ${darken(desaturate(adjustHue(accent, 9), 0.43), 0.22)} 100%)`,
-              }}
-            >
-              <Anatomy accentColor={accent} />
-            </div>
-          ))}
+          {ACCENTS.map((accent, i) => {
+            const startColor = toHex(darken(desaturate(adjustHue(accent, 9), 0.52), 0.1))
+            const stopColor = toHex(darken(desaturate(adjustHue(accent, 9), 0.43), 0.22))
+
+            return (
+              <div
+                key={i}
+                style={{
+                  background: `linear-gradient(112deg, ${startColor} 0%, ${stopColor} 100%)`,
+                }}
+              >
+                <Anatomy accentColor={accent} />
+              </div>
+            )
+          })}
         </div>
       ))}
     </div>
