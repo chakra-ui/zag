1357801e332bf0d45fa077611853c7a0c42bdb56
Segun Adebayo <joseshegs@gmail.com>
2023-08-14 09:06:12 +0100

    refactor: tabs

--- packages/machines/tabs/src/index.ts
+++ packages/machines/tabs/src/index.ts
@@ -1,4 +1,4 @@
 export { anatomy } from "./tabs.anatomy"
 export { connect } from "./tabs.connect"
 export { machine } from "./tabs.machine"
-export type { ContentProps, UserDefinedContext as Context, PublicApi, TriggerProps } from "./tabs.types"
+export type { ContentProps, UserDefinedContext as Context, MachineApi as Api, TriggerProps } from "./tabs.types"
