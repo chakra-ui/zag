09110fda7061be689746a3c10e88e2786f792eb6
Segun Adebayo <joseshegs@gmail.com>
2023-08-24 15:31:43 +0100

    chore: add checkbox and editable examples

---
+++ examples/nuxt-ts/pages/checkbox.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import * as checkbox from "@zag-js/checkbox"
+import { checkboxControls } from "@zag-js/shared"
+import { normalizeProps, useMachine } from "@zag-js/vue"
+
+const controls = useControls(checkboxControls)
+
+const [state, send] = useMachine(
+  checkbox.machine({
+    name: "checkbox",
+    id: "v1",
+  }),
+  { context: controls.context },
+)
+
+const api = computed(() => checkbox.connect(state.value, send, normalizeProps))
+</script>
+
+<template>
+  <main class="checkbox">
+    <form>
+      <fieldset>
+        <label v-bind="api.rootProps">
+          <div v-bind="api.controlProps" />
+          <span v-bind="api.labelProps">Input {{ api.isChecked ? "Checked" : "Unchecked" }}</span>
+          <input v-bind="api.hiddenInputProps" data-testid="hidden-input" />
+        </label>
+
+        <button type="button" :disabled="api.isChecked" @click="() => api.setChecked(true)">Check</button>
+        <button type="button" :disabled="!api.isChecked" @click="() => api.setChecked(false)">Uncheck</button>
+        <button type="reset">Reset Form</button>
+      </fieldset>
+    </form>
+  </main>
+
+  <Toolbar>
+    <StateVisualizer :state="state" />
+    <template #controls>
+      <Controls :config="controls.config" :state="controls.context" />
+    </template>
+  </Toolbar>
+</template>
---
+++ examples/nuxt-ts/pages/editable.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import * as editable from "@zag-js/editable"
+import { editableControls } from "@zag-js/shared"
+import { normalizeProps, useMachine } from "@zag-js/vue"
+
+const controls = useControls(editableControls)
+
+const [state, send] = useMachine(editable.machine({ id: "editable" }), {
+  context: controls.context,
+})
+
+const api = computed(() => editable.connect(state.value, send, normalizeProps))
+</script>
+
+<template>
+  <main class="editable">
+    <div v-bind="api.rootProps">
+      <div v-bind="api.areaProps">
+        <input data-testid="input" v-bind="api.inputProps" />
+        <span data-testid="preview" v-bind="api.previewProps" />
+      </div>
+      <div v-bind="api.controlProps">
+        <button v-if="!api.isEditing" data-testid="edit-button" v-bind="api.editTriggerProps">Edit</button>
+
+        <Fragment v-if="api.isEditing">
+          <button data-testid="save-button" v-bind="api.submitTriggerProps">Save</button>
+          <button data-testid="cancel-button" v-bind="api.cancelTriggerProps">Cancel</button>
+        </Fragment>
+      </div>
+    </div>
+  </main>
+
+  <Toolbar>
+    <StateVisualizer :state="state" />
+    <template #controls>
+      <Controls :config="controls.config" :state="controls.context" />
+    </template>
+  </Toolbar>
+</template>
