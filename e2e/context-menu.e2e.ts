512a30f51e08cef67864e5fc4d5cdc251099aad0
Segun Adebayo <joseshegs@gmail.com>
2023-08-25 09:33:26 +0100

    test: add e2e for context menu

---
+++ e2e/context-menu.e2e.ts
@@ -0,0 +1,31 @@
+import { expect, test, type Page } from "@playwright/test"
+import { part, repeat } from "./__utils"
+
+const trigger = part("context-trigger")
+const menu = part("content")
+
+const expectToBeFocused = async (page: Page, id: string) => {
+  return expect(page.locator(`[id=${id}]`).first()).toHaveAttribute("data-highlighted", "")
+}
+
+test.describe("context menu", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/context-menu")
+  })
+
+  test("should open on right click", async ({ page }) => {
+    await page.click(trigger, { button: "right" })
+
+    await expect(page.locator(menu)).toBeVisible()
+    await expect(page.locator(menu)).toBeFocused()
+
+    await page.press("body", "Escape")
+    await expect(page.locator(menu)).not.toBeVisible()
+  })
+
+  test("keyboard navigation works", async ({ page }) => {
+    await page.click(trigger, { button: "right" })
+    await repeat(() => page.keyboard.press("ArrowDown"), 3)
+    await expectToBeFocused(page, "delete")
+  })
+})
