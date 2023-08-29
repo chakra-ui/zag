c54f59669f185480d7adda761eb4f9986ab51636
Christian Schröter <dev@grizzly.codes>
2023-08-29 21:29:06 +0200

    feat(editable): rename MachineApi to Api to be consistent

--- packages/machines/editable/src/index.ts
+++ packages/machines/editable/src/index.ts
@@ -1,4 +1,4 @@
 export { anatomy } from "./editable.anatomy"
 export { connect } from "./editable.connect"
 export { machine } from "./editable.machine"
-export type { UserDefinedContext as Context, MachineApi, InteractOutsideEvent } from "./editable.types"
+export type { UserDefinedContext as Context, MachineApi as Api, InteractOutsideEvent } from "./editable.types"
