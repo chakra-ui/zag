54078b65aee8a558e762e83a6984141ff32b9850
Filip Sauer <sauer.filip@gmail.com>
2023-09-17 16:46:21 +0200

    fix(pagination): fix edge cases, refactor and simplify code, add tests

--- packages/machines/pagination/tests/pagination.utils.test.ts
+++ packages/machines/pagination/tests/pagination.utils.test.ts
@@ -1,44 +1,48 @@
 import { expect, describe, test } from "vitest"
 import * as utils from "../src/pagination.utils"
+import { getRange } from "../src/pagination.utils"
+import { getRangeTestCases } from "./pagination.utils.test-cases"
 
 describe("@zag-js/pagination utils", () => {
   test("range method", () => {
-    expect(utils.range(1, 5)).toMatchInlineSnapshot(`
-    [
-      1,
-      2,
-      3,
-      4,
-      5,
-    ]
-  `)
+    expect(utils.range(1, 5)).toEqual([1, 2, 3, 4, 5])
   })
 
   test("transform method", () => {
     const items = [1, 2, "ellipsis", 3, 4]
     const transformed = utils.transform(items)
-    expect(transformed).toMatchInlineSnapshot(`
-    [
+    expect(transformed).toEqual([
       {
-        "type": "page",
-        "value": 1,
+        type: "page",
+        value: 1,
       },
       {
-        "type": "page",
-        "value": 2,
+        type: "page",
+        value: 2,
       },
       {
-        "type": "ellipsis",
+        type: "ellipsis",
       },
       {
-        "type": "page",
-        "value": 3,
+        type: "page",
+        value: 3,
       },
       {
-        "type": "page",
-        "value": 4,
+        type: "page",
+        value: 4,
       },
-    ]
-  `)
+    ])
+  })
+
+  describe("getRange method", () => {
+    test.each(getRangeTestCases)(
+      "siblingCount: $siblingCount, totalPages: $totalPages, page: $pages",
+      ({ expected, pages, ...ctx }) => {
+        for (let page of pages) {
+          const range = getRange({ ...ctx, page })
+          expect(range).toEqual(expected)
+        }
+      },
+    )
   })
 })
