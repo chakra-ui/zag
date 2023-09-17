bef90c6bc80768e1dae100f73a47169203293ce3
Filip Sauer <sauer.filip@gmail.com>
2023-09-17 23:52:21 +0200

    test(color-picker): add swatch tests

--- e2e/color-picker.e2e.ts
+++ e2e/color-picker.e2e.ts
@@ -1,5 +1,11 @@
-import { test } from "@playwright/test"
-import { a11y } from "./__utils"
+import { expect, test } from "@playwright/test"
+import { a11y, testid } from "./__utils"
+
+const readOnlySwatch = testid("readonly-swatch")
+const clickableSwatch1 = testid("clickable-swatch-1")
+const clickableSwatch2 = testid("clickable-swatch-2")
+const value = testid("value")
+const channelInputHex = "[data-part=channel-input][data-channel=hex]"
 
 test.describe("color-picker", () => {
   test.beforeEach(async ({ page }) => {
@@ -8,5 +14,23 @@ test.describe("color-picker", () => {
 
   test("should have no accessibility violations", async ({ page }) => {
     await a11y(page, ".color-picker")
+  })
+
+  test("[swatch] should change the value if is not readonly", async ({ page }) => {
+    await page.click(readOnlySwatch)
+    await expect(page.locator(value)).toContainText("hsl(0, 100%, 50%)")
+    await page.click(clickableSwatch1)
+    await expect(page.locator(value)).toContainText("hsla(0, 85.43%, 70.39%, 1)")
+    await page.click(clickableSwatch2)
+    await expect(page.locator(value)).toContainText("hsla(215.62, 13.22%, 47.45%, 1)")
+  })
+
+  test("[swatch / channel input hex] should change the hex channel input if is not readonly", async ({ page }) => {
+    await page.click(readOnlySwatch)
+    await expect(page.locator(channelInputHex)).toHaveValue("#FF0000")
+    await page.click(clickableSwatch1)
+    await expect(page.locator(channelInputHex)).toHaveValue("#F47373")
+    await page.click(clickableSwatch2)
+    await expect(page.locator(channelInputHex)).toHaveValue("#697689")
   })
 })
