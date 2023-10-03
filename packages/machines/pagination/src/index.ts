169f6bd146b439b5396eecdfdd2bf853ec7eee83
Segun Adebayo <joseshegs@gmail.com>
2023-10-03 14:02:04 +0100

    fix: update exports

--- packages/machines/pagination/src/index.ts
+++ packages/machines/pagination/src/index.ts
@@ -1,9 +1,4 @@
 export { anatomy } from "./pagination.anatomy"
 export { connect } from "./pagination.connect"
 export { machine } from "./pagination.machine"
-export type {
-  UserDefinedContext as Context,
-  EllipsisProps,
-  PageTriggerProps,
-  MachineApi as Api,
-} from "./pagination.types"
+export type { MachineApi as Api, UserDefinedContext as Context, EllipsisProps, ItemProps } from "./pagination.types"
