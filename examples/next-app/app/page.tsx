c7cbcbfe76e77792ef4baee05ea29c19fffbc754
Segun Adebayo <joseshegs@gmail.com>
2023-12-14 14:40:35 +0000

    chore: add next-app example

---
+++ examples/next-app/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Page({ children }: { children: React.ReactNode }) {
+  return <div>{children}</div>
+}
---
+++ examples/next-app/app/select-search-params/page.tsx
@@ -0,0 +1,94 @@
+"use client"
+
+import { Portal, normalizeProps, useMachine } from "@zag-js/react"
+import * as select from "@zag-js/select"
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import { useId } from "react"
+
+const items = [
+  { label: "Taubaté", value: "Taubaté" },
+  { label: "São Paulo", value: "São Paulo" },
+  { label: "Rio de Janeiro", value: "Rio de Janeiro" },
+  { label: "Belo Horizonte", value: "Belo Horizonte" },
+  { label: "Curitiba", value: "Curitiba" },
+]
+
+function Select({ value, setValue }: { value: string | null | undefined; setValue: (value: string) => void }) {
+  const collection = select.collection({
+    items: items,
+    itemToString: (item) => item.label,
+    itemToValue: (item) => item.value,
+  })
+
+  const [state, send] = useMachine(select.machine({ id: useId(), collection }), {
+    context: {
+      value: value ? [value] : undefined,
+      onValueChange(details) {
+        setValue(details.value[0])
+      },
+    },
+  })
+
+  const api = select.connect(state, send, normalizeProps)
+
+  return (
+    <div {...api.rootProps}>
+      <div {...api.controlProps}>
+        <label {...api.labelProps}>Label</label>
+        <button {...api.triggerProps}>{api.valueAsString || "Select a city"}</button>
+      </div>
+
+      <Portal>
+        <div {...api.positionerProps}>
+          <ul {...api.contentProps}>
+            {items.map((item) => (
+              <li key={item.value} {...api.getItemProps({ item })}>
+                <span>{item.label}</span>
+                <span {...api.getItemIndicatorProps({ item })}>✓</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Portal>
+    </div>
+  )
+}
+
+export default function SearchParamClientPage() {
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const router = useRouter()
+
+  const selectedCountry = searchParams.get("country")
+
+  console.log("search param value:", selectedCountry)
+
+  return (
+    <>
+      <h1>{selectedCountry}</h1>
+
+      <button
+        onClick={() => {
+          router.push(`${pathname}?country=${items[0].value}`)
+        }}
+      >
+        Change to {items[0].label}
+      </button>
+
+      <button
+        onClick={() => {
+          router.push(`${pathname}?country=${items[1].value}`)
+        }}
+      >
+        Change to {items[1].label}
+      </button>
+
+      <Select
+        value={selectedCountry}
+        setValue={(value) => {
+          router.push(`${pathname}?country=${value}`)
+        }}
+      />
+    </>
+  )
+}
