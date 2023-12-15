49b6f26dc87e47eacea7236aab9e27f5fb920ab4
Michał Korczak <michal.k2001@gmail.com>
2023-12-15 12:31:52 +0100

    fix(progress): expose more types (#1094)
    * fix(progress): expose more types
    
    * fix: exports

--- packages/machines/progress/src/index.ts
+++ packages/machines/progress/src/index.ts
@@ -1,4 +1,10 @@
 export { anatomy } from "./progress.anatomy"
 export { connect } from "./progress.connect"
 export { machine } from "./progress.machine"
-export type { UserDefinedContext as Context } from "./progress.types"
+export type {
+  MachineApi as Api,
+  UserDefinedContext as Context,
+  IndicatorProps,
+  IntlTranslations,
+  ValueLabelOptions,
+} from "./progress.types"
