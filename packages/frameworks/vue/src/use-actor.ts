217f2cbdf3904522c569ed1623fd00bfbfce07f2
Segun Adebayo <joseshegs@gmail.com>
2023-12-16 13:48:22 +0000

    refactor: use actor for vue

--- packages/frameworks/vue/src/use-actor.ts
+++ packages/frameworks/vue/src/use-actor.ts
@@ -1,22 +1,11 @@
 import type { Machine, StateMachine as S } from "@zag-js/core"
-import { onBeforeUnmount, onMounted, shallowRef } from "vue"
+import { useSnapshot } from "./use-snapshot"
 
 export function useActor<
   TContext extends Record<string, any>,
   TState extends S.StateSchema,
   TEvent extends S.EventObject = S.AnyEventObject,
 >(service: Machine<TContext, TState, TEvent>) {
-  const state = shallowRef(service.state)
-
-  onMounted(() => {
-    const unsubscribe = service.subscribe((nextState) => {
-      state.value = nextState
-    })
-
-    onBeforeUnmount(() => {
-      unsubscribe?.()
-    })
-  })
-
+  const state = useSnapshot(service)
   return [state, service.send] as const
 }
--- packages/frameworks/vue/src/use-snapshot.ts
+++ packages/frameworks/vue/src/use-snapshot.ts
@@ -1,12 +1,16 @@
 import type { Machine, StateMachine as S } from "@zag-js/core"
 import { snapshot, subscribe } from "@zag-js/store"
 import { onUnmounted, shallowRef, watch, watchEffect, type Ref } from "vue"
+import type { MachineOptions } from "./types"
 
 export function useSnapshot<
   TContext extends Record<string, any>,
   TState extends S.StateSchema,
   TEvent extends S.EventObject = S.AnyEventObject,
->(service: Machine<TContext, TState, TEvent>, options?: any): Ref<S.State<TContext, TState, TEvent>> {
+>(
+  service: Machine<TContext, TState, TEvent>,
+  options?: MachineOptions<TContext, TState, TEvent>,
+): Ref<S.State<TContext, TState, TEvent>> {
   //
   const { actions, context } = options ?? {}
 
