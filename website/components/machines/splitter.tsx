9684009654ef81ec933107a69b6e8941efdb87fc
Dang Van Thanh <dangvanthanh@dangthanh.org>
2024-01-15 21:27:52 +0700

    docs: update vertial orientation for splitter

--- website/components/machines/splitter.tsx
+++ website/components/machines/splitter.tsx
@@ -1,8 +1,8 @@
-import * as splitter from "@zag-js/splitter"
-import { normalizeProps, useMachine } from "@zag-js/react"
-import { chakra } from "@chakra-ui/system"
-import { useId } from "react"
 import { Flex } from "@chakra-ui/layout"
+import { chakra } from "@chakra-ui/system"
+import { normalizeProps, useMachine } from "@zag-js/react"
+import * as splitter from "@zag-js/splitter"
+import { useId } from "react"
 
 export function Splitter(props: any) {
   const [state, send] = useMachine(
@@ -20,53 +20,56 @@ export function Splitter(props: any) {
   const api = splitter.connect(state, send, normalizeProps)
 
   return (
-    <chakra.div width="240px" {...api.rootProps}>
-      <Flex padding={20} {...api.rootProps}>
-        <Flex
-          bg="bg-bold"
-          align="center"
-          justify="center"
-          {...api.getPanelProps({ id: "aside" })}
-        >
-          <p>Aside</p>
-        </Flex>
-        <Flex
-          bg="#e2e8f0"
-          justify="center"
-          align="center"
-          w={2}
-          h={40}
-          {...api.getResizeTriggerProps({ id: "aside:content" })}
-        >
-          <chakra.div bg="#000" w={0.5} h={4} />
-        </Flex>
-        <Flex
-          bg="bg-bold"
-          align="center"
-          justify="center"
-          {...api.getPanelProps({ id: "content" })}
-        >
-          <p>Content</p>
-        </Flex>
-        <Flex
-          bg="#e2e8f0"
-          justify="center"
-          align="center"
-          w={2}
-          h={40}
-          {...api.getResizeTriggerProps({ id: "content:sources" })}
-        >
-          <chakra.div bg="#000" w={0.5} h={4} />
-        </Flex>
-        <Flex
-          bg="bg-bold"
-          align="center"
-          justify="center"
-          {...api.getPanelProps({ id: "sources" })}
-        >
-          <p>Sources</p>
-        </Flex>
+    <Flex
+      align="center"
+      justify="center"
+      padding={10}
+      h={40}
+      {...api.rootProps}
+    >
+      <Flex
+        bg="bg-bold"
+        w="full"
+        padding={2}
+        textAlign="center"
+        {...api.getPanelProps({ id: "aside" })}
+      >
+        <p>Aside</p>
       </Flex>
-    </chakra.div>
+      <Flex
+        bg="#e2e8f0"
+        w={1}
+        h={1}
+        {...api.getResizeTriggerProps({ id: "aside:content" })}
+      >
+        <chakra.div className="bar" />
+      </Flex>
+      <Flex
+        bg="bg-bold"
+        w="full"
+        padding={2}
+        textAlign="center"
+        {...api.getPanelProps({ id: "content" })}
+      >
+        <p>Content</p>
+      </Flex>
+      <Flex
+        bg="#e2e8f0"
+        w={1}
+        h={1}
+        {...api.getResizeTriggerProps({ id: "content:sources" })}
+      >
+        <chakra.div className="bar" />
+      </Flex>
+      <Flex
+        bg="bg-bold"
+        w="full"
+        padding={2}
+        textAlign="center"
+        {...api.getPanelProps({ id: "sources" })}
+      >
+        <p>Sources</p>
+      </Flex>
+    </Flex>
   )
 }
