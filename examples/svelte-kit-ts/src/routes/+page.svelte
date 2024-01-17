562eebb5ec4d039c1dc06a397258b2f5a4658394
Izzuddin Natsir <32354148+izznatsir@users.noreply.github.com>
2024-01-17 21:57:38 +0700

    chore: setup svelte-kit-ts example

---
+++ examples/svelte-kit-ts/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { routesData } from '@zag-js/shared';
+</script>
+
+<div class="index-nav">
+	<h2>Svelte UI Machines</h2>
+	<ul>
+		{#each routesData as route}
+			<li>
+				<a href={route.path}>{route.label}</a>
+			</li>
+		{/each}
+	</ul>
+</div>
