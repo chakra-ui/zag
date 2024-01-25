b5bc2d8e6a765902bdc5edf608adf314e9d93b1e
Izzuddin Natsir <32354148+izznatsir@users.noreply.github.com>
2024-01-25 15:56:29 +0700

    fix: convert style object to string

--- packages/frameworks/svelte/src/normalize-props.ts
+++ packages/frameworks/svelte/src/normalize-props.ts
@@ -12,20 +12,44 @@ const propMap: Record<string, string> = {
   onFocus: "onfocusin",
 }
 
+function toStyleString(style: Record<string, number | string>) {
+  let string = ""
+
+  for (let key in style) {
+    /**
+     * Ignore null and undefined values.
+     */
+    const value = style[key]
+    if (value === null || value === undefined) continue
+
+    /**
+     * Convert camelCase to kebab-case except for CSS custom properties.
+     */
+    if (!key.startsWith("--")) key = key.replace(/[A-Z]/g, (match) => `-${match.toLowerCase()}`)
+
+    string += `${key}:${value};`
+  }
+
+  return string
+}
+
 function toSvelteProp(key: string) {
   if (key in propMap) return propMap[key]
   return key.toLowerCase()
 }
 
-function toSveltePropValue(value: Dict[string]) {
-  return value === false ? undefined : value
+function toSveltePropValue(key: string, value: Dict[string]) {
+  if (key === "style" && typeof value === "object") return toStyleString(value)
+  if (value === false) return
+
+  return value
 }
 
 export const normalizeProps = createNormalizer((props) => {
   const normalized: Dict = {}
 
   for (const key in props) {
-    normalized[toSvelteProp(key)] = toSveltePropValue(props[key])
+    normalized[toSvelteProp(key)] = toSveltePropValue(key, props[key])
   }
 
   return normalized
