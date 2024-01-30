11a9a8fd3f00c08b9859843ffdfc1f269b6d681b
Segun Adebayo <joseshegs@gmail.com>
2024-01-30 16:23:24 +0000

    chore: update example

--- examples/next-app/app/date-picker/controlled-range-picker/page.tsx
+++ examples/next-app/app/date-picker/controlled-range-picker/page.tsx
@@ -20,7 +20,7 @@ export default function Page() {
         Close
       </button>
 
-      <DateRangePicker open={open} />
+      <DateRangePicker open={open} onOpenChange={({ open }) => setOpen(open)} />
     </div>
   )
 }
