01f6557f32ac4fd5defb113069c2e2d1697c7e39
Segun Adebayo <joseshegs@gmail.com>
2024-02-09 21:25:47 +0000

    chore: update docs

--- website/components/machines/collapsible.tsx
+++ website/components/machines/collapsible.tsx
@@ -1,14 +1,9 @@
-import { Box, Center, Stack, Text } from "@chakra-ui/layout"
+import { Stack } from "@chakra-ui/layout"
+import { keyframes } from "@emotion/react"
 import * as collapsible from "@zag-js/collapsible"
 import { normalizeProps, useMachine } from "@zag-js/react"
 import { Button } from "components/button"
 import { useId } from "react"
-
-const collapsibleData = {
-  headline: "@Omikorin starred 4 repositories",
-  visibleItem: "@chakra-ui/zag-js",
-  items: ["@chakra-ui/ark-ui", "@chakra-ui/panda", "@chakra-ui/chakra-ui"],
-}
 
 type CollapsibleProps = {
   controls: {
@@ -16,6 +11,28 @@ type CollapsibleProps = {
     dir: "ltr" | "rtl"
   }
 }
+
+const slideDown = keyframes({
+  from: {
+    height: 0,
+    opacity: 0.01,
+  },
+  to: {
+    height: "var(--height)",
+    opacity: 1,
+  },
+})
+
+const slideUp = keyframes({
+  from: {
+    height: "var(--height)",
+    opacity: 1,
+  },
+  to: {
+    height: 0,
+    opacity: 0.01,
+  },
+})
 
 export function Collapsible(props: CollapsibleProps) {
   const [state, send] = useMachine(collapsible.machine({ id: useId() }), {
@@ -25,37 +42,30 @@ export function Collapsible(props: CollapsibleProps) {
   const api = collapsible.connect(state, send, normalizeProps)
 
   return (
-    <Stack width="400px" {...api.rootProps}>
-      <Center justifyContent={"space-between"}>
-        <Text>{collapsibleData.headline}</Text>
-        <Button size="sm" variant="green" {...api.triggerProps}>
-          {api.isOpen ? "Collapse" : "Expand"}
-        </Button>
-      </Center>
-      <Box
-        bg="bg-subtle"
-        padding="4"
-        borderWidth="1px"
-        fontSize="sm"
-        width="full"
-        rounded="md"
+    <Stack width="400px" align="start" {...api.rootProps}>
+      <Button size="sm" variant="green" {...api.triggerProps}>
+        Click to Toggle
+      </Button>
+      <Stack
+        overflow="hidden"
+        {...api.contentProps}
+        css={{
+          "&[data-state=open]": {
+            animation: `${slideDown} 110ms cubic-bezier(0, 0, 0.38, 0.9)`,
+          },
+          "&[data-state=closed]": {
+            animation: `${slideUp} 110ms cubic-bezier(0, 0, 0.38, 0.9)`,
+          },
+        }}
       >
-        <Text>{collapsibleData.visibleItem}</Text>
-      </Box>
-      <Stack {...api.contentProps}>
-        {collapsibleData.items.map((item) => (
-          <Box
-            key={item}
-            bg="bg-subtle"
-            padding="4"
-            borderWidth="1px"
-            fontSize="sm"
-            width="full"
-            rounded="md"
-          >
-            <Text>{item}</Text>
-          </Box>
-        ))}
+        <p>
+          Lorem dfd dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna sfsd. Ut enim ad minimdfd
+          v eniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+          ea commodo consequat. Excepteur sint occaecat cupidatat non proident,
+          sunt in culpa qui officia deserunt mollit anim id est laborum.{" "}
+          <a href="#">Some Link</a>
+        </p>
       </Stack>
     </Stack>
   )
