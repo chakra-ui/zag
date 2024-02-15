9fc1128ad341dabf25bd92009e3f074fb2927b92
Segun Adebayo <joseshegs@gmail.com>
2024-02-15 22:22:43 +0000

    chore: rm unused

--- packages/utilities/dom-query/src/get-windows-scroll.ts
+++
@@ -1,7 +0,0 @@
-import { getDocumentElement } from "./env"
-import { getBoundingClientRect } from "./get-bounding-client-rect"
-import { getScrollPosition } from "./get-scroll-position"
-
-export function getWindowScrollBarX(element: Element): number {
-  return getBoundingClientRect(getDocumentElement(element)).left + getScrollPosition(element as HTMLElement).scrollLeft
-}
