68a2cc3b2d6808e4f55374140580f47d6243d4ac
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2024-02-20 11:12:42 -0500

    chore(machines/nav-menu): export MachineApi type from package
    Co-authored-by: Michał Korczak <michal.k2001@gmail.com>

--- packages/machines/nav-menu/src/index.ts
+++ packages/machines/nav-menu/src/index.ts
@@ -1,4 +1,4 @@
 export { anatomy } from "./nav-menu.anatomy"
 export { connect } from "./nav-menu.connect"
 export { machine } from "./nav-menu.machine"
-export type { UserDefinedContext as Context } from "./nav-menu.types"
+export type { MachineApi as Api, UserDefinedContext as Context } from "./nav-menu.types"
