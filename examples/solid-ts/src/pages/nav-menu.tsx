3549c476cfbbb54faf8ca9b34a315cbe55902d1f
tylerapfledderer <tyler.pfledderer@gmail.com>
2024-02-20 11:45:35 -0500

    feat(nav-menu): create solidjs example

--- examples/solid-ts/src/pages/nav-menu.tsx
+++ examples/solid-ts/src/pages/nav-menu.tsx
@@ -1,26 +1,47 @@
-import * as nav-menu from "@zag-js/nav-menu"
-import { normalizeProps, useMachine, mergeProps } from "@zag-js/solid"
-import { createMemo, createUniqueId } from "solid-js"
-import { nav-menuControls, nav-menuData } from "@zag-js/shared"
+import * as navMenu from "@zag-js/nav-menu"
+import { normalizeProps, useMachine } from "@zag-js/solid"
+import { createMemo, createUniqueId, For } from "solid-js"
+import { navMenuControls, navMenuData } from "@zag-js/shared"
 import { StateVisualizer } from "../components/state-visualizer"
 import { Toolbar } from "../components/toolbar"
 import { useControls } from "../hooks/use-controls"
 
 export default function Page() {
-  const controls = useControls(nav-menuControls)
+  const controls = useControls(navMenuControls)
 
-  const [state, send] = useMachine(nav-menu.machine({ id: createUniqueId() }), {
+  const [state, send] = useMachine(navMenu.machine({ id: createUniqueId() }), {
     context: controls.context,
   })
 
-  const api = createMemo(() => nav-menu.connect(state, send, normalizeProps))
+  const api = createMemo(() => navMenu.connect(state, send, normalizeProps))
 
   return (
     <>
-      <main class="nav-menu"> 
-        <div {...api().rootProps}> 
-            
-        </div>
+      <main class="nav-menu">
+        <nav {...api().rootProps}>
+          <ul style={{ display: "flex", "list-style": "none" }}>
+            <For each={navMenuData}>
+              {({ menu, menuList }) => (
+                <li>
+                  <button {...api().getTriggerProps({ id: menu.id })}>{menu.label}</button>
+                  <div {...api().getPositionerProps({ id: menu.id })}>
+                    <ul {...api().getContentProps({ id: menu.id })} style={{ "list-style": "none" }}>
+                      <For each={menuList}>
+                        {(item) => (
+                          <li>
+                            <a href={item.href} {...api().getMenuItemProps({ id: item.id })}>
+                              {item.label}
+                            </a>
+                          </li>
+                        )}
+                      </For>
+                    </ul>
+                  </div>
+                </li>
+              )}
+            </For>
+          </ul>
+        </nav>
       </main>
 
       <Toolbar controls={controls.ui}>
