24f1112e86e9e521d6a5d2bad9123f472cdbbc62
anubra266 <anubra266@gmail.com>
2024-03-10 15:23:38 -0500

    chore: segmented control styles

--- website/components/machines/radio.tsx
+++ website/components/machines/radio.tsx
@@ -26,31 +26,33 @@ export function Radio(props: RadioProps) {
   const api = radio.connect(state, send, normalizeProps)
 
   return (
-    <div {...api.rootProps}>
-      <h2 {...api.labelProps}>Fruits</h2>
-      <div>
-        {items.map((opt) => (
-          <label key={opt.id} {...api.getItemProps({ value: opt.id })}>
-            <span {...api.getItemTextProps({ value: opt.id })}>
-              {opt.label}
-            </span>
-            <input
-              data-peer=""
-              {...api.getItemHiddenInputProps({ value: opt.id })}
-            />
-            <div {...api.getItemControlProps({ value: opt.id })}>
-              {api.value === opt.id && (
-                <svg
-                  viewBox="0 0 24 24"
-                  fill="currentcolor"
-                  transform="scale(0.7)"
-                >
-                  <path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z" />
-                </svg>
-              )}
-            </div>
-          </label>
-        ))}
+    <div className="radio">
+      <div {...api.rootProps}>
+        <h2 {...api.labelProps}>Fruits</h2>
+        <div>
+          {items.map((opt) => (
+            <label key={opt.id} {...api.getItemProps({ value: opt.id })}>
+              <span {...api.getItemTextProps({ value: opt.id })}>
+                {opt.label}
+              </span>
+              <input
+                data-peer=""
+                {...api.getItemHiddenInputProps({ value: opt.id })}
+              />
+              <div {...api.getItemControlProps({ value: opt.id })}>
+                {api.value === opt.id && (
+                  <svg
+                    viewBox="0 0 24 24"
+                    fill="currentcolor"
+                    transform="scale(0.7)"
+                  >
+                    <path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z" />
+                  </svg>
+                )}
+              </div>
+            </label>
+          ))}
+        </div>
       </div>
     </div>
   )
