438789a607f7a865b48b17c311452417aabce0b6
anubra266 <anubra266@gmail.com>
2024-03-10 13:33:24 -0500

    chore: tags input styles

--- website/components/machines/tags-input.tsx
+++ website/components/machines/tags-input.tsx
@@ -1,6 +1,5 @@
 import * as tagsInput from "@zag-js/tags-input"
 import { normalizeProps, useMachine } from "@zag-js/react"
-import { chakra } from "@chakra-ui/system"
 import { useId } from "react"
 
 export function TagsInput(props: any) {
@@ -15,55 +14,25 @@ export function TagsInput(props: any) {
   const api = tagsInput.connect(state, send, normalizeProps)
 
   return (
-    <chakra.div width="400px">
-      <chakra.div {...api.rootProps}>
-        <label {...api.labelProps}>Enter frameworks:</label>
-        <chakra.div
-          className="focus-outline"
-          bg="bg-subtle"
-          borderWidth="1px"
-          mt="2"
-          py="2px"
-          px="1"
-          {...api.controlProps}
-        >
-          {api.value.map((value, index) => {
-            const opt = { index, value }
-            return (
-              <span key={index} {...api.getItemProps(opt)}>
-                <chakra.div
-                  bg="bg-bold"
-                  px="2"
-                  display="inline-block"
-                  margin="4px"
-                  _highlighted={{ bg: "bg-primary-bold", color: "white" }}
-                  _disabled={{ opacity: 0.6 }}
-                  {...api.getItemPreviewProps(opt)}
-                >
-                  <span>{value}</span>
-                  <chakra.button ml="1" {...api.getItemDeleteTriggerProps(opt)}>
-                    &#x2715;
-                  </chakra.button>
-                </chakra.div>
-                <chakra.input
-                  px="2"
-                  width="10"
-                  outline="0"
-                  {...api.getItemInputProps(opt)}
-                />
-              </span>
-            )
-          })}
-          <chakra.input
-            margin="4px"
-            px="2"
-            placeholder="Add tag..."
-            _focus={{ outline: "0" }}
-            bg="bg-subtle"
-            {...api.inputProps}
-          />
-        </chakra.div>
-      </chakra.div>
-    </chakra.div>
+    <div {...api.rootProps}>
+      <label {...api.labelProps}>Enter frameworks:</label>
+      <div {...api.controlProps}>
+        {api.value.map((value, index) => {
+          const opt = { index, value }
+          return (
+            <span key={index} {...api.getItemProps(opt)}>
+              <div {...api.getItemPreviewProps(opt)}>
+                <span>{value}</span>
+                <button {...api.getItemDeleteTriggerProps(opt)}>
+                  &#x2715;
+                </button>
+              </div>
+              <input {...api.getItemInputProps(opt)} />
+            </span>
+          )
+        })}
+        <input placeholder="Add tag..." {...api.inputProps} />
+      </div>
+    </div>
   )
 }
