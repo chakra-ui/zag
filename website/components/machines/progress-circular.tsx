f9f83a9ad91394f588d3ee799f5209145491c326
anubra266 <anubra266@gmail.com>
2024-03-09 18:45:04 -0600

    chore: progress styles

--- website/components/machines/progress-circular.tsx
+++ website/components/machines/progress-circular.tsx
@@ -1,52 +1,6 @@
-import { HStack, VStack } from "@chakra-ui/layout"
-import { chakra, keyframes } from "@chakra-ui/system"
 import * as progress from "@zag-js/progress"
 import { normalizeProps, useMachine } from "@zag-js/react"
-import { Button } from "components/button"
 import { useId } from "react"
-
-const spin = keyframes({
-  from: { transform: "rotate(0deg)" },
-  to: { transform: "rotate(360deg)" },
-})
-
-const circleAnimation = keyframes({
-  "0%": {
-    strokeDasharray: "1, 400",
-    strokeDashoffset: "0",
-  },
-  "100%": {
-    strokeDasharray: "400, 400",
-    strokeDashoffset: "-260",
-  },
-})
-
-const Circle = chakra("svg", {
-  baseStyle: {
-    "--size": "120px",
-    "--thickness": "12px",
-    "&[data-state=indeterminate]": {
-      animation: `${spin} 2s linear infinite`,
-    },
-  },
-})
-
-const CircleTrack = chakra("circle", {
-  baseStyle: {
-    stroke: "bg-subtle",
-  },
-})
-
-const CircleRange = chakra("circle", {
-  baseStyle: {
-    stroke: "green.500",
-    transitionProperty: "stroke-dasharray, stroke",
-    transitionDuration: "0.6s",
-    "&[data-state=indeterminate]": {
-      animation: `${circleAnimation} 1.5s ease-in-out infinite`,
-    },
-  },
-})
 
 export function CircularProgress(props: any) {
   const [state, send] = useMachine(progress.machine({ id: useId() }), {
@@ -57,36 +11,26 @@ export function CircularProgress(props: any) {
 
   return (
     <div>
-      <VStack mb="4" spacing="5" {...api.rootProps}>
+      <div className="centre" {...api.rootProps}>
         <div {...api.labelProps}>Upload progress</div>
-        <Circle {...(api.circleProps as any)}>
-          <CircleTrack {...(api.circleTrackProps as any)} />
-          <CircleRange {...(api.circleRangeProps as any)} />
-        </Circle>
+        <svg {...(api.circleProps as any)}>
+          <circle {...(api.circleTrackProps as any)} />
+          <circle {...(api.circleRangeProps as any)} />
+        </svg>
         <div {...api.valueTextProps}>{api.valueAsString}</div>
-      </VStack>
+      </div>
 
-      <HStack>
-        <Button
-          size="sm"
-          variant="outline"
-          onClick={() => api.setValue((api.value ?? 0) - 20)}
-        >
+      <div>
+        <button onClick={() => api.setValue((api.value ?? 0) - 20)}>
           Decrease
-        </Button>
+        </button>
 
-        <Button
-          size="sm"
-          variant="outline"
-          onClick={() => api.setValue((api.value ?? 0) + 20)}
-        >
+        <button onClick={() => api.setValue((api.value ?? 0) + 20)}>
           Increase
-        </Button>
+        </button>
 
-        <Button size="sm" variant="outline" onClick={() => api.setValue(null)}>
-          Indeterminate
-        </Button>
-      </HStack>
+        <button onClick={() => api.setValue(null)}>Indeterminate</button>
+      </div>
     </div>
   )
 }
--- website/components/machines/progress-linear.tsx
+++ website/components/machines/progress-linear.tsx
@@ -1,40 +1,6 @@
-import { HStack, Stack } from "@chakra-ui/layout"
-import { chakra, keyframes } from "@chakra-ui/system"
 import * as progress from "@zag-js/progress"
 import { normalizeProps, useMachine } from "@zag-js/react"
-import { Button } from "components/button"
 import { useId } from "react"
-
-const Track = chakra("div", {
-  baseStyle: {
-    borderWidth: "1px",
-    height: "20px",
-    rounded: "sm",
-    bg: "bg-subtle",
-    overflow: "hidden",
-  },
-})
-
-const animation = keyframes({
-  from: { transform: "scaleX(1) translateX(var(--translate-x))" },
-  to: { transform: "scaleX(0) translateX(var(--translate-x))" },
-})
-
-const Range = chakra("div", {
-  baseStyle: {
-    "--translate-x": "-100%",
-    height: "100%",
-    transition: "width 0.2s ease-in-out",
-    bg: "green.500",
-    "&[dir=rtl]": {
-      "--translate-x": "100%",
-    },
-    "&[data-state=indeterminate]": {
-      width: "210%",
-      animation: `1s cubic-bezier(0.694, 0.0482, 0.335, 1) 0s infinite normal none running ${animation}`,
-    },
-  },
-})
 
 export function LinearProgress(props: any) {
   const [state, send] = useMachine(progress.machine({ id: useId() }), {
@@ -45,35 +11,25 @@ export function LinearProgress(props: any) {
 
   return (
     <div>
-      <Stack mb="4" {...api.rootProps}>
+      <div {...api.rootProps}>
         <div {...api.labelProps}>Upload progress</div>
-        <Track {...api.trackProps}>
-          <Range {...api.rangeProps} />
-        </Track>
+        <div {...api.trackProps}>
+          <div {...api.rangeProps} />
+        </div>
         <div {...api.valueTextProps}>{api.valueAsString}</div>
-      </Stack>
+      </div>
 
-      <HStack>
-        <Button
-          size="sm"
-          variant="outline"
-          onClick={() => api.setValue((api.value ?? 0) - 20)}
-        >
+      <div>
+        <button onClick={() => api.setValue((api.value ?? 0) - 20)}>
           Decrease
-        </Button>
+        </button>
 
-        <Button
-          size="sm"
-          variant="outline"
-          onClick={() => api.setValue((api.value ?? 0) + 20)}
-        >
+        <button onClick={() => api.setValue((api.value ?? 0) + 20)}>
           Increase
-        </Button>
+        </button>
 
-        <Button size="sm" variant="outline" onClick={() => api.setValue(null)}>
-          Indeterminate
-        </Button>
-      </HStack>
+        <button onClick={() => api.setValue(null)}>Indeterminate</button>
+      </div>
     </div>
   )
 }
