9fdbfdffd00a703f5e79ca1d9ae9e771e7cbdf9a
tylerapfledderer <tyler.pfledderer@gmail.com>
2024-03-12 00:33:49 -0400

    chore: export machine props

--- packages/machines/nav-menu/src/nav-menu.props.ts
+++ packages/machines/nav-menu/src/nav-menu.props.ts
@@ -2,5 +2,17 @@ import { createProps } from "@zag-js/types"
 import { createSplitProps } from "@zag-js/utils"
 import type { UserDefinedContext } from "./nav-menu.types"
 
-export const props = createProps<UserDefinedContext>()([])
+export const props = createProps<UserDefinedContext>()([
+  "activeId",
+  "activeLinkId",
+  "dir",
+  "getRootNode",
+  "highlightedLinkId",
+  "id",
+  "ids",
+  "onFocusOutside",
+  "onInteractOutside",
+  "onPointerDownOutside",
+  "orientation",
+])
 export const splitProps = createSplitProps<Partial<UserDefinedContext>>(props)
