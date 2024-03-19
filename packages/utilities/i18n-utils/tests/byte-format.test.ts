ebb53541ab4792cce63f051630386e1fe81a9966
Segun Adebayo <joseshegs@gmail.com>
2024-03-19 17:29:38 +0000

    test: fix

--- packages/utilities/i18n-utils/tests/byte-format.test.ts
+++ packages/utilities/i18n-utils/tests/byte-format.test.ts
@@ -28,4 +28,15 @@ describe("formatBytes", () => {
     const result = formatBytes(1500000000, "en-US", options)
     expect(result).toMatchInlineSnapshot(`"1.5 GB"`)
   })
+
+  test("sink", () => {
+    expect(formatBytes(1024)).toMatchInlineSnapshot(`"1.02 kB"`)
+    expect(formatBytes(1048576)).toMatchInlineSnapshot(`"1.05 MB"`)
+    expect(formatBytes(1073741824)).toMatchInlineSnapshot(`"1.07 GB"`)
+    expect(formatBytes(1099511627776)).toMatchInlineSnapshot(`"1.1 TB"`)
+    expect(formatBytes(1023)).toMatchInlineSnapshot(`"1.02 kB"`)
+    expect(formatBytes(1048575)).toMatchInlineSnapshot(`"1.05 MB"`)
+    expect(formatBytes(1073741823)).toMatchInlineSnapshot(`"1.07 GB"`)
+    expect(formatBytes(1099511627775)).toMatchInlineSnapshot(`"1.1 TB"`)
+  })
 })
