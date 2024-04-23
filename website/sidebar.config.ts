fabd78859c2c226101b60d9894b1d7019c0159c2
Segun Adebayo <joseshegs@gmail.com>
2024-04-23 15:48:28 +0100

    chore: switch to unmount on exit

--- website/sidebar.config.ts
+++ website/sidebar.config.ts
@@ -79,9 +79,9 @@ const sidebar: Record<"docs", SidebarItem[]> = {
         { type: "doc", label: "Nested Menu", id: "nested-menu" },
         { type: "doc", label: "Number Input", id: "number-input" },
         { type: "doc", label: "Pagination", id: "pagination" },
-        { type: "doc", label: "Presence", id: "presence", new: true },
         { type: "doc", label: "Pin Input", id: "pin-input" },
         { type: "doc", label: "Popover", id: "popover" },
+        { type: "doc", label: "Presence", id: "presence", new: true },
         {
           type: "doc",
           label: "Progress - Linear",
