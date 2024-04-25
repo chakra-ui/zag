fdd9c1ea5b9582dd6127605ea06e975d3675c89e
Segun Adebayo <joseshegs@gmail.com>
2024-04-25 14:43:30 +0100

    refactor: dont use toSorted

--- packages/utilities/collection/src/collection.ts
+++ packages/utilities/collection/src/collection.ts
@@ -146,7 +146,7 @@ export class Collection<T extends CollectionItem = CollectionItem> {
    * Sort the values based on their index
    */
   sort = (values: string[]): string[] => {
-    return values.toSorted(this.sortFn)
+    return [...values].sort(this.sortFn)
   }
 
   /**
