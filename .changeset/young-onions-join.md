fb26e3960845bab8f3c3f987de8c3e28b1b5e361
Christian Schröter <dev@grizzly.codes>
2024-05-23 18:30:11 +0200

    fix(pagination): add missing PageSizeChangeDetails

---
+++ .changeset/young-onions-join.md
@@ -0,0 +1,5 @@
+---
+"@zag-js/pagination": patch
+---
+
+Export missing PageSizeChangeDetails
--- packages/machines/pagination/src/index.ts
+++ packages/machines/pagination/src/index.ts
@@ -11,4 +11,5 @@ export type {
   ItemLabelDetails,
   ItemProps,
   PageChangeDetails,
+  PageSizeChangeDetails,
 } from "./pagination.types"
