89ebf4dd87fbaff0c83b7696ebf472c30718ad9e
Abraham <anubra266@gmail.com>
2024-05-27 09:06:36 -0500

    chore: simplify vanilla samples (#1516)
    * chore: simplify vanilla samples
    
    * chore: simplify
    
    ---------
    
    Co-authored-by: Segun Adebayo <joseshegs@gmail.com>

--- examples/vanilla-ts/src/checkbox.ts
+++ examples/vanilla-ts/src/checkbox.ts
@@ -1,45 +1,24 @@
 import * as checkbox from "@zag-js/checkbox"
 import { normalizeProps } from "./normalize-props"
 import { spreadProps } from "./spread-props"
+import { Component } from "./component"
 
-export class Checkbox {
-  rootEl: HTMLElement
-  service: ReturnType<typeof checkbox.machine>
-  api: checkbox.Api<any>
-
-  constructor(rootEl: HTMLElement | null, context: checkbox.Context) {
-    if (!rootEl) throw new Error("Root element not found")
-    this.rootEl = rootEl
-
-    this.service = checkbox.machine(context)
-    this.api = checkbox.connect(this.service.state, this.service.send, normalizeProps)
+export class Checkbox extends Component<checkbox.Context, checkbox.Api> {
+  initService(context: checkbox.Context) {
+    return checkbox.machine(context)
   }
 
-  init = () => {
-    const { service } = this
-    this.render()
-    this.service.subscribe(() => {
-      this.api = checkbox.connect(service.state, service.send, normalizeProps)
-      this.render()
-    })
-
-    this.service.start()
-  }
-
-  destroy = () => {
-    this.service.stop()
+  initApi() {
+    return checkbox.connect(this.service.state, this.service.send, normalizeProps)
   }
 
   render = () => {
     const rootEl = this.rootEl
     spreadProps(this.rootEl, this.api.rootProps)
-
     const controlEl = rootEl.querySelector<HTMLElement>(".checkbox-control")
     if (controlEl) spreadProps(controlEl, this.api.controlProps)
-
     const labelEl = rootEl.querySelector<HTMLElement>(".checkbox-label")
     if (labelEl) spreadProps(labelEl, this.api.labelProps)
-
     const inputEl = rootEl.querySelector<HTMLInputElement>(".checkbox-input")
     if (inputEl) spreadProps(inputEl, this.api.hiddenInputProps)
   }
