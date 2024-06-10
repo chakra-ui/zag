e8cb9b2b16aeca900ef56fd08c78c4e009673e46
Segun Adebayo <joseshegs@gmail.com>
2024-06-10 19:42:26 +0100

    fix: types

--- packages/machines/presence/src/presence.props.ts
+++ packages/machines/presence/src/presence.props.ts
@@ -1,4 +1,4 @@
 import { createProps } from "@zag-js/types"
 import type { UserDefinedContext } from "./presence.types"
 
-export const props = createProps<UserDefinedContext>()(["onExitComplete", "present"])
+export const props = createProps<UserDefinedContext>()(["onExitComplete", "present", "immediate"])
