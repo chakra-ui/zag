22eb9f4769725e3a750baf416cb7d89a79f33e17
anubra266 <anubra266@gmail.com>
2024-06-15 03:05:25 -0500

    chore: update code

--- website/lib/open-in-stackblitz.ts
+++ website/lib/open-in-stackblitz.ts
@@ -3,64 +3,57 @@ import sdk from "@stackblitz/sdk"
 const html = String.raw
 const js = String.raw
 
-function normalizeComp(comp: string) {
-  if (comp.includes("menu")) comp = "menu"
-  return comp
-}
+export async function openInStackblitz(component: string) {
+  const componentCode = await getComponentCode(component)
+  const style = await getStyle(component)
 
-async function getStyle(comp: string) {
-  comp = normalizeComp(comp)
-  const STYLES =
-    "https://raw.githubusercontent.com/chakra-ui/zag/main/website/styles/machines"
-  // "https://raw.githubusercontent.com/chakra-ui/zag/main/shared/src/css"
-  const stylePath = `${STYLES}/${comp}.css`
-  return fetch(stylePath).then((r) => r.text())
-}
-
-async function getComponent(comp: string) {
-  const COMPONENTS =
-    // "https://raw.githubusercontent.com/chakra-ui/zag/main/examples/next-ts/pages"
-    "https://raw.githubusercontent.com/chakra-ui/zag/main/website/components/machines"
-  const componentPath = `${COMPONENTS}/${comp}.tsx`
-  let component = await fetch(componentPath).then((r) => r.text())
-  component = component.replace("export function", "export default function")
-  return component
-}
-
-export async function openInStackblitz(comp: string) {
-  const component = await getComponent(comp)
-  const style = await getStyle(comp)
-
-  const invalidComponent = component === "404: Not Found"
+  const invalidComponent = componentCode === "404: Not Found"
   if (invalidComponent) {
-    console.log("Invalid component")
     return null
   }
 
-  sdk.openProject(
-    {
-      title: `${comp} - Zag`,
-      description: `${comp} component zagjs example`,
-      template: "node",
-      files: {
-        "tsconfig.json": JSON.stringify(tsconfig, null, 2),
-        "tsconfig.node.json": JSON.stringify(tsconfigNode, null, 2),
-        "package.json": JSON.stringify(getPackageJson(comp), null, 2),
-        "vite.config.ts": viteConfig,
-        "index.html": indexHtml,
-        "src/App.tsx": component,
-        "src/index.css": style,
-        "src/main.tsx": main,
-      },
-    },
-    {
-      // clickToLoad: true,
-    },
-  )
+  const files = {
+    "tsconfig.json": JSON.stringify(tsconfig, null, 2),
+    "package.json": JSON.stringify(getPackageJson(component), null, 2),
+    "vite.config.ts": viteConfig,
+    "index.html": indexHtml,
+    "src/App.tsx": componentCode,
+    "src/index.css": style,
+    "src/main.tsx": main,
+  }
+
+  sdk.openProject({
+    title: component,
+    description: `${component} component zagjs example`,
+    template: "node",
+    files,
+  })
 }
 
-const getPackageJson = (comp: string) => {
-  comp = normalizeComp(comp)
+function normalizeComponent(component: string) {
+  if (component.includes("menu")) component = "menu"
+  return component
+}
+
+async function getStyle(component: string) {
+  component = normalizeComponent(component)
+  const STYLES =
+    "https://raw.githubusercontent.com/chakra-ui/zag/main/website/styles/machines"
+  const stylePath = `${STYLES}/${component}.css`
+  return fetch(stylePath).then((r) => r.text())
+}
+
+async function getComponentCode(component: string) {
+  const COMPONENTS =
+    "https://raw.githubusercontent.com/chakra-ui/zag/main/website/components/machines"
+  const componentPath = `${COMPONENTS}/${component}.tsx`
+  let code = await fetch(componentPath).then((r) => r.text())
+  code = code.replace("export function", "export default function")
+  return code
+}
+
+const getPackageJson = (component: string) => {
+  component = normalizeComponent(component)
 
   return {
     name: "vite-react-typescript-starter",
@@ -77,7 +70,7 @@ const getPackageJson = (comp: string) => {
       react: "^18.3.1",
       "react-dom": "^18.3.1",
       "@zag-js/react": "latest",
-      [`@zag-js/${comp}`]: "latest",
+      [`@zag-js/${component}`]: "latest",
       "react-icons": "latest",
     },
     devDependencies: {
@@ -118,15 +111,15 @@ const indexHtml = html` <!doctype html>
   </html>`
 
 const main = js`import React from 'react'
-  import ReactDOM from 'react-dom/client'
-  import App from './App.tsx'
-  import './index.css'
-  
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <App controls={{} as any} />
-    </React.StrictMode>,
-  )`
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App controls={{} as any} />
+  </React.StrictMode>,
+)`
 
 const tsconfig = {
   compilerOptions: {
@@ -135,33 +128,16 @@ const tsconfig = {
     lib: ["ES2020", "DOM", "DOM.Iterable"],
     module: "ESNext",
     skipLibCheck: true,
-
-    /* Bundler mode */
     moduleResolution: "bundler",
     allowImportingTsExtensions: true,
     resolveJsonModule: true,
     isolatedModules: true,
     noEmit: true,
     jsx: "react-jsx",
-
-    /* Linting */
     strict: true,
     noUnusedLocals: true,
     noUnusedParameters: true,
     noFallthroughCasesInSwitch: true,
   },
   include: ["src"],
-  references: [{ path: "./tsconfig.node.json" }],
-}
-
-const tsconfigNode = {
-  compilerOptions: {
-    composite: true,
-    skipLibCheck: true,
-    module: "ESNext",
-    moduleResolution: "bundler",
-    allowSyntheticDefaultImports: true,
-    strict: true,
-  },
-  include: ["vite.config.ts"],
 }
