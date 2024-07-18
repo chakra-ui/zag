822ffb5c3617fa531cd1e33eda197f434a11ce86
Segun Adebayo <joseshegs@gmail.com>
2024-07-18 19:03:49 +0100

    chore: only snapshot in dev

--- packages/frameworks/react/src/use-service.ts
+++ packages/frameworks/react/src/use-service.ts
@@ -2,6 +2,7 @@ import type { MachineSrc, StateMachine as S } from "@zag-js/core"
 import { useRef } from "react"
 import { useConstant } from "./use-constant"
 import { useSafeLayoutEffect } from "./use-layout-effect"
+import { isDev } from "@zag-js/utils"
 
 export function useService<
   TContext extends Record<string, any>,
@@ -23,7 +24,10 @@ export function useService<
     const stateInit = hydratedState ?? snapshotRef.current
     service.start(stateInit)
     return () => {
-      snapshotRef.current = service.getHydrationState()
+      // HMR improvements for dev
+      if (isDev()) {
+        snapshotRef.current = service.getHydrationState()
+      }
       service.stop()
     }
   }, [])
