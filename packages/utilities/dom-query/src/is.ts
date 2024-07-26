7bcd60b66837aec91abfa51073e9bb21107006c6
ocavue <ocavue@gmail.com>
2024-07-26 11:29:57 +1000

    Revert "fix: improve is* checkers compatibility"
    This reverts commit 563ff42060d5b8717304359bdb1aaca45d4a90bb.

--- packages/utilities/dom-query/src/is.ts
+++ packages/utilities/dom-query/src/is.ts
@@ -1,18 +1,12 @@
-const ELEMENT_NODE: typeof Node.ELEMENT_NODE = 1
-const DOCUMENT_NODE: typeof Node.DOCUMENT_NODE = 9
-const DOCUMENT_FRAGMENT_NODE: typeof Node.DOCUMENT_FRAGMENT_NODE = 11
+export const isHTMLElement = (v: any): v is HTMLElement =>
+  typeof v === "object" && v?.nodeType === Node.ELEMENT_NODE && typeof v?.nodeName === "string"
 
-const isObject = (v: unknown): v is Record<string, unknown> => typeof v === "object" && v !== null
+export const isDocument = (el: any): el is Document => el.nodeType === Node.DOCUMENT_NODE
 
-export const isHTMLElement = (el: any): el is HTMLElement =>
-  typeof isObject(el) && el.nodeType === ELEMENT_NODE && typeof el.nodeName === "string"
-
-export const isDocument = (el: any): el is Document => isObject(el) && el.nodeType === DOCUMENT_NODE
-
-export const isWindow = (el: any): el is Window => isObject(el) && el === el.window
+export const isWindow = (el: any): el is Window => el != null && el === el.window
 
 export const isVisualViewport = (el: any): el is VisualViewport =>
-  isObject(el) && el.constructor.name === "VisualViewport"
+  el != null && el.constructor.name === "VisualViewport"
 
 export const getNodeName = (node: Node | Window): string => {
   if (isHTMLElement(node)) return node.localName || ""
@@ -23,7 +17,7 @@ export function isRootElement(node: Node): boolean {
   return ["html", "body", "#document"].includes(getNodeName(node))
 }
 
-export const isNode = (el: any): el is Node => isObject(el) && el.nodeType !== undefined
+export const isNode = (el: any): el is Node => el.nodeType !== undefined
 
 export const isShadowRoot = (el: any): el is ShadowRoot =>
- isNode(el) && el.nodeType === DOCUMENT_FRAGMENT_NODE && "host" in el
+  el && isNode(el) && el.nodeType === Node.DOCUMENT_FRAGMENT_NODE && "host" in el
