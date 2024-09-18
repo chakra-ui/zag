f29951bc0f74fd889c3fdfd798d122acfbcebd87
anubra266 <anubra266@gmail.com>
2024-09-18 02:23:35 -0500

    chore: update types

--- examples/qwik-ts/src/hooks/types.ts
+++ examples/qwik-ts/src/hooks/types.ts
@@ -1,9 +1,21 @@
-import type { StateMachine as S } from "@zag-js/core"
+import type { NoSerialize, QRL, Signal } from "@builder.io/qwik"
+import type { Machine, StateMachine as S } from "@zag-js/core"
 
-export type MachineOptions<
+export interface UseMachineOptions<
   TContext extends Record<string, any>,
   TState extends S.StateSchema,
-  TEvent extends S.EventObject = S.AnyEventObject,
-> = Omit<S.HookOptions<TContext, TState, TEvent>, "context"> & {
-  context?: Partial<TContext> | (() => Partial<TContext>)
+  TEvent extends S.EventObject,
+> {
+  qrl: () => Promise<NoSerialize<Machine<TContext, TState, TEvent>>>
+  initialState: NoSerialize<S.State<TContext, TState>>
 }
+
+export type UseMachineReturn<
+  TContext extends Record<string, any>,
+  TState extends S.StateSchema,
+  TEvent extends S.EventObject,
+> = [
+  Signal<NoSerialize<S.State<TContext, TState>> | null>,
+  QRL<(event: TEvent | string) => void>,
+  Signal<NoSerialize<Machine<TContext, TState, TEvent>> | null>,
+]
--- examples/qwik-ts/src/hooks/use-machine.ts
+++ examples/qwik-ts/src/hooks/use-machine.ts
@@ -1,26 +1,8 @@
-import { $, NoSerialize, QRL, Signal } from "@builder.io/qwik"
-import type { Machine, StateMachine as S } from "@zag-js/core"
+import { $ } from "@builder.io/qwik"
+import type { StateMachine as S } from "@zag-js/core"
 import { useService } from "./use-service"
 import { useSnapshot } from "./use-snapshot"
-
-interface UseMachineOptions<
-  TContext extends Record<string, any>,
-  TState extends S.StateSchema,
-  TEvent extends S.EventObject,
-> {
-  qrl: () => Promise<NoSerialize<Machine<TContext, TState, TEvent>>>
-  initialState: NoSerialize<S.State<TContext, TState>>
-}
-
-type UseMachineReturn<
-  TContext extends Record<string, any>,
-  TState extends S.StateSchema,
-  TEvent extends S.EventObject,
-> = [
-  Signal<NoSerialize<S.State<TContext, TState>> | null>,
-  QRL<(event: TEvent | string) => void>,
-  Signal<NoSerialize<Machine<TContext, TState, TEvent>> | null>,
-]
+import { UseMachineOptions, UseMachineReturn } from "~/hooks/types"
 
 export function useMachine<
   TContext extends Record<string, any>,
