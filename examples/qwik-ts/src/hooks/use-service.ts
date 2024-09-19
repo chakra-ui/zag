cf1369aeb3d7e4abef4323ededde63f3eac9e9a2
Abraham <anubra266@gmail.com>
2024-09-19 14:28:13 -0500

    feat(qwik): add controlled context for qwik js (#1868)
    * feat(qwik): controlled context
    
    * chore: use .ts
    
    * chore: separate hooks
    
    * chore: update types
    
    * chore: use signals
    
    * chore: cleanup
    
    * chore: revert to stores
    
    * chore: update types
    
    * chore: hydrate

---
+++ examples/qwik-ts/src/hooks/use-service.ts
@@ -0,0 +1,38 @@
+import { NoSerialize, noSerialize, useComputed$, useStore, useVisibleTask$ } from "@builder.io/qwik"
+import type { Machine, StateMachine as S } from "@zag-js/core"
+import { UseMachineOptions } from "~/hooks/types"
+
+export function useService<
+  TContext extends Record<string, any>,
+  TState extends S.StateSchema,
+  TEvent extends S.EventObject,
+>(props: UseMachineOptions<TContext, TState, TEvent>, options?: S.HookOptions<TContext, TState, TEvent>) {
+  const { qrl } = props
+  const { state: hydratedState, context } = options ?? {}
+
+  const store = useStore<{
+    service: NoSerialize<Machine<TContext, TState, TEvent>> | null
+  }>({
+    service: null,
+  })
+
+  useVisibleTask$(async ({ track, cleanup }) => {
+    // Load the service
+    const service = await qrl()
+
+    if (context) {
+      service!.setContext(context)
+      track(context)
+    }
+
+    service!.start(hydratedState)
+
+    store.service = noSerialize(service)
+
+    cleanup(() => {
+      service!.stop()
+    })
+  })
+
+  return useComputed$(() => store.service)
+}
---
+++ examples/qwik-ts/src/hooks/use-snapshot.ts
@@ -0,0 +1,37 @@
+import { NoSerialize, noSerialize, Signal, useComputed$, useSignal, useVisibleTask$ } from "@builder.io/qwik"
+import type { Machine, StateMachine as S } from "@zag-js/core"
+
+export function useSnapshot<
+  TContext extends Record<string, any>,
+  TState extends S.StateSchema,
+  TEvent extends S.EventObject,
+>(
+  serviceSignal: Signal<NoSerialize<Machine<TContext, TState, TEvent>> | null>,
+  options?: S.HookOptions<TContext, TState, TEvent>,
+  initialState?: NoSerialize<S.State<TContext, TState, S.AnyEventObject>>,
+) {
+  const { actions } = options ?? {}
+
+  const stateSignal = useSignal<NoSerialize<S.State<TContext, TState>> | null>(initialState)
+
+  useVisibleTask$(({ track, cleanup }) => {
+    track(() => serviceSignal.value)
+
+    const service = serviceSignal.value
+    if (!service) return
+
+    stateSignal.value = noSerialize(service.getState())
+
+    const unsubscribe = service.subscribe((state: any) => {
+      stateSignal.value = noSerialize(state)
+    })
+
+    service.setOptions({ actions })
+
+    cleanup(() => {
+      unsubscribe()
+    })
+  })
+
+  return useComputed$(() => stateSignal.value)
+}
