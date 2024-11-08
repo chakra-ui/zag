ab29fb86988b6e5e7945f3f9df2c9d6195a42d9c
Segun Adebayo <joseshegs@gmail.com>
2024-11-08 13:59:05 +0000

    refactor: improve autoresize

--- packages/utilities/auto-resize/src/autoresize-textarea.ts
+++ packages/utilities/auto-resize/src/autoresize-textarea.ts
@@ -1,19 +1,21 @@
-import { getComputedStyle } from "@zag-js/dom-query"
+import { getComputedStyle, getDocument, getWindow } from "@zag-js/dom-query"
 
 export const autoresizeTextarea = (el: HTMLTextAreaElement | null) => {
   if (!el) return
 
   const style = getComputedStyle(el)
-  const win = el.ownerDocument.defaultView || window
 
-  const onInput = () => {
+  const win = getWindow(el)
+  const doc = getDocument(el)
+
+  const resize = () => {
     el.style.height = "auto"
     const borderTopWidth = parseInt(style.borderTopWidth, 10)
     const borderBottomWidth = parseInt(style.borderBottomWidth, 10)
     el.style.height = `${el.scrollHeight + borderTopWidth + borderBottomWidth}px`
   }
 
-  el.addEventListener("input", onInput)
+  el.addEventListener("input", resize)
 
   const elementPrototype = Object.getPrototypeOf(el)
   const descriptor = Object.getOwnPropertyDescriptor(elementPrototype, "value")
@@ -22,15 +24,22 @@ export const autoresizeTextarea = (el: HTMLTextAreaElement | null) => {
     set() {
       // @ts-ignore
       descriptor?.set?.apply(this, arguments as unknown as [unknown])
-      onInput()
+      resize()
     },
   })
 
-  const resizeObserver = new win.ResizeObserver(() => onInput())
-
+  const resizeObserver = new win.ResizeObserver(() => resize())
   resizeObserver.observe(el)
 
+  const attrObserver = new win.MutationObserver(() => resize())
+  attrObserver.observe(el, { attributes: true, attributeFilter: ["rows", "placeholder"] })
+
+  doc.fonts?.addEventListener("loadingdone", resize)
+
   return () => {
-    el.removeEventListener("input", onInput)
+    el.removeEventListener("input", resize)
+    doc.fonts?.removeEventListener("loadingdone", resize)
+    resizeObserver.disconnect()
+    attrObserver.disconnect()
   }
 }
