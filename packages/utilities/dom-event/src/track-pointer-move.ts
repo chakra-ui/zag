f253f5d8aa1610141607aed5789a7085e5bcd450
Segun Adebayo <joseshegs@gmail.com>
2024-11-20 21:31:52 +0000

    refactor: remove unused velocity data

--- packages/utilities/dom-event/src/track-pointer-move.ts
+++ packages/utilities/dom-event/src/track-pointer-move.ts
@@ -7,13 +7,6 @@ interface Point {
   y: number
 }
 
-interface TimestampedPoint extends Point {
-  /**
-   * The time when the point was recorded.
-   */
-  timestamp: number
-}
-
 export interface PointerMoveDetails {
   /**
    * The current position of the pointer.
@@ -23,10 +16,6 @@ export interface PointerMoveDetails {
    * The event that triggered the move.
    */
   event: PointerEvent
-  /**
-   * The velocity of the pointer on the x and y axis.
-   */
-  velocity: Point
 }
 
 export interface PointerMoveHandlers {
@@ -43,11 +32,8 @@ export interface PointerMoveHandlers {
 export function trackPointerMove(doc: Document, handlers: PointerMoveHandlers) {
   const { onPointerMove, onPointerUp } = handlers
 
-  const history: TimestampedPoint[] = []
-
   const handleMove = (event: PointerEvent) => {
     const point = getEventPoint(event)
-    history.push({ ...point, timestamp: performance.now() })
 
     const distance = Math.sqrt(point.x ** 2 + point.y ** 2)
     const moveBuffer = event.pointerType === "touch" ? 10 : 5
@@ -60,7 +46,7 @@ export function trackPointerMove(doc: Document, handlers: PointerMoveHandlers) {
       return
     }
 
-    onPointerMove({ point, event, velocity: getVelocity(history, 0.1) })
+    onPointerMove({ point, event })
   }
 
   const cleanups = [
@@ -73,52 +59,5 @@ export function trackPointerMove(doc: Document, handlers: PointerMoveHandlers) {
 
   return () => {
     cleanups.forEach((cleanup) => cleanup())
-    history.length = 0
-  }
-}
-
-function lastDevicePoint(history: TimestampedPoint[]): TimestampedPoint {
-  return history[history.length - 1]
-}
-
-function ms(seconds: number): number {
-  return seconds * 1000
-}
-
-function sec(milliseconds: number): number {
-  return milliseconds / 1000
-}
-
-function getVelocity(history: TimestampedPoint[], timeDelta: number): Point {
-  if (history.length < 2) return { x: 0, y: 0 }
-
-  let i = history.length - 1
-  let timestampedPoint: TimestampedPoint | null = null
-  const lastPoint = lastDevicePoint(history)
-
-  while (i >= 0) {
-    timestampedPoint = history[i]
-    if (lastPoint.timestamp - timestampedPoint.timestamp > ms(timeDelta)) {
-      break
-    }
-    i--
-  }
-
-  if (!timestampedPoint) return { x: 0, y: 0 }
-
-  const time = sec(lastPoint.timestamp - timestampedPoint.timestamp)
-  if (time === 0) return { x: 0, y: 0 }
-
-  const currentVelocity = {
-    x: (lastPoint.x - timestampedPoint.x) / time,
-    y: (lastPoint.y - timestampedPoint.y) / time,
-  }
-
-  if (currentVelocity.x === Infinity) currentVelocity.x = 0
-  if (currentVelocity.y === Infinity) currentVelocity.y = 0
-
-  return {
-    x: Math.abs(currentVelocity.x),
-    y: Math.abs(currentVelocity.y),
   }
 }
