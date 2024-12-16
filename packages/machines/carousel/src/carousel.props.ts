f6203c22007a23cbc5ddec0d9a6992bb0fc5bbef
Segun Adebayo <joseshegs@gmail.com>
2024-12-16 18:07:01 +0000

    fix: types and add split props

--- packages/machines/carousel/src/carousel.props.ts
+++ packages/machines/carousel/src/carousel.props.ts
@@ -1,6 +1,6 @@
 import { createProps } from "@zag-js/types"
 import { createSplitProps } from "@zag-js/utils"
-import type { IndicatorProps, UserDefinedContext } from "./carousel.types"
+import type { IndicatorProps, ItemProps, UserDefinedContext } from "./carousel.types"
 
 export const props = createProps<UserDefinedContext>()([
   "dir",
@@ -11,7 +11,8 @@ export const props = createProps<UserDefinedContext>()([
   "snapIndex",
   "onSnapChange",
   "orientation",
-  "slidesPerView",
+  "slideCount",
+  "slidesPerPage",
   "spacing",
   "padding",
   "scrollBy",
@@ -25,3 +26,6 @@ export const splitProps = createSplitProps<Partial<UserDefinedContext>>(props)
 
 export const indicatorProps = createProps<IndicatorProps>()(["index", "readOnly"])
 export const splitIndicatorProps = createSplitProps<IndicatorProps>(indicatorProps)
+
+export const itemProps = createProps<ItemProps>()(["index", "snapAlign"])
+export const splitItemProps = createSplitProps<ItemProps>(itemProps)
