c6ebf272421c2f53d8e0cb82ef710be07af1e454
Segun Adebayo <joseshegs@gmail.com>
2024-12-19 18:21:29 +0000

    fix: demo

--- examples/next-ts/pages/carousel.tsx
+++ examples/next-ts/pages/carousel.tsx
@@ -14,7 +14,6 @@ export default function Page() {
       id: useId(),
       spacing: "20px",
       slidesPerPage: 2,
-      slidesPerMove: 1,
       slideCount: carouselData.length,
       allowMouseDrag: true,
     }),
