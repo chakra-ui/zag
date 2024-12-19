4bf006f8960a417db6eb17ea7fa5ad82f25aa9de
Segun Adebayo <joseshegs@gmail.com>
2024-12-19 18:25:14 +0000

    chore: react to slider per move changing

--- packages/machines/carousel/src/carousel.machine.ts
+++ packages/machines/carousel/src/carousel.machine.ts
@@ -56,6 +56,7 @@ export function machine(userContext: UserDefinedContext) {
 
       watch: {
         slidesPerPage: ["setSnapPoints"],
+        slidesPerMove: ["setSnapPoints"],
         page: ["scrollToPage", "focusIndicatorEl"],
         orientation: ["setSnapPoints", "scrollToPage"],
       },
