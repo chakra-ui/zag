05a83151d80c029f7ad1d241a8f4b8c1f63faa47
sana123 <saanatamboli@gmail.com>
2025-02-18 15:41:07 +0530

    Update deep-merge.ts
    fixing CVE-2024-57079

--- packages/core/src/deep-merge.ts
+++ packages/core/src/deep-merge.ts
@@ -1,16 +1,22 @@
 import { compact, isPlainObject } from "@zag-js/utils"
 
-export function deepMerge<T extends Record<string, any>>(source: T, ...objects: T[]): T {
+export function deepMerge<T extends Record<string, any>>(source: any, ...objects: any[]): T {
   for (const obj of objects) {
-    const target = compact(obj)
-    for (const key in target) {
-      if (isPlainObject(obj[key])) {
-        if (!source[key]) {
-          source[key] = {} as any
+    if (Object.prototype.hasOwnProperty.call(objects, obj)) {
+      if (obj === '__proto__' || obj === 'constructor' || obj === 'prototype') {
+        continue;
+      }
+
+      const target = compact(obj)
+      for (const key in target) {
+        if (isPlainObject(obj[key])) {
+          if (!source[key]) {
+            source[key] = {} as any
+          }
+          deepMerge(source[key], obj[key])
+        } else {
+          source[key] = obj[key]
         }
-        deepMerge(source[key], obj[key])
-      } else {
-        source[key] = obj[key]
       }
     }
   }
