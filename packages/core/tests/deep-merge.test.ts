4eece26e23f9b6e745124667dd0cdb59cc36ae85
Segun Adebayo <joseshegs@gmail.com>
2025-02-20 19:18:45 +0000

    test: add more deep merge tests

--- packages/core/tests/deep-merge.test.ts
+++ packages/core/tests/deep-merge.test.ts
@@ -97,4 +97,146 @@ describe("deepMerge", () => {
     const obj2 = { b: undefined }
     expect(deepMerge<any>(obj1, obj2)).toEqual({ a: undefined, b: undefined })
   })
+
+  test("merges nested objects recursively", () => {
+    const obj1 = {
+      a: { x: 1, y: 2 },
+      b: { z: 3 },
+    }
+    const obj2 = {
+      a: { y: 4, w: 5 },
+      b: { z: 6 },
+    }
+    expect(deepMerge<any>(obj1, obj2)).toEqual({
+      a: { x: 1, y: 4, w: 5 },
+      b: { z: 6 },
+    })
+  })
+
+  test("handles multiple levels of nesting", () => {
+    const obj1 = {
+      level1: {
+        level2: {
+          level3: { a: 1 },
+        },
+      },
+    }
+    const obj2 = {
+      level1: {
+        level2: {
+          level3: { b: 2 },
+        },
+      },
+    }
+    expect(deepMerge<any>(obj1, obj2)).toEqual({
+      level1: {
+        level2: {
+          level3: { a: 1, b: 2 },
+        },
+      },
+    })
+  })
+
+  test("preserves unmodified nested objects", () => {
+    const obj1 = {
+      unchanged: { a: 1 },
+      changed: { b: 2 },
+    }
+    const obj2 = {
+      changed: { c: 3 },
+    }
+    expect(deepMerge<any>(obj1, obj2)).toEqual({
+      unchanged: { a: 1 },
+      changed: { b: 2, c: 3 },
+    })
+  })
+
+  test("handles mixed nested and flat properties", () => {
+    const obj1 = {
+      flat: 1,
+      nested: { a: 2 },
+    }
+    const obj2 = {
+      flat: 3,
+      nested: { b: 4 },
+    }
+    expect(deepMerge<any>(obj1, obj2)).toEqual({
+      flat: 3,
+      nested: { a: 2, b: 4 },
+    })
+  })
+
+  test("handles mixed object and primitive values", () => {
+    const obj1 = {
+      key: "string value",
+      another: { nested: true },
+    }
+    const obj2 = {
+      key: { foo: "bar" },
+      another: 42,
+    }
+    expect(deepMerge<any>(obj1, obj2)).toEqual({
+      key: { foo: "bar" },
+      another: 42,
+    })
+  })
+
+  test("handles mixed primitive and object values (reverse case)", () => {
+    const obj1 = {
+      key: { foo: "bar" },
+      another: 42,
+    }
+    const obj2 = {
+      key: "string value",
+      another: { nested: true },
+    }
+    expect(deepMerge<any>(obj1, obj2)).toEqual({
+      key: "string value",
+      another: { nested: true },
+    })
+  })
+
+  test("handles undefined values", () => {
+    const obj1 = {
+      a: 1,
+      b: undefined,
+      c: { d: undefined },
+    }
+    const obj2 = {
+      b: 2,
+      c: { d: 3 },
+    }
+    expect(deepMerge<any>(obj1, obj2)).toEqual({
+      a: 1,
+      b: 2,
+      c: { d: 3 },
+    })
+  })
+
+  test("ignores undefined objects in merge", () => {
+    const obj1 = {
+      a: 1,
+      b: { c: 2 },
+    }
+    const obj2 = undefined
+    expect(deepMerge<any>(obj1, obj2)).toEqual({
+      a: 1,
+      b: { c: 2 },
+    })
+  })
+
+  test("handles undefined nested objects", () => {
+    const obj1 = {
+      a: { b: { d: 1 } },
+      c: { e: 2 },
+    }
+    const obj2 = {
+      a: { b: undefined },
+      c: undefined,
+    }
+    expect(deepMerge<any>(obj1, obj2)).toEqual({
+      a: { b: { d: 1 } },
+      c: { e: 2 },
+    })
+  })
 })
