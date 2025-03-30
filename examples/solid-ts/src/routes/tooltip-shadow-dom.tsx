9d870990c729e2d9704d238a552c18b668163014
Segun Adebayo <joseshegs@gmail.com>
2025-03-30 15:38:07 +0100

    chore: add solid shadom dom examples

---
+++ examples/solid-ts/src/routes/tooltip-shadow-dom.tsx
@@ -0,0 +1,33 @@
+import { normalizeProps, useMachine } from "@zag-js/solid"
+import * as tooltip from "@zag-js/tooltip"
+import styles from "@zag-js/shared/src/style.css?inline"
+import { createMemo, createSignal, Show } from "solid-js"
+import { Portal } from "solid-js/web"
+
+export default function Page() {
+  let mountRef!: HTMLElement
+  const [shadowRef, setShadowRef] = createSignal<HTMLDivElement | null>(null)
+
+  const getRootNode = () => shadowRef()?.shadowRoot!
+
+  const service = useMachine(tooltip.machine, { id: "1", getRootNode })
+  const api = createMemo(() => tooltip.connect(service, normalizeProps))
+
+  return (
+    <main ref={mountRef}>
+      <p>Testing</p>
+      <Portal ref={setShadowRef} useShadow mount={mountRef}>
+        <style>{styles}</style>
+        <button {...api().getTriggerProps()}>Hover me</button>
+        <Show when={api().open}>
+          <Portal mount={getRootNode()?.getElementById("portal-root")!}>
+            <div {...api().getPositionerProps()}>
+              <div {...api().getContentProps()}>Tooltip</div>
+            </div>
+          </Portal>
+        </Show>
+        <div id="portal-root" />
+      </Portal>
+    </main>
+  )
+}
