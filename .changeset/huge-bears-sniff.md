2fedd2996dedb8f6ffa5afe75d0aa0f60b181752
Segun Adebayo <joseshegs@gmail.com>
2025-05-25 17:36:54 +0100

    fix(svelte): microtask + flushsync

---
+++ .changeset/huge-bears-sniff.md
@@ -0,0 +1,5 @@
+---
+"@zag-js/svelte": patch
+---
+
+Fix issue with microtask timing in svelte `5.28.3+`
--- packages/frameworks/svelte/src/machine.svelte.ts
+++ packages/frameworks/svelte/src/machine.svelte.ts
@@ -287,7 +287,7 @@ function useProp<T>(value: () => T) {
 }
 
 function flush(fn: VoidFunction) {
-  queueMicrotask(() => {
-    flushSync(() => fn())
+  flushSync(() => {
+    queueMicrotask(() => fn())
   })
 }
