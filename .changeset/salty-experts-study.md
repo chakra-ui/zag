ff4c244549019680ada2afc073102c523a428524
Segun Adebayo <joseshegs@gmail.com>
2025-06-13 16:09:26 +0100

    refactor: download file

---
+++ .changeset/salty-experts-study.md
@@ -0,0 +1,5 @@
+---
+"@zag-js/file-utils": patch
+---
+
+Improve download file function to handle webview scenarios
--- packages/utilities/file-utils/src/download-file.ts
+++ packages/utilities/file-utils/src/download-file.ts
@@ -1,8 +1,3 @@
-export function isMSEdge(win: Window): win is Window & { navigator: { msSaveOrOpenBlob: Function } } {
-  // @ts-ignore
-  return Boolean(win.navigator && win.navigator.msSaveOrOpenBlob)
-}
-
 export interface DownloadFileOptions {
   /**
    * The name of the file
@@ -19,35 +14,90 @@ export interface DownloadFileOptions {
   /**
    * The window environment
    */
-  win: typeof window
+  win?: typeof window
+  /**
+   * Whether to add a BOM (Byte Order Mark) to the file.
+   * Useful for CSV files.
+   */
+  appendBOM?: boolean
+  /**
+   * The timeout in milliseconds to revoke the object URL.
+   * This is a safeguard for when the browser has not finished reading the
+   * file data from memory before the URL is revoked.
+   * @default 0
+   */
+  revokeTimeout?: number
+}
+
+const BOM_REGEX = /^\s*(?:text\/\S*|application\/xml|\S*\/\S*\+xml)\s*;.*charset\s*=\s*utf-8/i
+const MAC_REGEX = /Macintosh/
+const APPLE_WEBKIT_REGEX = /AppleWebKit/
+const SAFARI_REGEX = /Safari/
+
+function getBlob(blobOrString: Blob | File | string, type: string, appendBOM?: boolean) {
+  let blob = typeof blobOrString === "string" ? new Blob([blobOrString], { type }) : blobOrString
+  if (appendBOM && BOM_REGEX.test(blob.type)) {
+    return new Blob([String.fromCharCode(0xfeff), blob], { type: blob.type })
+  }
+  return blob
+}
+
+function isMSEdge(win: any): win is Window & { navigator: { msSaveOrOpenBlob: Function } } {
+  return Boolean(win.navigator && win.navigator.msSaveOrOpenBlob)
+}
+
+function isWebView(win: Window) {
+  return (
+    win.navigator &&
+    MAC_REGEX.test(win.navigator.userAgent) &&
+    APPLE_WEBKIT_REGEX.test(win.navigator.userAgent) &&
+    !SAFARI_REGEX.test(win.navigator.userAgent)
+  )
 }
 
 export function downloadFile(options: DownloadFileOptions) {
-  const { file, win, type, name } = options
+  const { file, win = window, type, name, appendBOM, revokeTimeout = 0 } = options
 
   const doc = win.document
 
-  const obj = typeof file === "string" ? new Blob([file], { type }) : file
-  const fileName = file instanceof File ? name || file.name : name
+  const blob = getBlob(file, type, appendBOM)
+  const fileName = (file instanceof File ? name || file.name : name) || "file-download"
 
   if (isMSEdge(win)) {
-    win.navigator.msSaveOrOpenBlob(obj, fileName || "file-download")
+    win.navigator.msSaveOrOpenBlob(blob, fileName)
     return
   }
 
-  const url = win.URL.createObjectURL(obj)
+  const isMacOSWebView = isWebView(win)
 
   const anchor = doc.createElement("a")
-  anchor.style.display = "none"
-  anchor.href = url
-  anchor.rel = "noopener"
-  anchor.download = fileName || "file-download"
+  const canUseDownload = "download" in anchor && !isMacOSWebView
 
-  doc.documentElement.appendChild(anchor)
-  anchor.click()
+  if (canUseDownload) {
+    const url = win.URL.createObjectURL(blob)
+    anchor.href = url
+    anchor.rel = "noopener"
+    anchor.download = fileName
+    anchor.style.display = "none"
+
+    doc.body.appendChild(anchor)
+    anchor.dispatchEvent(new win.MouseEvent("click"))
+
+    setTimeout(() => {
+      win.URL.revokeObjectURL(url)
+      anchor.remove()
+    }, revokeTimeout)
+
+    return
+  }
+
+  const url = win.URL.createObjectURL(blob)
+  const popup = win.open(url, "_blank")
+  if (!popup) {
+    win.location.href = url
+  }
 
   setTimeout(() => {
     win.URL.revokeObjectURL(url)
-    anchor.remove()
-  }, 0)
+  }, revokeTimeout)
 }
