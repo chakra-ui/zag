e5f698d082ea8ae7f9f45958c4e319de7c7b6107
Segun Adebayo <joseshegs@gmail.com>
2025-06-15 17:43:09 +0100

    refactor: link navigation

---
+++ .changeset/thick-animals-nail.md
@@ -0,0 +1,5 @@
+---
+"@zag-js/dom-query": patch
+---
+
+Fix issue in Svelte where menu and combobox items rendered as links don't work.
--- packages/utilities/dom-query/src/navigate.ts
+++ packages/utilities/dom-query/src/navigate.ts
@@ -1,8 +1,12 @@
+import { getWindow } from "./node"
 import { isFirefox } from "./platform"
 import { queueBeforeEvent } from "./raf"
 
 export function clickIfLink(el: HTMLAnchorElement) {
-  const click = () => el.click()
+  const click = () => {
+    const win = getWindow(el)
+    el.dispatchEvent(new win.MouseEvent("click"))
+  }
   if (isFirefox()) {
     queueBeforeEvent(el, "keyup", click)
   } else {
