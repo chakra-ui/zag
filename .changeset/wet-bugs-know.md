210c92dec281b789f5ead583817c13e49b2b83d0
Segun Adebayo <joseshegs@gmail.com>
2025-07-21 20:30:30 +0100

    Revert "chore(date-picker): use runtime timezone (#2592)"
    This reverts commit 624718f0345c87cd171a45b4722860d5b48f4f32.

--- .changeset/wet-bugs-know.md
+++
@@ -1,5 +0,0 @@
----
-"@zag-js/date-picker": patch
----
-
-Use runtime for default `timeZone`
--- packages/machines/date-picker/src/date-picker.types.ts
+++ packages/machines/date-picker/src/date-picker.types.ts
@@ -103,10 +103,8 @@ export interface DatePickerProps extends DirectionProperty, CommonProperties {
    */
   name?: string | undefined
   /**
-   * The timezone to use, in `IANA` format.
-   * @default "Intl.DateTimeFormat().resolvedOptions().timeZone"
-   *
-   * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Temporal/ZonedDateTime#time_zones_and_offsets
+   * The time zone to use
+   * @default "UTC"
    */
   timeZone?: string | undefined
   /**
