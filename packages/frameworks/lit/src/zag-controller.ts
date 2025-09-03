839395ca250677c790bd5e31a9a50f6e889bc20f
Nik Paro <niklas.paro@gmail.com>
2025-09-03 17:51:01 +0300

    fix: refine ZagController

--- packages/frameworks/lit/src/zag-controller.ts
+++ packages/frameworks/lit/src/zag-controller.ts
@@ -2,48 +2,35 @@ import type { Machine, MachineSchema } from "@zag-js/core"
 import type { ReactiveController, ReactiveControllerHost } from "lit"
 import { LitMachine } from "./machine"
 
-export class ZagController<T extends MachineSchema> implements ReactiveController {
-  private machine: LitMachine<T>
-  public api: any // Will be set in constructor and updated on changes
+export class ZagController<TSchema extends MachineSchema> implements ReactiveController {
+  private machine: LitMachine<TSchema>
 
   constructor(
     private host: ReactiveControllerHost,
-    machineConfig: Machine<T>,
-    props: Partial<T["props"]> = {},
+    machineConfig: Machine<TSchema>,
+    getProps?: () => Partial<TSchema["props"]>,
   ) {
-    this.machine = new LitMachine(machineConfig, props)
+    this.machine = new LitMachine(machineConfig, getProps)
+
+    // Register for lifecycle updates
     host.addController(this)
   }
 
   hostConnected() {
+    // Start the machine when the host is connected
     this.machine.subscribe(() => {
-      // Update API when machine state changes
-      this.updateApi()
       // Request Lit component update
       this.host.requestUpdate()
     })
     this.machine.start()
-    this.updateApi()
-    // Trigger initial update to sync with host element
-    this.host.requestUpdate()
   }
 
   hostDisconnected() {
     this.machine.stop()
   }
 
-  private updateApi() {
-    // This will be implemented once we have the connect function
-    // For now, just expose the service
-    this.api = this.machine.service
-  }
-
   // Expose machine methods for advanced usage
   get service() {
     return this.machine.service
-  }
-
-  send = (event: any) => {
-    this.machine.send(event)
   }
 }
