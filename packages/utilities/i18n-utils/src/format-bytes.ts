bf0a68334bdc136f1ea3deb7310981f80c0ea3cc
Ivan Kalinin <12299212+IvanKalinin@users.noreply.github.com>
2025-09-17 21:48:15 -0700

    Introduced definition property in format bytes options.

--- packages/utilities/i18n-utils/src/format-bytes.ts
+++ packages/utilities/i18n-utils/src/format-bytes.ts
@@ -4,6 +4,8 @@ const bitPrefixes = ["", "kilo", "mega", "giga", "tera"]
 const bytePrefixes = ["", "kilo", "mega", "giga", "tera", "peta"]
 
 export interface FormatBytesOptions {
+  precision?: number | undefined
+  definition?: "binary" | "decimal" | undefined
   unit?: "bit" | "byte" | undefined
   unitDisplay?: "long" | "short" | "narrow" | undefined
 }
@@ -12,7 +14,9 @@ export const formatBytes = (bytes: number, locale = "en-US", options: FormatByte
   if (isNaN(bytes)) return ""
   if (bytes === 0) return "0 B"
 
-  const { unit = "byte", unitDisplay = "short" } = options
+  const { definition = "decimal", precision = 3, unit = "byte", unitDisplay = "short" } = options
+
+  const factor = definition === "binary" ? 1024 : 1000
 
   const prefix = unit === "bit" ? bitPrefixes : bytePrefixes
   const index = Math.max(0, Math.min(Math.floor(Math.log10(bytes) / 3), prefix.length - 1))
@@ -20,7 +24,7 @@ export const formatBytes = (bytes: number, locale = "en-US", options: FormatByte
   const _unit = prefix[index] + unit
   const _unitDisplay = unitDisplay || "short"
 
-  const v = parseFloat((bytes / Math.pow(1000, index)).toPrecision(3))
+  const v = parseFloat((bytes / Math.pow(factor, index)).toPrecision(precision))
 
   return formatNumber(v, locale, {
     style: "unit",
