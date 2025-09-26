8e9b0dc36cfc86a60a5b6c0c1e68e5ea1e18dd7c
Nelson Lai <me@nelsonlai.dev>
2025-09-26 19:20:52 +0800

    refactor: correct type

--- packages/machines/image-cropper/src/image-cropper.props.ts
+++ packages/machines/image-cropper/src/image-cropper.props.ts
@@ -4,4 +4,4 @@ import type { ImageCropperProps } from "./image-cropper.types"
 
 export const props = createProps<ImageCropperProps>()(["id", "ids", "dir", "getRootNode"])
 
-export const splitProps = createSplitProps<Partial<Partial<ImageCropperProps>>>(props)
+export const splitProps = createSplitProps<Partial<ImageCropperProps>>(props)
