39f1124f8a9af0046388a94171e522f65f644987
Segun Adebayo <joseshegs@gmail.com>
2025-11-01 20:27:48 +0000

    docs: improve mdx hmr

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -15,12 +15,4 @@ const nextConfig = {
   ],
 }
 
-const isDev = process.argv.indexOf("dev") !== -1
-const isBuild = process.argv.indexOf("build") !== -1
-if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
-  process.env.VELITE_STARTED = "1"
-  const { build } = await import("velite")
-  await build({ watch: isDev, clean: !isDev })
-}
-
 export default nextConfig
