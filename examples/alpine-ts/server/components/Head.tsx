e7c065431ef57392456d67f81c092ca574326eed
nilpotential <nilpotential@proton.me>
2025-11-19 23:19:48 +0900

    feat(alpine): add style script

--- examples/alpine-ts/server/components/Head.tsx
+++ examples/alpine-ts/server/components/Head.tsx
@@ -4,6 +4,7 @@ export default function Head() {
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
       <link rel="icon" type="image/svg+xml" href="/nitro.svg" />
+      <script type="module" src="/scripts/style.ts"></script>
       <title>Nitro + Vite</title>
       <slot />
     </head>
