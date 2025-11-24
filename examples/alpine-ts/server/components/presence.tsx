040dee1e81facbb8f0aed51e97251be34b7c8622
nilpotential <nilpotential@proton.me>
2025-11-25 08:45:27 +0900

    feat(alpine): add bottom-sheet example

--- examples/alpine-ts/server/components/presence.tsx
+++ examples/alpine-ts/server/components/presence.tsx
@@ -1,10 +1,13 @@
-export function Presence() {
+export function Presence(props: any) {
   return (
     <div
+      {...props}
       data-scope="presence"
       x-data="{present: !hidden}"
       x-presence="{ present }"
       x-bind:data-state="$presence.skip ? undefined : present ? 'open' : 'closed'"
-    />
+    >
+      <slot />
+    </div>
   )
 }
