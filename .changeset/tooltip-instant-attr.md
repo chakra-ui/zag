28c05354dfda5de5c2812cc9757d71cfcb688f34
Segun Adebayo <joseshegs@gmail.com>
2026-01-10 15:00:52 +0200

    feat: tooltip [data-instant]

---
+++ .changeset/tooltip-instant-attr.md
@@ -0,0 +1,6 @@
+---
+"@zag-js/tooltip": patch
+---
+
+Add `data-instant` attribute to tooltip content to indicate when animations should be instant (e.g., when switching between tooltips quickly)
+
--- packages/machines/tooltip/src/tooltip.store.ts
+++ packages/machines/tooltip/src/tooltip.store.ts
@@ -1,3 +1,7 @@
 import { createStore } from "@zag-js/utils"
 
-export const store = createStore<{ id: string | null }>({ id: null })
+export const store = createStore<{ id: string | null; prevId: string | null; instant: boolean }>({
+  id: null,
+  prevId: null,
+  instant: false,
+})
