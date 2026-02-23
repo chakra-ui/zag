e7cfee2118f849bf914b2f9faf53452a07bc166a
jramke <jstrmk@gmail.com>
2026-02-23 21:25:42 +0100

    fix(vanilla): styles prop with mergeProps

--- packages/frameworks/vanilla/src/index.ts
+++ packages/frameworks/vanilla/src/index.ts
@@ -1,4 +1,4 @@
-export { mergeProps } from "@zag-js/core"
+export { mergeProps } from "./merge-props"
 export { normalizeProps } from "./normalize-props"
 export { spreadProps, type Attrs } from "./spread-props"
 export { VanillaMachine } from "./machine"
