dc1e5c997bd03a4e3784aadf45bc2a53db551b3a
Chang Min Lee <nilpotential@proton.me>
2026-03-09 20:19:06 +0900

    feat(alpine): listbox

--- examples/alpine-ts/scripts/listbox.ts
+++ examples/alpine-ts/scripts/listbox.ts
@@ -3,6 +3,13 @@ import { listboxControls, selectData } from "@zag-js/shared"
 import Alpine from "alpinejs"
 import { useData, usePlugin } from "../lib"
 
+const items = [
+  { label: "React", value: "react" },
+  { label: "Vue", value: "vue" },
+  { label: "Solid", value: "solid" },
+]
+
+Alpine.magic("items", () => items)
 Alpine.magic("selectData", () => selectData)
 Alpine.data("listbox", useData(listboxControls))
 Alpine.plugin(usePlugin("listbox", listbox))
