9b778f66374c49e4a72d2373dbca995c9f634f14
Chang Min Lee <nilpotential@proton.me>
2026-03-09 18:20:02 +0900

    feat(alpine): navigation menu

--- examples/alpine-ts/server/components/presence.tsx
+++ examples/alpine-ts/server/components/presence.tsx
@@ -1,11 +1,13 @@
 export function Presence(props: any) {
   return (
     <div
-      {...props}
-      data-scope="presence"
-      x-data="{present: !hidden}"
+      x-data="{present: !$el.hidden}"
       x-presence="{ present }"
-      x-bind:data-state="$presence.skip ? undefined : present ? 'open' : 'closed'"
+      x-ref="presence"
+      x-init="$presence().setNode($refs.presence)"
+      data-scope="presence"
+      x-bind:data-state="$presence().skip ? undefined : present ? 'open' : 'closed'"
+      {...props}
     >
       <slot />
     </div>
---
+++ examples/alpine-ts/server/routes/navigation-menu/basic.tsx
@@ -0,0 +1,106 @@
+import { navigationMenuControls } from "@zag-js/shared"
+import { ChevronDown } from "lucide-static"
+import { defineHandler } from "nitro/h3"
+import { Controls } from "../../components/controls"
+import { Head } from "../../components/head"
+import { Nav } from "../../components/nav"
+import { Presence } from "../../components/presence"
+import { StateVisualizer } from "../../components/state-visualizer"
+import { Toolbar } from "../../components/toolbar"
+
+export default defineHandler((event) => {
+  const renderLinks = (opts: { value: string; items: string[] }) => {
+    const { value, items } = opts
+    return items.map((item, index) => (
+      <a href="#" x-navigation-menu:link={`{value: '${value}'}`}>
+        {item}
+      </a>
+    ))
+  }
+
+  return (
+    <html>
+      <Head>
+        <script type="module" src="/scripts/navigation-menu.ts"></script>
+      </Head>
+
+      <body>
+        <div class="page" x-data="navigationMenu" x-navigation-menu="{id: $id('menu'), ...context}">
+          <Nav currentComponent={event.context.currentComponent as string} />
+
+          <main class="navigation-menu basic">
+            <div x-navigation-menu:root>
+              <div x-navigation-menu:list>
+                <div x-navigation-menu:item="{value: 'products'}">
+                  <button x-navigation-menu:trigger="{value: 'products'}">
+                    Products
+                    {html(ChevronDown)}
+                  </button>
+                  <Presence x-navigation-menu:content="{value: 'products'}">
+                    <Presence x-navigation-menu:indicator>
+                      <div x-navigation-menu:arrow />
+                    </Presence>
+                    {renderLinks({
+                      value: "products",
+                      items: [
+                        "Analytics Platform",
+                        "Customer Engagement",
+                        "Marketing Automation",
+                        "Data Integration",
+                        "Enterprise Solutions",
+                        "API Documentation",
+                      ],
+                    })}
+                  </Presence>
+                </div>
+
+                <div x-navigation-menu:item="{value: 'company'}">
+                  <button x-navigation-menu:trigger="{value: 'company'}">
+                    Company
+                    {html(ChevronDown)}
+                  </button>
+                  <Presence x-navigation-menu:content="{value: 'company'}">
+                    <Presence x-navigation-menu:indicator>
+                      <div x-navigation-menu:arrow />
+                    </Presence>
+                    {renderLinks({
+                      value: "company",
+                      items: ["About Us", "Leadership Team", "Careers", "Press Releases"],
+                    })}
+                  </Presence>
+                </div>
+
+                <div x-navigation-menu:item="{value: 'developers'}">
+                  <button x-navigation-menu:trigger="{value: 'developers'}">
+                    Developers
+                    {html(ChevronDown)}
+                  </button>
+                  <Presence x-navigation-menu:content="{value: 'developers'}">
+                    <Presence x-navigation-menu:indicator>
+                      <div x-navigation-menu:arrow />
+                    </Presence>
+                    {renderLinks({
+                      value: "developers",
+                      items: ["Investors", "Partners", "Corporate Responsibility"],
+                    })}
+                  </Presence>
+                </div>
+
+                <div x-navigation-menu:item="{value: 'pricing'}">
+                  <a x-navigation-menu:link="{value: 'pricing'}" href="#">
+                    Pricing
+                  </a>
+                </div>
+              </div>
+            </div>
+          </main>
+
+          <Toolbar viz>
+            <Controls config={navigationMenuControls} />
+            <StateVisualizer label="navigation-menu" context={["value", "previousValue"]} />
+          </Toolbar>
+        </div>
+      </body>
+    </html>
+  )
+})
