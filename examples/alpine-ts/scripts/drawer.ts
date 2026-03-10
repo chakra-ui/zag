5e5f53046f0e84c77c412c032dd08437753ea06e
Chang Min Lee <nilpotential@proton.me>
2026-03-10 22:40:07 +0900

    refactor: drawer wip

--- examples/alpine-ts/scripts/drawer.ts
+++ examples/alpine-ts/scripts/drawer.ts
@@ -1,6 +1,11 @@
 import * as drawer from "@zag-js/drawer"
+import * as presence from "@zag-js/presence"
+import { drawerControls } from "@zag-js/shared"
 import Alpine from "alpinejs"
-import { usePlugin } from "../lib"
+import { useControls, usePlugin } from "../lib"
+import "../../shared/styles/drawer.module.css"
 
+Alpine.plugin(usePlugin("presence", presence))
+Alpine.data("drawer", useControls(drawerControls))
 Alpine.plugin(usePlugin("drawer", drawer))
 Alpine.start()
--- examples/alpine-ts/server/routes/drawer/basic.tsx
+++ examples/alpine-ts/server/routes/drawer/basic.tsx
@@ -1,15 +1,14 @@
 import { defineHandler } from "nitro/h3"
-import { drawerControls, getControlDefaults } from "@zag-js/shared"
+import { drawerControls } from "@zag-js/shared"
 import { Controls } from "../../components/controls"
 import { Head } from "../../components/head"
 import { Nav } from "../../components/nav"
 import { Presence } from "../../components/presence"
 import { StateVisualizer } from "../../components/state-visualizer"
 import { Toolbar } from "../../components/toolbar"
+import styles from "../../../../shared/styles/drawer.module.css"
 
 export default defineHandler((event) => {
-  const state = getControlDefaults(drawerControls)
-
   return (
     <html>
       <Head>
@@ -17,34 +16,35 @@ export default defineHandler((event) => {
       </Head>
 
       <body>
-        <div
-          class="page"
-          x-data={JSON.stringify(state)}
-          x-id="['drawer']"
-          x-drawer={`{id: $id('drawer'), ${Object.keys(state)}}`}
-        >
+        <div class="page" x-data="drawer" x-id="['drawer']" x-drawer="{id: $id('drawer'), ...context}">
           <Nav currentComponent={event.context.currentComponent as string} />
 
-          <main class="drawer" x-data="{hidden: !$drawer.open}">
-            <button x-drawer:trigger>Open</button>
-            <Presence x-drawer:backdrop x-on:animationstart="(e)=> console.log(e)" />
-            <Presence x-drawer:content x-on:animationstart="(e) => console.log(e)">
-              <div x-drawer:grabber>
-                <div x-drawer:grabber-indicator />
-              </div>
-              <div x-drawer:title>Drawer</div>
-              <div data-no-drag="true">No drag area</div>
-              <div class="scrollable">
-                {Array.from({ length: 100 }).map((_element, index) => (
-                  <div>Item {index}</div>
-                ))}
-              </div>
-            </Presence>
+          <main class="drawer">
+            <button class={styles.trigger} x-drawer:trigger>
+              Open
+            </button>
+            <Presence class={styles.backdrop} x-drawer:backdrop />
+            <div class={styles.positioner} x-drawer:positioner>
+              <Presence class={styles.content} x-drawer:content>
+                <div class={styles.grabber} x-drawer:grabber>
+                  <div class={styles.grabberIndicator} x-drawer:grabber-indicator />
+                </div>
+                <div x-drawer:title>Drawer</div>
+                <div data-no-drag class={styles.noDrag}>
+                  No drag area
+                </div>
+                <div class={styles.scrollable} data-testid="scrollable">
+                  {Array.from({ length: 100 }).map((_element, index) => (
+                    <div>Item {index}</div>
+                  ))}
+                </div>
+              </Presence>
+            </div>
           </main>
 
           <Toolbar>
-            <Controls config={drawerControls} state={state} slot="controls" />
-            <StateVisualizer label="drawer" context={["dragOffset", "activeSnapPoint", "resolvedActiveSnapPoint"]} />
+            <Controls config={drawerControls} slot="controls" />
+            <StateVisualizer label="drawer" context={["dragOffset", "snapPoint", "resolvedActiveSnapPoint"]} />
           </Toolbar>
         </div>
       </body>
