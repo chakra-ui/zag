ee720f24bc7b0398d0501f02fa4d9253edd954e5
Chang Min Lee <nilpotential@proton.me>
2026-03-17 16:28:57 +0900

    feat(alpine): drawer indent

--- examples/alpine-ts/scripts/drawer.ts
+++ examples/alpine-ts/scripts/drawer.ts
@@ -3,9 +3,16 @@ import * as presence from "@zag-js/presence"
 import { drawerControls } from "@zag-js/shared"
 import Alpine from "alpinejs"
 import { useControls, usePlugin } from "../lib"
+import { normalizeProps } from "../lib/normalize-props"
 import "../../shared/styles/drawer.module.css"
+import "../../shared/styles/drawer-indent.module.css"
 
 Alpine.plugin(usePlugin("presence", presence))
+Alpine.magic("stack", () => drawer.createStack())
+Alpine.magic(
+  "connectStack",
+  () => (snapshot: drawer.DrawerStackSnapshot) => drawer.connectStack(snapshot, normalizeProps),
+)
 Alpine.data("drawer", useControls(drawerControls))
 Alpine.plugin(usePlugin("drawer", drawer))
 Alpine.start()
