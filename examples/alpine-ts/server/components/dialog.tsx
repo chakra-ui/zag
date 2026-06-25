d7622b648c5b48db4aee718567f93026938e049c
Chang Min LEE <nilpotential@proton.me>
2026-06-25 12:23:56 +0900

    fix: single child inside template

--- examples/alpine-ts/server/components/dialog.tsx
+++ examples/alpine-ts/server/components/dialog.tsx
@@ -4,16 +4,18 @@ export function Dialog(props: any) {
       <button x-dialog:trigger>Open Dialog</button>
       <template x-if="$dialog().open">
         <template x-teleport="body">
-          <div x-dialog:backdrop />
-          <div x-dialog:positioner>
-            <div x-dialog:content>
-              <h2 x-dialog:title>Edit profile</h2>
-              <p x-dialog:description>Make changes to your profile here. Click save when you are done.</p>
-              <div>
-                <input placeholder="Enter name..." />
-                <button>Save</button>
+          <div>
+            <div x-dialog:backdrop />
+            <div x-dialog:positioner>
+              <div x-dialog:content>
+                <h2 x-dialog:title>Edit profile</h2>
+                <p x-dialog:description>Make changes to your profile here. Click save when you are done.</p>
+                <div>
+                  <input placeholder="Enter name..." />
+                  <button>Save</button>
+                </div>
+                <button x-dialog:close-trigger>Close</button>
               </div>
-              <button x-dialog:closse-trigger>Close</button>
             </div>
           </div>
         </template>
