9b2b708d7d06ce211faac2e4f89f123c051fdc74
Chang Min LEE <nilpotential@proton.me>
2026-06-25 23:32:16 +0900

    fix: watch object of deps

--- examples/alpine-ts/lib/track.ts
+++ examples/alpine-ts/lib/track.ts
@@ -4,18 +4,8 @@ import Alpine from "alpinejs"
 export const track = (deps: any[], effect: VoidFunction) => {
   // @ts-ignore @types/alpinejs is outdated
   Alpine.watch(
-    () => [...deps.map((d) => d())],
-    (current: any[], previous: any[]) => {
-      let changed = false
-      for (let i = 0; i < current.length; i++) {
-        if (!isEqual(previous[i], current[i])) {
-          changed = true
-          break
-        }
-      }
-      if (changed) {
-        effect()
-      }
-    },
+    () => Object.fromEntries(deps.map((d, i) => [i, d()])),
+    (current: any, previous: any) =>
+      Object.keys(current).some((key) => !isEqual(current[key], previous[key])) && effect(),
   )
 }
