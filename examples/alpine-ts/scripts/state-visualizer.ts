f872ed2a16d9514e033fb609948e64f4ee670f3d
Chang Min LEE <nilpotential@proton.me>
2026-06-26 14:56:56 +0900

    fix: service in state visualizer

--- examples/alpine-ts/scripts/state-visualizer.ts
+++ examples/alpine-ts/scripts/state-visualizer.ts
@@ -3,7 +3,7 @@ import Alpine from "alpinejs"
 
 Alpine.magic("highlightState", (el) => {
   return ({ label, omit, context }: { label: string; omit?: string[]; context?: string[] }) => {
-    const { [`_x_${label.replaceAll("-", "_")}_service`]: service } = Alpine.$data(el) as any
+    const service = (Alpine.$data(el) as any)[`$_x_${label.replaceAll("-", "_")}_service`]
     return highlightState(
       {
         state: service.state.get(),
